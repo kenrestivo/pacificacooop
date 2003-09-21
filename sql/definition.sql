2b38d771a4094b2be8afeadfbd3634c126a946ad
ken restivo <ken@restivo.org>
2003-09-21 00:27:47 +0000

    first post!
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@7 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ sql/definition.sql
@@ -0,0 +1,47 @@
+--  $Id$
+--  database schema for co-op insurance database
+
+create table coop.ins(
+    insid int(32) not null auto_increment,
+    last varchar(255),
+    first varchar(255),
+    middle varchar(255),
+    poilicynum varchar(255),
+    vidnum varchar(255),
+    expires date,
+    companyname varchar(255),
+    naic varchar(255),
+    primary key (insid)
+);
+
+
+create table coop.license(
+    licid int(32) not null auto_increment,
+    last varchar(255),
+    first varchar(255),
+    middle varchar(255),
+    state varchar(40),
+    licensenum varchar(100),
+    expires date,
+    primary key (licid)
+);
+
+
+create table coop.kids(
+    kidsid int(32) not null auto_increment,
+    last varchar(255),
+    first varchar(255),
+    primary key (kidsid)
+);
+
+create table coop.parents(
+    parentsid int(32) not null auto_increment,
+    parentslast varchar(255),
+    parentsfirst varchar(255),
+    kidslast varchar(255),
+    kidsfirst varchar(255),
+    primary key (parentsid)
+);
+
+
+-- EOF
