d03dc338f72b48607dd1524af510f983faf1c8de
ken restivo <ken@restivo.org>
2003-10-28 15:37:45 +0000

    backup script now handles multiple tables.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@123 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
 #$Id$
-#
+# nightly cron job to back up all of the key databases
+# in the live version of the app
 
 
 DATE=`/bin/date +%Y.%m.%d.%H.%M.%S`
+BASEURL="http://www.pacificacoop.org/sf/export.php?command=backup&db="
+SAVE=/mnt/kens/ki/is/c/coopinsurance/sql/backups
+DBS="leads figlue inc"
 
 /home/ken/bin/online on 
 
-/usr/bin/lynx -source 'http://www.pacificacoop.org/sf/export.php?command=backup&db=leads' > /mnt/kens/ki/is/c/coopinsurance/sql/backups/coopbackup${DATE}.txt
+for i in $DBS ; do
+	echo "----- backing up ${i} ------" >> $SAVE/coopbackup${DATE}.txt
+	/usr/bin/lynx -source ${BASEURL}${i} >> $SAVE/coopbackup${DATE}.txt
+done
 
 /home/ken/bin/online off
 
