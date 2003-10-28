d860594fce1c7a6049384fcaa10a8702f2f9486b
ken restivo <ken@restivo.org>
2003-10-28 02:21:54 +0000

    added in matt's nag page.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@121 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ web/reports/nag.php
@@ -0,0 +1,85 @@
+<HTML>
+<HEAD>
+	<TITLE>Springfest Reminder List</TITLE>
+</HEAD>
+
+<BODY>
+
+<h2>Pacifica Co-Op Nursery School</h2>
+<h2>Springfest Leads Summary</h2>
+
+<?php
+	# $Id$
+
+	#  Copyright (C) 2003  ken restivo <ken@restivo.org>
+	# 
+	#  This program is free software; you can redistribute it and/or modify
+	#  it under the terms of the GNU General Public License as published by
+	#  the Free Software Foundation; either version 2 of the License, or
+	#  (at your option) any later version.
+	# 
+	#  This program is distributed in the hope that it will be useful,
+	#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+	#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	#  GNU General Public License for more details. 
+	# 
+	#  You should have received a copy of the GNU General Public License
+	#  along with this program; if not, write to the Free Software
+	#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+	require_once("shared.php");
+
+	#useful for form debugging. 
+	#some host's security policies tend to break globals. use this to check.
+	#confessVars();
+
+	printf("<form method=POST ACTION='%s'>\n", $_SERVER['PHP_SELF']);
+	
+	$latechecked =  $HTTP_POST_VARS['lateonly'] ? "checked" : "";
+	printf("\t<input type='checkbox' name='lateonly' %s>", $latechecked);
+	
+	print "Show only families with fewer than 10 leads submitted<br>\n";
+	print "\t<input type='submit' name='subbutton' value='refresh'>\n";
+	print "</form>\n";
+
+	
+	print "<font size=10>\n";
+	print "<table border='0'>";
+
+	$query = "select families.name, families.phone, count(leads.leadsid) as cntlead
+	       	from families 
+       		left join leads on families.familyid = leads.familyid
+		group by families.name\n";
+
+	if ($latechecked)
+		$query = $query . "having cntlead < 10\n";
+
+	$query = $query . "order by cntlead desc, families.name;";
+
+	$list = mysql_query($query);
+	
+	echo mysql_error();
+
+	print "<tr><td><em>Family Name</em></td><td><em>Leads Submitted</em></td><td><em>Phone</em></td></tr>\n";
+	
+	while($row = mysql_fetch_array($list))
+	{
+		print "<tr><td>\n";
+		print $row[name];
+		print "</td><td>";
+		print $row[cntlead];
+		print "</td><td>";
+		print $row[phone];
+		print "</td></tr>\n";
+	}
+
+	print "</table>\n";
+	print "</font>\n";
+	# end of inner php code
+?>
+
+</BODY>
+</HTML>
+
+<!-- END INDEX -->
+ 
