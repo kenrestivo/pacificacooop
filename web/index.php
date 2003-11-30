76d457991973917440776bc3e9fdb7540d949e1e
ken restivo <ken@restivo.org>
2003-11-30 07:58:53 +0000

    at last, the pachinko machine is working.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@217 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/index.php
+++ web/index.php
@@ -29,13 +29,14 @@
 		<h2>Pacifica Co-Op Nursery School</h2>
 	";
 
-//	confessArray($HTTP_POST_VARS, "vars");
+	//confessArray($HTTP_POST_VARS, "vars");
 
 	$auth = logIn($HTTP_POST_VARS['auth']);
 
-	print "index.php. logIn() returns with:";
-//	confessArray($auth, "afterlogin");
+	//confessArray($auth, "index.php. login() returns with");
 	error_log("   ");
+	
+
 
 	print "</BODY> </HTML>"
 
