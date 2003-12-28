6bfdcb3e7cacbd738f556d2c2376209c5d2ee194
ken restivo <ken@restivo.org>
2003-12-28 05:28:44 +0000

    notes. also, make save location modular in backup script.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@316 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -6,10 +6,12 @@
 
 
 DBS="leads figlue inc faglue auction users groups privs families parents kids attendance"
+#SAVE=../sql/backups
 SAVE=/mnt/kens/ki/is/c/coopinsurance/sql/backups
 BASEURL="http://www.pacificacoop.org/sf/export.php?command=backup&db="
 DATE=`/bin/date +%Y.%m.%d.%H.%M.%S`
 
+mkdir -p ${SAVE}
 
 for i in $DBS ; do
 	echo "----- backing up ${i} ------" >> $SAVE/coopbackup${DATE}.txt
