946bb2b28fa7dfdb8404b426b7f5783ec5a36e5b
ken restivo <ken@restivo.org>
2004-02-05 03:05:47 +0000

    yeow! big showstopper, warnDev() wasn't included because shared.inc wasnt. duh.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@403 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/index.php
+++ web/index.php
@@ -18,6 +18,7 @@
 	#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 	require_once("first.inc");
+	require_once("shared.inc");
 
 	require_once("auth.inc");
 	require_once("auctionfuncs.inc");
