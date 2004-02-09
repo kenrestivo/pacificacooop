c20613c291c73dd852c90192290e1d33f32e8761
ken restivo <ken@restivo.org>
2004-02-09 05:12:10 +0000

    duhh.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@427 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- qa/httpunit-jscheme.scm
+++ qa/httpunit-jscheme.scm
@@ -3,4 +3,16 @@
 (import "com.meterware.httpunit.*")
 (define wc (new 'WebConversation))
 (define wr (.getResponse wc "http://www/coop-dev/")) 
+(define els (.getElementNames wr))
+(define links (.getLinks wr))
+(define forms (.getForms wr))
 
+(define form (vector-ref forms 0)) ;; the first one, just for grunts.
+(.getAction form)
+(.getParameterNames form)
+(.getSubmitButtons form)
+
+
+;; (.getText wr)
+
+;;EOF
