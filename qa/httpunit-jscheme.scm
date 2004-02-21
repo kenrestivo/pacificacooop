be0466529b2ee6bc2161567d4656bcb84a60eb09
ken restivo <ken@restivo.org>
2004-02-21 21:07:45 +0000

    skij-ified it!
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@458 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- qa/httpunit-jscheme.scm
+++ qa/httpunit-jscheme.scm
@@ -3,28 +3,30 @@
 ;; experimental scripts to control jwebunit and httpunit via jscheme
 ;; (load "/mnt/kens/ki/proj/coop/qa/httpunit-jscheme.scm")
 
-(import "net.sourceforge.jwebunit.*")
 
-(define wtc (new 'WebTestCase))
+(define wtc (new 'net.sourceforge.jwebunit.WebTestCase))
 
 (define gtc (invoke wtc 'getTestContext))
 (invoke gtc 'setBaseUrl "http://www/coop-dev")
 
-;; basic login
+;; choose family
 (invoke wtc 'beginAt "/")
 ;; TODO add asserts that this is the RIGHT page
 (invoke wtc 'selectOption "auth[uid]" "Restivo Family")
 (invoke wtc 'assertSubmitButtonPresent "login")
 (invoke wtc 'submit "login")
 ;; TODO add asserts here that i have the RIGHT page!
+
+;;enter password
 (invoke wtc 'setFormElement "auth[pwd]" "tester")
 (invoke wtc 'setFormElement "auth[pwd]" "tester")
 (invoke wtc 'assertSubmitButtonPresent "login")
 (invoke wtc 'assertFormElementPresent "auth[pwd]")
 (invoke wtc 'submit "login")
+;; TODO assertions here that it's the right page.
 
 
 (define dl (invoke wtc 'getDialog))
-;;(invoke dl 'getResponseText)
+(invoke dl 'getResponseText)
 
 ;;EOF
