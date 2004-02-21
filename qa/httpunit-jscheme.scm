4dd0147f893e1b1d920ded71a665be40849a37f0
ken restivo <ken@restivo.org>
2004-02-21 18:26:55 +0000

    well, add the asserts.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@457 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- qa/httpunit-jscheme.scm
+++ qa/httpunit-jscheme.scm
@@ -1,6 +1,7 @@
 ;; $Id$
 
 ;; experimental scripts to control jwebunit and httpunit via jscheme
+;; (load "/mnt/kens/ki/proj/coop/qa/httpunit-jscheme.scm")
 
 (import "net.sourceforge.jwebunit.*")
 
@@ -9,6 +10,21 @@
 (define gtc (invoke wtc 'getTestContext))
 (invoke gtc 'setBaseUrl "http://www/coop-dev")
 
+;; basic login
 (invoke wtc 'beginAt "/")
+;; TODO add asserts that this is the RIGHT page
+(invoke wtc 'selectOption "auth[uid]" "Restivo Family")
+(invoke wtc 'assertSubmitButtonPresent "login")
+(invoke wtc 'submit "login")
+;; TODO add asserts here that i have the RIGHT page!
+(invoke wtc 'setFormElement "auth[pwd]" "tester")
+(invoke wtc 'setFormElement "auth[pwd]" "tester")
+(invoke wtc 'assertSubmitButtonPresent "login")
+(invoke wtc 'assertFormElementPresent "auth[pwd]")
+(invoke wtc 'submit "login")
+
+
+(define dl (invoke wtc 'getDialog))
+;;(invoke dl 'getResponseText)
 
 ;;EOF
