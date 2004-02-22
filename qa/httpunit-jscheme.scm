e614a48b0b0319ff48d6e6061654d790eed2523c
ken restivo <ken@restivo.org>
2004-02-22 03:19:49 +0000

    added get tables
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@462 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- qa/httpunit-jscheme.scm
+++ qa/httpunit-jscheme.scm
@@ -6,7 +6,7 @@
 
 (define wtc (new 'net.sourceforge.jwebunit.WebTestCase))
 
-(define gtc (invoke wtc 'getTestContext))
+(define gtc (invoke wtc 'getTestContext)) ;; the junit object
 (invoke gtc 'setBaseUrl "http://www/coop-dev")
 
 ;; choose family
@@ -15,6 +15,9 @@
 	(begin
 	  (invoke wtc 'beginAt "/")
 	  ;; TODO add asserts that this is the RIGHT page
+	  (invoke wtc 'assertTextPresent "</html>")
+	  (invoke wtc 'assertFormPresent )
+	  (invoke wtc 'assertSubmitButtonPresent "login")
 
 	  (invoke wtc 'selectOption "auth[uid]" "Restivo Family")
 	  (invoke wtc 'assertSubmitButtonPresent "login")
@@ -25,6 +28,8 @@
 (define enter-password
   (lambda (wtc)
 	(begin
+	  (invoke wtc 'assertTextPresent "</html>")
+	  (invoke wtc 'assertFormPresent )
 	  (invoke wtc 'assertSubmitButtonPresent "login")
 	  (invoke wtc 'assertFormElementPresent "auth[pwd]")
 
@@ -33,11 +38,28 @@
 	  (invoke wtc 'submit "login")
 	  )))
 
+(define main-page-ok
+  (lambda (wtc)
+	(begin
+	  (invoke wtc 'assertTextPresent "</html>")
+	  (invoke wtc 'assertLinkPresentWithText "Log Out")
+	  (invoke wtc 'assertLinkPresentWithText "Enter New")
+	  (invoke wtc 'assertLinkPresentWithText "View")
+	  )))
+
 (define dump-page
   (lambda(wtc)
 	(begin
-	  (define dl (invoke wtc 'getDialog))
+	(let* ((dl (invoke wtc 'getDialog))) ;; the httpunit object
 	  (invoke dl 'getResponseText)
-	  )))
+	  ))))
+
+(define get-tables
+  (lambda(wtc)
+	(begin
+	  (describe (invoke (invoke (peek (invoke
+									   wtc 'getDialog)
+									  'wc) 'getCurrentPage)
+						'getTables)))))
 
 ;;EOF
