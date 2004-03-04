eccf0b652f0fcb67036f4e1e946710859d0c642a
ken restivo <ken@restivo.org>
2004-03-04 10:54:37 +0000

    duh stupid sql bug
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@488 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/import_packages.scm
+++ scripts/import_packages.scm
@@ -9,7 +9,7 @@
   (sql-query cid
 			 "select auctionid, description, amount
 				from auction
-				where package_id < 1
+				where (package_id is null or package_id < 1)
 					and date_received > '0000-00-00'"))
 
 ;;;; last-insert-id
@@ -19,14 +19,15 @@
 	 (sql-query cid "select last_insert_id()") 1) 0))
 
 ;;;; for-each-vector
-(define for-each-vector (lambda (procedure vec use-number)
+(define for-each-vector
+  (lambda (procedure vec use-number)
 	(do 
 		;;variable, init, step (basically, a let)
 		( (i 1 (+ 1 i)))
 		;;test, expression
 		( (>= i (vector-length vec)))
-		;; command
-		(procedure (if use-number i (vector-ref vec i))))))
+	  ;; command
+	  (procedure (if use-number i (vector-ref vec i))))))
 
 ;;; new-package
 (define new-package
