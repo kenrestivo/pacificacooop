6244b942337a7083c6d2853b1ed9993415748a74
ken restivo <ken@restivo.org>
2004-03-09 16:21:38 +0000

    fix query. and ticket purchase info. and... lots of todo and design thrashing.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@495 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- sql/queries.sql
+++ sql/queries.sql
@@ -264,16 +264,18 @@ select coa.description,
 
 
 --- nasty package join. the 'users' hack is to get "xxx family" w/o coding
- select auction.* , coalesce(users.name, companies.company_name) as donor
-    from auction 
-        left join packages on auction.package_id = packages.package_id 
-        left join faglue on faglue.auctionid = auction.auctionid 
-            left join users on users.familyid = faglue.familyid 
-        left join companies_auction_join 
-            on companies_auction_join.auctionid = auction.auctionid
-            left join companies 
-                on companies_auction_join.company_id = companies.company_id
-    where auction.package_id is null;
-
-
+select auction.* , 
+            coalesce(users.name, companies.company_name) as donor
+        from auction 
+            left join packages on auction.package_id = packages.package_id 
+            left join faglue on faglue.auctionid = auction.auctionid 
+                left join users on users.familyid = faglue.familyid 
+            left join companies_auction_join 
+                on companies_auction_join.auctionid = auction.auctionid
+                left join companies 
+                    on companies_auction_join.company_id = 
+                        companies.company_id
+        where auction.package_id < 1 and date_received is not null 
+            and date_received > '0000-00-00'
+        
 --- EOF
