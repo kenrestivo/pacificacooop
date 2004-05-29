e6697f5956c289f39fc7b6f94ec07da8671854ed
ken restivo <ken@restivo.org>
2004-05-29 02:31:32 +0000

    need create privs in order to use temp tables
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@506 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- sql/definition.sql
+++ sql/definition.sql
@@ -458,11 +458,11 @@ create table enhancement_hours (
 -- the user/passwords used by the web view page AND my update tool..
 -- these MUST be done manually for db's not named coop!!
 --TODO: i have to grant all to myself on this db! duh.
-grant select, update, insert, delete on coop.* to input@'%' 
+grant select, update, insert, delete, create on coop.* to input@'%' 
 	identified by 'test'; 
 grant select, update, insert, delete on coop.* to input@localhost 
 	identified by 'test';
-grant select, insert, update, delete on coop.* to springfest@'%'
+grant select, insert, update, delete, create on coop.* to springfest@'%'
     identified by '92xPi9';
 grant select, insert, update, delete on coop.* to springfest@localhost
     identified by '92xPi9';
