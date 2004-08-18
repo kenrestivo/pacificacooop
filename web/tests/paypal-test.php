eff3f7b3581007ca3d8bd5b986d1d93950b2e25c
ken restivo <ken@restivo.org>
2004-08-18 03:48:16 +0000

    fix comments.ad an idea for possible val/def
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@527 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/tests/paypal-test.php
+++ web/tests/paypal-test.php
@@ -14,28 +14,18 @@
 
 	print " here i am <br>\n";
 	
-/*
-	if(NULL < "2004-01-02"){
-		print "NULL is less<br>";
+function foobar($thing, $another = NULL)
+{
+	print "\n<br>$thing: ";
+	if($another != NULL){
+		print "YES [$another]";
 	}
-
-	if("0000-00-00" < "2004-01-02"){
-		print "0000 is less<br>";
-	}
-
-	if("2004-01-02" < "2004-01-02"){
-		print "day later is less<br>";
-	}
-*/
-
-
-$ar = array("2004-03-01", "2004-08-01", "2005-03-01", "2000-12-04");
-foreach ($ar as $i){
-	print "$i = " . findSchoolYear($i) . "<br>";
 }
-	print "TODAY = " . findSchoolYear() . "<br>";
 
-	print "</body></html>";
 
+foobar("noval");
+foobar("emptystring" , "");
+foobar("string" , "fa");
+foobar("num" , "1");
 
 ?>
