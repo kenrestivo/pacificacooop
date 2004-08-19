d4138da492d35a7b9c8123a5ce7b08847f82ae60
ken restivo <ken@restivo.org>
2004-08-19 07:22:54 +0000

    combine this into one tight little function.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@532 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/fixfamilies.scm
+++ scripts/fixfamilies.scm
@@ -7,28 +7,27 @@
 			 (database simplesql))
 (require 'printf)
 
+;;(load "/mnt/kens/ki/is/scheme/lib/kenlib.scm")
+
 ;; XXX note, this is fakery. you'll need to manually put in the root pw's
 ;;(define dbh (simplesql-open 'mysql "coop" "127.0.0.1" "input" "test" "2299"))
 (define dbh (simplesql-open 'mysql "coop" "bc" "input" "test"))
 	
-
-(define generic-families
-  (simplesql-query dbh
-		   "select leads.leadsid, leads.last, inc.payer 
+(for-each 
+ (lambda (vec)
+   (simplesql-query dbh
+					(sprintf #f "update leads set last = '%s'
+							where leadsid = %d" 
+							 (vector-ref vec 2)
+							 (vector-ref vec 0))))
+ (cdr (simplesql-query dbh
+			 "select leads.leadsid, leads.last, inc.payer 
 				from leads left join invitation_rsvps 
 					on leads.leadsid = invitation_rsvps.leadsid 
 				left join inc on invitation_rsvps.incid = inc.incid 
 			where last like \"%Family%\" 
-				and invitation_rsvps.incid is not null"))
+				and invitation_rsvps.incid is not null")))
 
-(define (fix-families vec)
-  (simplesql-query dbh
-		   (sprintf #f "update leads set last = '%s'
-							where leadsid = %d" 
-						(vector-ref vec 2)
-						(vector-ref vec 0))))
-
-(for-each fix-families (cdr generic-families))
 
 (simplesql-close dbh)
 
