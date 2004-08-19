a8d18ae7e28fd4b700f4b785b1ee158e759558bb
ken restivo <ken@restivo.org>
2004-08-19 01:51:35 +0000

    goddamn i love scheme. quick script to fix the old names
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@531 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ scripts/fixfamilies.scm
@@ -0,0 +1,35 @@
+;;; $Id$
+;; use the proper family name in leads where all i have is "family"
+;; grab the family name from the income stuff.
+;; (load "/mnt/kens/ki/proj/coop/scripts/fixfamilies.scm")
+
+(use-modules (ice-9 slib)
+			 (database simplesql))
+(require 'printf)
+
+;; XXX note, this is fakery. you'll need to manually put in the root pw's
+;;(define dbh (simplesql-open 'mysql "coop" "127.0.0.1" "input" "test" "2299"))
+(define dbh (simplesql-open 'mysql "coop" "bc" "input" "test"))
+	
+
+(define generic-families
+  (simplesql-query dbh
+		   "select leads.leadsid, leads.last, inc.payer 
+				from leads left join invitation_rsvps 
+					on leads.leadsid = invitation_rsvps.leadsid 
+				left join inc on invitation_rsvps.incid = inc.incid 
+			where last like \"%Family%\" 
+				and invitation_rsvps.incid is not null"))
+
+(define (fix-families vec)
+  (simplesql-query dbh
+		   (sprintf #f "update leads set last = '%s'
+							where leadsid = %d" 
+						(vector-ref vec 2)
+						(vector-ref vec 0))))
+
+(for-each fix-families (cdr generic-families))
+
+(simplesql-close dbh)
+
+;;; EOF
