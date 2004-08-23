dacf2173c4a6249f694eb266b7923f02a9e16753
ken restivo <ken@restivo.org>
2004-08-23 08:58:14 +0000

    moved code around
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@537 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/fixschema.scm
+++ scripts/fixschema.scm
@@ -11,47 +11,6 @@
 ;; XXX note, this is fakery. you'll need to manually put in the root pw's
 ;;(define dbh (simplesql-open 'mysql "coop" "127.0.0.1" "paccoop" "test" "2299"))
 
-
-(define (rename-table-query items)
-   (sprintf #f "rename table %s to %s"
-		   (car items) (cadr items))) 
-
-;; TODO i have to fish the definition out of the definition.sql,
-;; or out of a mysqldump somewhere
-(define (rename-column-query items)
-  (let ((sp (string-split (car items) #\.))
-		(new (string-split (cadr items) #\.)))
-	(sprintf #f "alter table %s change column %s %s"
-				 (car sp) (cadr sp) (cadr new))) )
-
-;; simple dispatcher, using cute scheme-ism
-(define (rename-query items)
-  ((if (string-index (car items) #\.)
-	  rename-column-query 
-	  rename-table-query )
-   items))
-
-(define (fix-schema fixfile)
-  (let ((p (open-input-file fixfile) ) )
-	(begin 
-	  (do ((line (read-line p) (read-line p)))
-		  ((or (eof-object? line) ))
-		((lambda (x) (if (and (= 2 (length x))
-							  (not (eq? (string-index (car x) #\#) 0) ))
-						 (pp (rename-query x))
-						 ))
-		 (string-split line #\space)))
-	  (close p) )))
-
-;; clean sql definition line
-;; TODO! must combine before yanking ,'s!
-(define (clean-line line)
-		 (map (lambda (y)
-				 (regexp-substitute/global #f  "[ \t]" y  'pre 'post)) 
-		  (map (lambda (x)
-				 (regexp-substitute/global #f  ",$" x  'pre 'post)) 
-			   (delete "" line))))
-
 ;; if it is a COLUMN, i'll want to do:
 ;;(cons (car tl) (string-join (cdr tl)))
 
@@ -71,5 +30,52 @@
 					   (pp x)))
 	   (clean-line (string-split line #\space))))
 	 (close p) ))	
+
+
+;;; the easy one: tables.
+(define (rename-table-query items)
+   (sprintf #f "rename table %s to %s"
+		   (car items) (cadr items))) 
+
+;; TODO i have to fish the definition out of the definition.sql,
+;; or out of a mysqldump somewhere
+(define (rename-column-query items)
+  (let ((sp (string-split (car items) #\.))
+		(new (string-split (cadr items) #\.)))
+	(sprintf #f "alter table %s change column %s %s"
+				 (car sp) (cadr sp) (cadr new))) )
+
+;; simple dispatcher, using cute scheme-ism
+(define (rename-query items)
+  ((if (string-index (car items) #\.)
+	  rename-column-query 
+	  rename-table-query )
+   items))
+
+;;; this is basically MAIN
+(define (fix-schema fixfile)
+  (let ((p (open-input-file fixfile) ) )
+	(begin 
+	  (do ((line (read-line p) (read-line p)))
+		  ((or (eof-object? line) ))
+		((lambda (x) (if (and (= 2 (length x))
+							  (not (eq? (string-index (car x) #\#) 0) ))
+						 (pp (rename-query x))
+						 ))
+		 (string-split line #\space)))
+	  (close p) )))
+
+
+
+
+
+;; clean sql definition line
+;; TODO! must combine before yanking ,'s!
+(define (clean-line line)
+		 (map (lambda (y)
+				 (regexp-substitute/global #f  "[ \t]" y  'pre 'post)) 
+		  (map (lambda (x)
+				 (regexp-substitute/global #f  ",$" x  'pre 'post)) 
+			   (delete "" line))))
 
 ;; EOF
