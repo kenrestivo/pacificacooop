df4cc5ff435ef93f469d79071b21639ad1a6c50b
ken restivo <ken@restivo.org>
2004-09-13 17:54:54 +0000

    consolidated code to make it crisp and clean. made global look global
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@566 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/yearify.scm
+++ scripts/yearify.scm
@@ -9,11 +9,11 @@
 (define dbh (apply simplesql-open "mysql"
 				   (read-conf "/mnt/kens/ki/proj/coop/sql/db.conf")))
 
-(define debug-flag #t)
+(define *debug-flag* #t)
 
 ;; silly little diagnostic
 (define (doit query)
-  (if debug-flag
+  (if *debug-flag*
 	  (pp query)
 	  (catch #t
 			 (lambda ()
@@ -21,20 +21,21 @@
 			 (lambda x (printf "caught error on [%s]\n" query) (pp x)))))
 
 (define (add-new-column table column-name definition default)
-  (doit (sprintf #f "alter table %s add column %s %s" table column-name definition))
+  (doit (sprintf #f "alter table %s add column %s %s"
+				 table column-name definition))
   (doit (sprintf #f "update %s set %s = '%s'" table column-name default)))
 
 ;;;;;;;;;;;;;;;
 ;; main
-(define tables '("auction_donation_items"
-				 "packages"
-				 "income"
-				 "springfest_attendees"
-				 "territories"))
 
 (for-each  (lambda (table)
 			 (add-new-column table "school_year" "varchar(50)" "2003-2004"))
-			 tables)
+		   '("auction_donation_items"
+			 "packages"
+			 "income"
+			 "springfest_attendees"
+			 "territories"))
+
 
 (simplesql-close dbh)
 
