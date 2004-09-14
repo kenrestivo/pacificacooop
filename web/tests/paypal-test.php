335829fe3f4f872274e769d919baf48a6673b8e0
ken restivo <ken@restivo.org>
2004-09-14 00:12:42 +0000

    well, we are here, hopefully, fixing the narsty-ass bug i had in fillform. it was simple: i just needed mergefields and had the wrong number of args before.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@567 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/tests/paypal-test.php
+++ web/tests/paypal-test.php
@@ -13,19 +13,37 @@
 
 
 	print " here i am <br>\n";
+
+	printf("<FORM METHOD=POST ACTION='%s'>", 
+					$_SERVER['PHP_SELF']);
+	print "\n<table border=1>\n";
+	//user popup (or display) stuff
+	print "\n<tr>\n";
+		print "\t<td>Select user name:\n</td>\n";
+
+		print "\t<td>";
+///here
+		print "</td>\n";
+
+	print "</tr>\n";
+
+	//password stuff
+	print "\n<tr>\n";
+			print "<td>Please type in your new password here: </td>\n";
+		print "\t<td><INPUT TYPE=text NAME='top[auth][pwd]'></td>\n";
+	print "</tr>\n";
+
+
+	print "\n</table>\n";
+
+
+	printf("<INPUT TYPE=submit NAME='login' VALUE='%s'>",
+		$type == 'confirm' || $type == 'both' ? 'Save New Password' : 'Log In');
+	printf("<INPUT TYPE=submit NAME='logout' VALUE='Cancel'>");
+    print "</FORM>";
 	
-function foobar($thing, $another = NULL)
-{
-	print "\n<br>$thing: ";
-	if(!is_null($another)){
-		print "YES [$another]";
-	}
-}
+	confessArray($_POST, "postvars");
+	confessArray($GLOBALS, "globals");
 
-
-foobar("noval");
-foobar("emptystring" , "");
-foobar("string" , "fa");
-foobar("num" , "1");
 
 ?>
