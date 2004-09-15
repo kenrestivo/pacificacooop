3c6ab9e10ba488009883a2f80ec3b6ed0beaa1cd
ken restivo <ken@restivo.org>
2004-09-15 17:39:59 +0000

    making huge progress here!
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@572 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/rastaimport.scm
+++ scripts/rastaimport.scm
@@ -1,44 +1,42 @@
 ;; $Id$
 ;; import the rasta. this is a re-do of the old perl code i wrote a year ago
 
+(use-modules (kenlib) 
+			 (ice-9 slib))
+(require 'printf)
 
-(define *header-found* #f)
-(define *empty-line* #f)
+(define *rasta* '())
 
-(define (check-header line)
-  (let ((header '("Last Name"
-				  "Mom Name *"
-				  "Dad/Partner *"
-				  "Child "
-				  "DOB"
-				  "Address"
-				  "Phone"
-				  "Email"
-				  "M"
-				  "Tu"
-				  "W"
-				  "Th"
-				  "F"
-				  "School Job")))
+(define header '("Last Name"
+				 "Mom Name *"
+				 "Dad/Partner *"
+				 "Child "
+				 "DOB"
+				 "Address"
+				 "Phone"
+				 "Email"
+				 "M"
+				 "Tu"
+				 "W"
+				 "Th"
+				 "F"
+				 "School Job"))
 
-)
-  
 (define (process-rasta-line session line)
-  (set! *header-found* (or *header-found* (check-header line)))
-  (set! *empty-line* (check-empty line))
-  (if (and *header-found*  (not *empty-line))
-	  (list->vector (cons session (parse-csv line)))))
-
+  (set! *rasta* (append *rasta* (list
+								   (list-head (parse-csv line)
+										 (length header))))))
+(define (clean-up-rasta rasta header)
+  (cdr (member header rasta)))
 
 (define (grab-csv-rasta session fixfile)
   (let ((p (open-input-file fixfile) ) )
 	(begin
-	  (set! *header-found* #f)
-	  (set! *empty-line* #f)
+	  (set! *rasta* '())
 	  (do ((line (read-line p) (read-line p)))
 		  ((or (eof-object? line) ))
 			(process-rasta-line session line))
-	  (if (not *header-found*) (write-line "no header found")) ; TODO session
+	  (set! *rasta* (clean-up-rasta *rasta*))
 	  (close p) )))
 
 (grab-csv-rasta "am" "/mnt/kens/ki/proj/coop/imports/AM.csv")
