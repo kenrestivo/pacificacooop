b19e72014975b20e5103febbec39b14a6d1d2310
ken restivo <ken@restivo.org>
2004-09-15 16:44:15 +0000

    this is a depreceated approach. i just found a better one.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@571 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/rastaimport.scm
+++ scripts/rastaimport.scm
@@ -1,15 +1,47 @@
 ;; $Id$
 ;; import the rasta. this is a re-do of the old perl code i wrote a year ago
 
-(define (grab-csv-file fixfile)
+
+(define *header-found* #f)
+(define *empty-line* #f)
+
+(define (check-header line)
+  (let ((header '("Last Name"
+				  "Mom Name *"
+				  "Dad/Partner *"
+				  "Child "
+				  "DOB"
+				  "Address"
+				  "Phone"
+				  "Email"
+				  "M"
+				  "Tu"
+				  "W"
+				  "Th"
+				  "F"
+				  "School Job")))
+
+)
+  
+(define (process-rasta-line session line)
+  (set! *header-found* (or *header-found* (check-header line)))
+  (set! *empty-line* (check-empty line))
+  (if (and *header-found*  (not *empty-line))
+	  (list->vector (cons session (parse-csv line)))))
+
+
+(define (grab-csv-rasta session fixfile)
   (let ((p (open-input-file fixfile) ) )
-	(begin 
+	(begin
+	  (set! *header-found* #f)
+	  (set! *empty-line* #f)
 	  (do ((line (read-line p) (read-line p)))
 		  ((or (eof-object? line) ))
-		(pp (parse-csv line))
-		)
+			(process-rasta-line session line))
+	  (if (not *header-found*) (write-line "no header found")) ; TODO session
 	  (close p) )))
 
-(grab-csv-file "/mnt/kens/ki/proj/coop/imports/AM.csv")
+(grab-csv-rasta "am" "/mnt/kens/ki/proj/coop/imports/AM.csv")
+(grab-csv-rasta "pm" "/mnt/kens/ki/proj/coop/imports/PM.csv")
 
 ;; EOF
