048bb22e5d283395ff396e5b43e182882138ad6d
ken restivo <ken@restivo.org>
2004-09-16 04:21:37 +0000

    jeeebus! moved stuff around. also added a map function to do string trimming.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@579 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/rastaimport.scm
+++ scripts/rastaimport.scm
@@ -3,11 +3,13 @@
 ;; (load "/mnt/kens/ki/proj/coop/scripts/rastaimport.scm")
 
 (use-modules (kenlib) (srfi srfi-1)
+			 (srfi srfi-13)
 			 (ice-9 slib))
 (require 'printf)
 
 (define *rasta* (make-hash-table 3))
 
+;; be sure to modify this if the damn thing ever changes
 (define *header* '("Last Name"
 				 "Mom Name *"
 				 "Dad/Partner *"
@@ -23,27 +25,34 @@
 				 "F"
 				 "School Job"))
 
+;; each session gets dumped in separately, but then gets fixed here.
 (define (merge-am-pm rasta)
   (hash-set! rasta "BOTH"
-			 (append 
+			 (append 					; when i get elite, use macro here
 			  (map  (lambda (x) (append x '("AM"))) (hash-ref *rasta* "AM"))
 			  (map  (lambda (x) (append x '("PM"))) (hash-ref *rasta* "PM")))))
-  
+
+;; wrapper around between: remove the header and footer crap
+(define (clean-up-rasta rasta header session)
+  (hash-set! rasta session
+			 (between  header (make-list (length header) #f)
+					   (hash-ref rasta session))))
+
+;; each line.
 (define (process-rasta-line rasta header session line )
-  (let ((parsed-line (parse-csv line)))
+  (letrec ((parsed-line
+			(map (lambda (x) (safe-string-trim x)) (parse-csv line))))
 	(if (> (length header) (length parsed-line))
-		(throw 'too-short))
+		(throw 'too-short))				;  excel sucks, as does parse-csv
 	(hash-set! rasta session
 			   (append
 				(hash-ref rasta session '())
 				(list
 				 (safe-list-head parsed-line (length header)))))))
 
-(define (clean-up-rasta rasta header session)
-  (hash-set! rasta session
-			 (between  header (make-list (length header) #f)
-					   (hash-ref rasta session))))
-  
+;;(map (lambda (x) (string-trim x)) (parse-csv line))
+
+;; iterate through file, happily singing along the way
 (define (grab-csv-rasta session fixfile)
   (let ((p (open-input-file fixfile) ) )
 	(begin
