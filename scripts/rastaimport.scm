3cce6f207a6f0a8d63bf015f628b0a3398db3e19
ken restivo <ken@restivo.org>
2004-09-16 02:55:42 +0000

    asd hash-ref stuff, or attempt to, anyway
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@577 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/rastaimport.scm
+++ scripts/rastaimport.scm
@@ -1,11 +1,12 @@
 ;; $Id$
 ;; import the rasta. this is a re-do of the old perl code i wrote a year ago
+;; (load "/mnt/kens/ki/proj/coop/scripts/rastaimport.scm")
 
 (use-modules (kenlib) (srfi srfi-1)
 			 (ice-9 slib))
 (require 'printf)
 
-(define *rasta* '())
+(define *rasta* (make-hash-table 3))
 
 (define *header* '("Last Name"
 				 "Mom Name *"
@@ -22,24 +23,30 @@
 				 "F"
 				 "School Job"))
 
-(define (process-rasta-line header session line )
-  (set! *rasta* (append *rasta* (list
-								   (safe-list-head (parse-csv line)
-										 (length header))))))
+(define (process-rasta-line rasta header session line )
+  (hash-set! rasta session
+			 (append
+			  (hash-ref rasta session '())
+			  (list
+			   (safe-list-head (parse-csv line)
+							   (length header))))))
 
-(define (clean-up-rasta rasta header)
-  (between  header (make-list (length header) #f) rasta ))
+(define (clean-up-rasta rasta header session)
+  (hash-set! rasta session
+			 (between  header (make-list (length header) #f)
+					   (hash-ref rasta session))))
   
 (define (grab-csv-rasta session fixfile)
   (let ((p (open-input-file fixfile) ) )
 	(begin
-	  (set! *rasta* '())
+	  (hash-set! *rasta* session '())			; well, i have to
 	  (do ((line (read-line p) (read-line p)))
 		  ((or (eof-object? line) ))
-			(process-rasta-line *header* session line))
+			(process-rasta-line *rasta* *header* session line))
+	  (clean-up-rasta *rasta* *header* session)
 	  (close p) )))
 
-(grab-csv-rasta "am" "/mnt/kens/ki/proj/coop/imports/AM.csv")
-(grab-csv-rasta "pm" "/mnt/kens/ki/proj/coop/imports/PM.csv")
+(grab-csv-rasta "AM" "/mnt/kens/ki/proj/coop/imports/AM.csv")
+(grab-csv-rasta "PM" "/mnt/kens/ki/proj/coop/imports/PM.csv")
 
 ;; EOF
