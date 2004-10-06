dc2bca2ff410f55615400fecfbeb81b29a0e52c6
ken restivo <ken@restivo.org>
2004-10-06 18:14:44 +0000

    first post
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@629 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ scripts/unfixed.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#$Id$
+#move those .fixed temp files to their original names
+
+for i in `ls *.fixed`; do
+ 	FOO=`echo "$i" | sed -e "s/\.fixed//"`
+	 echo "moving $i to $FOO"
+	mv "$i" $FOO"
+done
