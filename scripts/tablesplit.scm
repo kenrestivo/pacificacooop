b8ef8da2403cda24ff8d2ff90aeebc9d781c6d62
ken restivo <ken@restivo.org>
2004-10-10 16:38:07 +0000

    first post
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@638 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ scripts/tablesplit.scm
@@ -0,0 +1,7 @@
+;; $Id$
+;; move from everything-globbed-together to split tables
+;; required for new solicitation/invitations stuff
+;; (load "/mnt/kens/ki/proj/coop/scripts/tablesplit.scm")
+
+
+;; EOF
