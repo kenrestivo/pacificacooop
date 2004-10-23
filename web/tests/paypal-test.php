bd2d712b6817ba334c00ab2011b892f3ce967648
ken restivo <ken@restivo.org>
2004-10-23 20:40:47 +0000

    son of a bitch.. objectifying the table form actually worked!!!
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@669 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/tests/paypal-test.php
+++ web/tests/paypal-test.php
@@ -1,4 +1,4 @@
-<?php 
+26868<?php 
 
 //$Id$
 
@@ -49,169 +49,201 @@ if($auth['state'] != 'loggedin'){
 
 topNavigation($auth,  getUser($auth['uid']));
 
-///////////////////////
+//////////////////////////////////////////
+/////////////////////// COOP CLASS
+class coopPage
+{
+	var $obj;
+	var $build;
+	var $pager_result_size;
+	var $pager_start;
+	var $table;
 
-///////// TABLE POPUP
-$obj = new DB_DataObject();
-$obj->databaseStructure();
-global $_DB_DATAOBJECT;
-//confessArray($_DB_DATAOBJECT, "dataobject");
+	// constructor.
+	function coopPage($table)
+		{
+			// set it here
+			$this->table = $table;
 
-foreach($_DB_DATAOBJECT['INI']['coop'] as $table => $cols) {
-	if(strpos($table, '__keys') == 0)
-		$vals[$table] = $table;
-}
-
-$form = new HTML_QuickForm('gettable', 'get');
-// $grp[] =& HTML_QuickForm::createElement(
-// 	'text', 'table', 'Browse table:');
-$grp[] =& HTML_QuickForm::createElement('select', 'table', null, $vals);
-$grp[] =& HTML_QuickForm::createElement('submit', null, 'Change');
-$form->addGroup($grp, NULL, 'Browse table:');
-$form->display();
-print "<hr>";
-
+			$this->obj = DB_DataObject::factory ($this->table); // & instead?
+			if (PEAR::isError($obj)){
+				die ($obj->getMessage ());
+			}
 
 
-//////////COMMON
-if (!isset ($_REQUEST['table'])){
-	done();
-}
-$table = $_REQUEST['table'];
-
-$obj = DB_DataObject::factory ($table);
-if (PEAR::isError($obj)){
-	die ($obj->getMessage ());
-}
-
-if($_REQUEST['action'] == 'detail'){
-
-	if (isset ($_REQUEST['id'])){
-		$obj->get ($_REQUEST['id']);
-	}
-
-//////////////////
-/////////////DETAIL FORM
-//////////////////
-
-
-//now the good stuff
-	$build =& DB_DataObject_FormBuilder::create ($obj);
-	$form = new HTML_QuickForm($_SERVER['PHP_SELF']);
-	$form->addElement('hidden', 'action', 'detail');
-	$form->addElement('html', thruAuth($auth, 1)); // MUST be global!
-	$form->addElement('hidden', 'table', $table);
-	$form->addElement('hidden', 'id');
-	$build->useForm($form);
-	$form =& $build->getForm();
-	if($form->validate ()){
-		$res = $form->process (array (&$build, 'processForm'), false);
-		if ($res){
-			$obj->debug('processed successfully', 'detailform', 0);
-			header(sprintf('Location: %s?%saction=list&table=%s', 
-						   $_SERVER['PHP_SELF'], 
-						   SID ? SID . "&" : "", 
-						   $table));
 		}
-		echo "aaauugh!<br>";
+
+	function requestOrSession($itemName){
 	}
 
-	$form->display();
+	// HANDL EOBJ STUFF
+	function tablePopup()
+		{
+			//XXX broken. use menu instead anyway? i'd rather.
+			$obj =& new DB_DataObject();	// use the $this->obj?
+			$obj->databaseStructure();
+			global $_DB_DATAOBJECT;
+			//confessArray($_DB_DATAOBJECT, "dataobject");
+
+			foreach($_DB_DATAOBJECT['INI']['coop'] 
+					as $table => $cols) {
+				if(strpos($table, '__keys') == 0)
+					$vals[$table] = $table;
+			}
+
+			$form =& new HTML_QuickForm('gettable', 'get');
+			// $grp[] =& HTML_QuickForm::createElement(
+			// 	'text', 'table', 'Browse table:');
+			$grp[] =& HTML_QuickForm::createElement(
+				'select', 'table', null, $vals);
+			$grp[] =& HTML_QuickForm::createElement(
+				'submit', null, 'Change');
+			$form->addGroup($grp, NULL, 'Browse table:');
+			
+			return $form->toHTML();
+			//print "<hr>";
+		}
+
+	function detailForm()
+		{
+			$auth = $_SESSION['auth'];
+			$table = $_SESSION['table'];
+
+			$this->obj->get($_SESSION[$this->table]['id']);
+			$this->build =& DB_DataObject_FormBuilder::create ($this->obj);
+			$form = new HTML_QuickForm($_SERVER['PHP_SELF']); // XXX &
+			$form->addElement('html', thruAuth($auth, 1));
+			$build->useForm($form);
+			$form =& $build->getForm();
+			if($form->validate ()){
+				$res = $form->process (array 
+									   (&$build, 'processForm'), 
+									   false);
+				if ($res){
+					$obj->debug('processed successfully', 
+								'detailform', 0);
+					$_SESSION['action'] = 'list'; //  next action
+					header(sprintf(
+							   'Location: %s%s', 
+							   $_SERVER['PHP_SELF'], 
+							   SID ? "?" . SID  : ""));
+				}
+				echo "aaauugh!<br>";
+			}
+
+			return $form->toHTML();
 	
-	done();
-}
-
-////////////////////////////
-////////// the list table
-////////////////////////////
+		}
 
 
-// most have only one key. feel around for primary if not
-$keys = $obj->keys ();
-if (is_array ($keys)){
-	$primaryKey = $keys[0];
-}
+	// get the pager info for the table ->obj
+	function calcPager()
+		{
 
-// handle default display field
-$titlefield = $_DB_DATAOBJECT_FORMBUILDER['CONFIG']['select_display_field'];
-if (isset ($obj->select_display_field)){
-	$titlefield = $obj->select_display_field;
-}
+			$count = $this->obj->find();
+			$res .= "$count total records found<br>";
 
-$count = $obj->find();
+				/// pager driver calculations
+			for($i = 1; $i <= $count; $i++){
+				$pager_item_data[] = $i;
+			}
+			$pager_parms = array (
+				'mode' => 'Jumping',
+				'perPage' => 10,
+				'delta' => 4,
+				'itemData' => $pager_item_data
+				);
+			$pager =& new Pager($pager_parms);
+			$pager_result_data = $pager->getPageData();
+			$pager_links = $pager->getLinks();
+			$this->pager_result_size = sizeof($pager_result_data);
+			$this->pager_start = array_shift($pager_result_data);
+			//confessArray($pager_result_data, "pagerresult");
+			//confessArray($pager_links, "pagerlinks");
+			
+			$res .= $pager_links['first'];
+			$res .= $pager_links['all'];
+			$res .= $pager_links['last'];
+			
+			return $res;
+		}
 
-print "$count records found<br>";
+	function linkStuff()
+		{
+			$this->obj->get(1);
+			$this->obj->find();
+			$dosdv = $this->build->getDataObjectSelectDisplayValue(
+				&$this->obj);
+			print "DOSDV $dosdv\n";
+		}
 
-$tab =& new HTML_Table();
+
+	function listTable()
+		{
+
+		// most have only one key. feel around for primary if not
+			$keys = $this->obj->keys ();
+			if (is_array ($keys)){
+				$primaryKey = $keys[0];
+			}
 
 
-/// pager driver calculations
-for($i = 1; $i <= $count; $i++){
-	$pager_item_data[] = $i;
-}
-$pager_parms = array (
-	'mode' => 'Jumping',
-	'perPage' => 10,
-	'delta' => 4,
-	'itemData' => $pager_item_data
-	);
-$pager = new Pager($pager_parms);
-$pager_result_data = $pager->getPageData();
-$pager_links = $pager->getLinks();
+			$tab =& new HTML_Table();
 
-//confessArray($pager_result_data, "pagerresult");
-$pager_result_size = sizeof($pager_result_data);
-$start = array_shift($pager_result_data);
-$obj->limit($start - 1, $pager_result_size);
-$obj->find();					// new find with limit.
+			$res .= $this->calcPager();	
+			$this->obj->limit($this->pager_start - 1, 
+							  $this->pager_result_size);
+			$this->obj->find();					// new find with limit.
 
-// now the table
-$hdr = 0;
-while ($obj->fetch()){
-	$build =& DB_DataObject_FormBuilder::create ($obj);
+		// now the table
+			$hdr = 0;
+			while ($this->obj->fetch()){
+				$this->build =& DB_DataObject_FormBuilder::create (
+					$this->obj);
 
-	$ar = array_merge(sprintf('<a href="%s?action=detail&id=%s&table=%s">
+				$ar = array_merge(
+					sprintf('<a href="%s?action=detail&id=%s&table=%s">
 						Edit</a><br>',
-							  $_SERVER['PHP_SELF'], 
-							  $obj->$primaryKey, $table),
-					  array_values($obj->toArray()));
-	//yay! i updated my config.php, and now $obj->$titlefield works!
+							$_SERVER['PHP_SELF'], 
+							$this->obj->$primaryKey, $this->table),
+					array_values($this->obj->toArray()));
 
-	if($hdr++ < 1){
-		$tab->addRow(array_merge("Action", 
-								 array_keys($obj->toArray())), 
-					 "bgcolor=9999cc", "TH" );
-	}
-	$tab->addRow($ar);
 
-	//print_r($obj->toArray());
+				if($hdr++ < 1){
+					$tab->addRow(array_merge("Action", 
+											 array_keys(
+												 $this->obj->toArray())), 
+								 "bgcolor=9999cc", "TH" );
+				}
+				$tab->addRow($ar);
 
-//	print_r ($obj);
-//oh, this so fuckign rules
+			}
+
 	
-}
+			//$this->linkStuff();
 
-/// DOSDVHACK
-$obj->limit();
-$obj->find();
-$dosdv = $build->getDataObjectSelectDisplayValue(&$obj);
-print "DOSDV $dosdv\n";
+				
+			$res .= sprintf(
+				'<p><a href="%s?action=detail&table=%s">Add new</a></p>', 
+				   $_SERVER['PHP_SELF'], $table) ;
+
+			$tab->altRowAttributes(1, "bgcolor=#CCCCC", "bgcolor=white");
+			$res .= $tab->toHTML();
+
+//			$res .= "<hr><br>";
+
+		
+			return $res;
+		}
+
+} // END CLASS
 
 
-/// DRAW THE LIST
-printf('<p><a href="%s?action=detail&table=%s">Add new</a></p>', 
-	   $_SERVER['PHP_SELF'], $table) ;
+//MAIN
+//$_SESSION['table'] = 'income';
 
-$tab->altRowAttributes(1, "bgcolor=#CCCCC", "bgcolor=white");
-$tab->display();
-
-print "<hr><br>";
-
-//confessArray($pager_links, "pagerlinks");
-print $pager_links['first'];
-print $pager_links['all'];
-print $pager_links['last'];
-
+$cp =& new coopPage('income');
+print $cp->listTable();
 done();
 
 
