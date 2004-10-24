c90b382c813408165db68d699f73be6e7f0abd2a
ken restivo <ken@restivo.org>
2004-10-24 09:32:32 +0000

    well it works. but it's way out of sync with livesite now!
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@679 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/companies-to-leads.scm
+++ scripts/companies-to-leads.scm
@@ -23,9 +23,10 @@
 	("do_not_contact" . "do_not_contact")
 	("company_id" . "company_id")))
 
-(define old-cols
-  '(
-	)
+(define redundant-old-cols
+  '("address1" "address2" "city" "state" "zip"
+	"country" "phone" "fax" "email_address"  "do_not_contact" ))
+	
   
 
 (define (replace-new dbh cols new-table old-table)
@@ -39,14 +40,19 @@
 					  (map (lambda (x) (car x)) cols) ", ")
 					 old-table)))
 
-
+(define (remove-old dbh redundant-cols old-table)
+  (for-each (lambda (x)
+			  (safe-sql dbh
+						(sprintf #f "alter table %s drop column %s"
+								 old-table x)))
+			redundant-cols))
 
 (define (do-companies-leads)
   (let ((dbh
 		 (apply simplesql-open "mysql"
 				(read-conf "/mnt/kens/ki/proj/coop/sql/db-input.conf"))))
 	(replace-new dbh table-mapping "leads" "companies"  )	
-; 	(remove-old dbh old-cols)			
+ 	(remove-old dbh redundant-old-cols "companies")			
 	(simplesql-close *dbh*)))
 
 
