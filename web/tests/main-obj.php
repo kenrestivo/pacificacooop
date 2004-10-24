1aba9daf8d5dcab3dc3a5051a7785951a3575f8a
ken restivo <ken@restivo.org>
2004-10-24 19:30:28 +0000

    first post
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@685 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ web/tests/main-obj.php
@@ -0,0 +1,23 @@
+<?php 
+
+//$Id$
+
+require_once('CoopPage.php');
+
+//MAIN
+//$_SESSION['toptable'] 
+
+
+$cp =& new coopPage();
+$cp->pageTop();
+$cp->engine();
+
+done();
+
+
+////KEEP EVERTHANG BELOW
+
+?>
+<!-- END COOP PAGE -->
+
+
