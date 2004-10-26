e2237bb5cdfb52a1242aa1c11abe97241950d7e2
ken restivo <ken@restivo.org>
2004-10-26 08:12:42 +0000

    wacky menu stuff
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@691 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/tests/paypal-test.php
+++ web/tests/paypal-test.php
@@ -1,46 +1,113 @@
 <?php 
 
 //$Id$
+require_once("first.inc");
+require_once("shared.inc");
+require_once("auth.inc");
 
-require_once('DB/DataObject.php');
-require_once('DB/DataObject/FormBuilder.php');
+require_once 'HTML/Menu.php';
+require_once 'HTML/Menu/DirectTreeRenderer.php';
 
-require_once('object-config.php');
 
-$obj =& DB_DataObject::factory ('families_income_join'); // & instead?
-if (PEAR::isError($obj)){
-	die ($obj->getMessage ());
+function indexEverything($everything){
+	foreach ($everything as $thang => $val){
+		$val['fields'] = $$val['fields'];
+		$indexed_everything[$val['page']] = $val;
+	
+	}
+	//confessArray($indexed_everything, 'indexedeverythinag');
+	return $indexed_everything;
+} 
+
+function callbacksToMenu($everything){
+	foreach($everything as $key => $cbs){
+		$menustruct[] = array(
+			'title' => $cbs['description'],
+			'url' => $cbs['page']);
+	}
+	//confessArray($menustruct, 'menustruct');
+	return $menustruct; 
 }
 
-//print_r($this);
-$obj->get(10);
-$build =& DB_DataObject_FormBuilder::create ($obj);
-$form =& $build->getForm();
-$form->freeze();
-if($form->validate ()){
-	$res = $form->process (array 
-						   (&$build, 'processForm'), 
-						   false);
-	if ($res){
-		$obj->debug('processed successfully', 
-						  'detailform', 0);
-		header('Location: %s', $_SERVER['PHP_SELF']);
+function getMoney($ie){
+	foreach($ie as $page => $cbs){
+		if($cbs['maintable'] == 'income'){
+			$moneymenu[] = array(
+				'title' => $cbs['description'],
+				'url' => $cbs['page']);
+		}
+		confessArray($moneymenu, 'moneymenu');
+		return $moneymenu;
 	}
 }
 
-$form->display();
+
+///////////////////////
+print '<HTML>
+				<HEAD>
+						<link rel=stylesheet href="main.css" title=main>
+							<TITLE>Data Entry</TITLE>
+				</HEAD>
+				<BODY>
+				<h2>Pacifica Co-Op Nursery School Data Entry</h2>
+				';
+
+			
+///
+warnDev();
+
+user_error("states.inc: ------- NEW PAGE --------", E_USER_NOTICE);
+
+
+$this->auth = logIn($_REQUEST);
+
+
+if($this->auth['state'] != 'loggedin'){
+	done();
+}
+
+
+topNavigation($this->auth,  getUser($this->auth['uid']));
+
+
+
+include('everything.inc');
+$sf = indexEverything($everything);
+include('members.inc');
+$members = indexEverything($everything);
+
+
+$heirmenu = array(
+	array(
+		'title' => 'Enhancement',
+		'sub' => callbacksToMenu($members)),
+	array(
+		'title' => 'Springfest',
+		'sub' => callbacksToMenu($sf)));
 	
-print"<pre>";
-print "======== BUILDER ============\n";
-print htmlentities(print_r($build, 1));
-print "======== FORM ============\n";
-print htmlentities(print_r($form, 1));
-print "</pre>";
+//confessArray($sf, "sf");
+
+$menu =& new HTML_Menu($heirmenu, "sitemap");
+$renderer =& new HTML_Menu_DirectTreeRenderer();
+$menu->render($renderer, $type);
+print $renderer->toHtml();
+
+
+	
+//$menu->setMenuType('urhere');
+//$menu->show();
+
+
+
+done();
+
+
+
 
 
 ////KEEP EVERTHANG BELOW
 
 ?>
-<!-- END TRASH -->
+<!-- END TEST -->
 
 
