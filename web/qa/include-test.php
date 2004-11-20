000fe3c125e239a439a25c2d8308ee3c7021e256
ken restivo <ken@restivo.org>
2004-11-20 08:06:47 +0000

    first post
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@768 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ web/qa/include-test.php
@@ -0,0 +1,20 @@
+<?php
+
+// unit test for my vitally required includes. return an error if
+// the shit has hit the fan
+
+require_once "HTML/QuickForm.php";
+require_once "HTML/QuickForm/group.php";
+require_once "HTML/QuickForm/group.php";
+require_once "HTML/Table.php";
+require_once 'HTML/Menu/DirectRenderer.php';
+require_once 'HTML/Menu/DirectTreeRenderer.php';
+require_once("DB/DataObject.php");
+require_once("HTML/Table.php");
+require_once('DB/DataObject.php');
+require_once('DB/DataObject/FormBuilder.php');
+require_once('Pager/Pager.php');
+
+print "OK";
+
+?>
