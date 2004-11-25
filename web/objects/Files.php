7db40ef4c642a71e12b62fc0b869b782939dac4b
ken restivo <ken@restivo.org>
2004-11-25 04:05:23 +0000

    ok well it uploads at least
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@778 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/objects/Files.php
+++ web/objects/Files.php
@@ -12,6 +12,8 @@ class Files extends DB_DataObject
     var $__table = 'files';                           // table name
     var $file_id;                         // int(32)  not_null primary_key unique_key auto_increment
     var $file_description;                // string(255)  
+    var $original_filename;               // string(255)  
+    var $disk_filename;                   // string(255)  
     var $school_year;                     // string(50)  
     var $file_date;                       // date(10)  
     var $upload_date;                     // datetime(19)  
@@ -27,33 +29,52 @@ class Files extends DB_DataObject
     ###END_AUTOCODE
 
 
-	function postGenerateForm(&$uploadForm)
+	var	$kenPath = "../../files/"; // NOTE! for the tests folder
+
+	function preGenerateForm()
 		{
-			$uploadForm->setMaxFileSize(8388608); // 8MB s/b as big as i need
-			$this->kenFile =& $uploadForm->addElement('file', 'original_filename', 'File:');
-			$uploadForm->addRule('original_filename', 'You must select a file', 'uploadedfile');
+
+			$this->fb_preDefElements['original_filename'] =& 
+				HTML_QuickForm::createElement('file', 
+											   'original_filename', 
+											   'File to upload:',
+											  'maxfilesize=8388608');
+                                        // 8MB s/b as big as i need
+			// $uploadForm->addRule('original_filename', 
+//                                  'You must select a file', 'uploadedfile');
 		}
 
-	function preProcessForm(&$formValues)
+	function insert()
 		{
-			$path = "../../files"; // NOTE! for the tests folder
-			$fileValues =& $this->kenFile->getValue();
-		
-			print_r_html($this->kenFile);
-			print_r_html($fileValues);
-		
+
+			$actual =& $_FILES['original_filename'];			
+			confessObject($actual, "actual_file");
+			$unique_filename = sprintf("%d-%s", rand(1,200), 
+                                       $actual['name']);
+            // TODO also check for a size < 1... zero-byte file. or, js?
+            if (is_uploaded_file($actual['tmp_name'])) {
+				if(move_uploaded_file($actual['tmp_name'],
+                                      $this->kenPath . $unique_filename)){
+					//TODO add mimetype
+					print "file uploaded!";
+					// TODO: add the original_filename['name']
+					// unique_file and to the db's field
+					return parent::insert();
+				}
+			}
 			
-			$unique_filename = sprintf("%d-%s", rand(1,200), $fileValues['name']);
-			if ($this->kenFile->isUploadedFile()) {
-				$this->kenFile->moveUploadedFile($path, $unique_filename);
-				$this->mime_type = $fileValues['type'];
-				print_r_html($this);
-				print "file uploaded!";
-				// TODO: add the original_filename['name'] unique_file and to the db's field
+			return false;  // uh oh. boo boo.
+		} // end insert
+
+		function delete()
+		{
+			$result = parent::delete();
+                               
+			if ($result == false) {
+				unlink($this->kenPath . $this->unique_filename);
 			}
-			else {
-				print "No file uploaded";
-			}
-		} // end preprocess
+                               
+			return $result;
+		}
 	
 } 
