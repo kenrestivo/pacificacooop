3f6a69e4216322c60396c2d4128715a11b734df3
ken restivo <ken@restivo.org>
2004-11-26 03:00:33 +0000

    much simpler. use chdir instead of ../ hacks
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@784 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/tests/frontend-test.php
+++ web/tests/frontend-test.php
@@ -1,31 +1,14 @@
 <?php
 //$Id$
-define('DB_DATAOBJECT_NO_OVERELOAD', true);
+define('DB_DATAOBJECT_NO_OVERLOAD', true);
 require_once('DB/DataObject.php');
-$config = parse_ini_file('../coop-dbobj.ini', true);
-foreach($config as $class => $values) {
-    $options = &PEAR::getStaticProperty($class, 'options');
-    $options = $values;
+require_once('DB/DataObject/FormBuilder/Frontend.php');
 
-}
-// HACK for "tests" directory. remove this.
-$options = &PEAR::getStaticProperty('DB_DataObject','options');
-$options['schema_location'] = "../" . $options['schema_location'];
-$options['class_location'] = "../" . $options['class_location'];
-print_r_html($config);
-$_DB_DATAOBJECT_FORMBUILDER['CONFIG'] = $config['DB_DataObject_FormBuilder'];
+chdir("../");                   // XXX only for "test" dir hack!
+require_once("object-config.php");
 
-function print_r_html($val, $return = false) {
-    $ret = '<pre>
-'.htmlentities(print_r($val, true)).'
-</pre>';
-    if($return) {
-        return $ret;
-    }
-    echo $ret;
-}
-include('DB/DataObject/FormBuilder/Frontend.php');
 
 $frontend = new DB_DataObject_FormBuilder_Frontend();
 $frontend->display();
+
 ?>
