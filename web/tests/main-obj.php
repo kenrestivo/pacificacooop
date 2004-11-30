74bbb90065fd15cd46172e0009fdb528a381deb4
ken restivo <ken@restivo.org>
2004-11-30 17:49:28 +0000

    nasty note on calls. also, begin the report screen.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@796 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/tests/main-obj.php
+++ web/tests/main-obj.php
@@ -15,34 +15,6 @@ $debug = 1;
 
 $cp =& new coopPage(1);
 $cp->pageTop();
-$menu =& new CoopMenu;
-
-
-$menu->page =& $cp;
-$menu->setMenu(
-	array(
-		array(
-			'title' => 'Solicitation Test',
-			'url' => $cp->selfURL(
-				false, 'tables[companies][action]=list')),
-		array(
-			'title' => 'New Invitations Test',
-			'url' => $cp->selfURL(
-				false, 'tables[invitations][action]=list')),
-		array(
-			'title' => 'Files Test',
-			'url' => $cp->selfURL(
-				false, 'tables[files][action]=list')),
-		array(
-			'title' => 'Leads Test',
-			'url' => $cp->selfURL(
-				false, 'tables[leads][action]=list'))));
-
-
-
-print $menu->topNavigation();
-print $menu->kenRender();
-$cp->engine();
 
 //global $_DB_DATAOBJECT;
 //confessObj($_DB_DATAOBJECT, "dataobject");
