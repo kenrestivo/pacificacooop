450622445965e7783dd0ec0e079b1ab0f1ccbc2d
ken restivo <ken@restivo.org>
2004-12-09 23:35:23 +0000

    add in-kind to objects too
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@840 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ web/objects/In_kind_donations.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Table Definition for in_kind_donations
+ */
+require_once 'DB/DataObject.php';
+
+class In_kind_donations extends DB_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    var $__table = 'in_kind_donations';               // table name
+    var $in_kind_donation_id;             // int(32)  not_null primary_key unique_key auto_increment
+    var $item_description;                // blob(16777215)  blob
+    var $quantity;                        // int(5)  
+    var $item_value;                      // real(11)  
+    var $date_received;                   // date(10)  
+    var $school_year;                     // string(50)  
+
+    /* ZE2 compatibility trick*/
+    function __clone() { return $this;}
+
+    /* Static get */
+    function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('In_kind_donations',$k,$v); }
+
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+}
