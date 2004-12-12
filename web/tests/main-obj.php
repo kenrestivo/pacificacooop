fec73a12688ace69dedad19573564726aaa4e556
ken restivo <ken@restivo.org>
2004-12-12 04:12:05 +0000

    crapcan the damn pager, which is broken anyway. start nag details. fix joins in solicit_inkind.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@842 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/tests/main-obj.php
+++ web/tests/main-obj.php
@@ -17,40 +17,25 @@ $debug = 0;
 $page = new coopPage( $debug);
 $page->pageTop();
 
+$mi= 'thank_you_id';
+$cid = 1;
 
-if(0){
- foreach(array("families", "leads") as $table){
- 	$view = new CoopView(&$page, $table);
- 		// XXX hack! use the get() form instead if you know index
- 		//$pk = $view->getPK(); 
- 	$u = getUser($page->auth['uid']);
- 	$view->obj->family_id = $u['family_id'];
- 	print $view->simpleTable();
- }
+$top = new CoopView(&$cp, 'thank_you');
+//print "CHECKING $table<br>";
+$top->obj->$mi = $cid;
+$summary = $top->getSummary();
+print $top->simpleTable($summary);
 
-} else {
-	$cid = 114;
-	foreach(array("companies", 'flyer_deliveries') as $table){
-		$view = new CoopView(&$page, $table);
-		$view->obj->get($cid);
-		print $view->simpleTable();
-	}
-
-	$co =& new CoopObject(&$page, 'companies_income_join');
-	$co->obj->company_id = $cid;
-	$real =& new CoopView(&$page, 'income');
-	$real->obj->joinadd($co->obj);
-	print $real->simpleTable();
+// XXX this just SXCREAMS for a refactoring. a repetitive function.
+// linktable, destinationtable, mainindex, and id?
+$co =& new CoopObject(&$cp, 'companies_income_join');
+$co->obj->$mi = $cid;
+$real =& new CoopView(&$cp, 'income');
+$real->obj->joinadd($co->obj);
+$real->parentSummary = $summary;
+print $real->simpleTable($summary);
 	
 
-	$co =& new CoopObject(&$page, 'companies_auction_join');
-	$co->obj->company_id = $cid;
-	$real =& new CoopView(&$page, 'auction_donation_items');
-	$real->obj->joinadd($co->obj);
-	print $real->simpleTable();
-	
-//'companies_income_join', 'companies_auction_join', 
-}
 
 done ();
 
