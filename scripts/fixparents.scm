a64b79c2e79ab229fd43cc97cdab4e4f2e20e25d
ken restivo <ken@restivo.org>
2004-12-19 04:29:23 +0000

    damn, guile is amazing. it makes sql usable
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@865 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ scripts/fixparents.scm
@@ -0,0 +1,43 @@
+;;; $Id$
+;; put the parentid in for all the solicitation auction and money
+;; grab the family name from the income stuff.
+;; (load "/mnt/kens/ki/proj/coop/scripts/fixparents.scm")
+
+(use-modules (ice-9 slib)
+			 (kenlib)
+			 (database simplesql))
+(require 'printf)
+
+
+(define *dbh* (apply simplesql-open "mysql"
+					 (read-conf "/mnt/kens/ki/proj/coop/sql/db-input.conf")))
+
+
+(for-each
+ (lambda (table)
+   (for-each 
+	(lambda (record)
+	  (safe-sql *dbh*
+				(sprintf #f "update %s set parent_id = %d where family_id = %d"
+						 table
+						 (vector-ref record 0)
+						 (vector-ref record 1)
+						 )))
+	
+	(cdr (simplesql-query *dbh*
+						  (sprintf #f
+		    "select parents.parent_id, parents.family_id
+				from %s
+					left join parents
+ 	  				on parents.family_id = %s.family_id
+				and parents.worker = 'Yes'
+			group by parents.family_id
+			order by parents.family_id"
+			table table)))))
+ '("companies_auction_join"
+   "companies_income_join"
+   "companies_in_kind_join"))
+
+(simplesql-close *dbh*)
+
+;;; EOF
