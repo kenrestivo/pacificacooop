1f107d238fb51e7c69011b6a9d7167bff01a2dad
ken restivo <ken@restivo.org>
2004-12-31 23:51:29 +0000

    yay, test program. me likes this
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@931 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ web/tests/mail-test.php
@@ -0,0 +1,29 @@
+<?php
+
+//$Id$
+
+// unit test for my vitally required includes. return an error if
+// the shit has hit the fan
+
+require_once('Mail.php');
+
+PEAR::setErrorHandling(PEAR_ERROR_PRINT);
+
+$recipients = 'ken@restivo.org';
+
+$headers['From']    = 'Test User <test@pacificacoop.org>';
+$headers['To']      = 'ken@restivo.org';
+$headers['Subject'] = 'Test message';
+
+$body = 'Test message. this be a test. i am testing. 1 2 3. check 1,2. check.';
+
+
+// Create the mail object using the Mail::factory method
+$mail_object =& Mail::factory('smtp', $params);
+
+$mail_object->send($recipients, $headers, $body);
+
+
+print "OK";
+
+?>
