06d07ce9a5865a42473f6095bea13d5a5c386e68
ken restivo <ken@restivo.org>
2005-01-02 16:43:01 +0000

    link update.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@941 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ web/objects/Thank_you.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Table Definition for thank_you
+ */
+require_once 'DB/DataObject.php';
+
+class Thank_you extends DB_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    var $__table = 'thank_you';                       // table name
+    var $thank_you_id;                    // int(32)  not_null primary_key unique_key auto_increment
+    var $date_sent;                       // date(10)  
+    var $family_id;                       // int(32)  
+    var $method;                          // string(7)  enum
+
+    /* ZE2 compatibility trick*/
+    function __clone() { return $this;}
+
+    /* Static get */
+    function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('Thank_you',$k,$v); }
+
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+
+	var $fb_linkDisplayFields = array('date_sent', 'method');
+	var $fb_fieldLabels = array (
+		'thank_you_id' => 'Thank You Note',
+		'date_sent' => 'Date Sent',
+		'method' => 'Sent Via',
+		'family_id' => 'Printed/Sent By'
+		);
+
+}
