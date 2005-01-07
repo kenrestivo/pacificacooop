7ff89779987675220e05d6a248dfbcb2a17e6980
ken restivo <ken@restivo.org>
2005-01-07 10:45:34 +0000

    duh2
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@954 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/pages/print_popup.php
+++ web/pages/print_popup.php
@@ -19,6 +19,7 @@
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
+require_once("first.inc");
 require_once("auth.inc");
 require_once("shared.inc");
 require_once("CoopPage.php");
