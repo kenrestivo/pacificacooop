6ba158dcf6891c9a0727787b1fc9efcb974ab314
ken restivo <ken@restivo.org>
2005-01-23 22:08:43 +0000

    busted out showuser so's i can use it elsewhere.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1039 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/tests/sane-donate.php
+++ web/tests/sane-donate.php
@@ -13,7 +13,37 @@ PEAR::setErrorHandling(PEAR_ERROR_PRINT);
 
 $debug = 1;
 
+function showUser(&$cp, $leadid)
+{
+		$top = new CoopView(&$cp, 'leads', &$nothing);
+		$mi = $top->pk;
+		$top->obj->$mi = $leadid;
+		$found = $top->obj->find(true);
+		if(!$found){
+			return false;
+		}
+		// ripped from thankyou, mostly. should abstract it out!
+		$address_array[] = implode(' ', array($top->obj->salutation,
+										   $top->obj->first_name,
+										   $top->obj->last_name
+									));
 
+		// note: it's company in leads, company_name in companies. fock.
+		foreach(array('title', 'company', 'address1', 'address2') 
+				as $var)
+		{
+			if($top->obj->$var){
+				$address_array[] = $top->obj->$var;
+			}
+		}
+		$address_array[] = sprintf("%s %s, %s", 
+										 $top->obj->city,
+										 $top->obj->state,
+										 $top->obj->zip);		
+		$address = implode('<br>', $address_array);
+
+		return $address;
+}
 
 
 function donateDispatcher(&$cp, $action = false)
@@ -54,41 +84,14 @@ function donateDispatcher(&$cp, $action = false)
 		break;
 		
 	case 'verifyuser':
-		$top = new CoopView(&$cp, 'leads', &$nothing);
-		$mi = $top->pk;
-		$top->obj->$mi = $_REQUEST['response_code'];;
-		$found = $top->obj->find(true);
-		if(!$found){
+		$address = showUser(&$cp, $_REQUEST['response_code']);
+		if(!$address){
 			printf("<p>I'm sorry, there is no such code as %d. You may have mistyped or the code on your invitation may not be legible.</p>",
 				   $_REQUEST['response_code']);
-			// nice idea, but fux0red
-			//unset($_SESSION['response_code']);
-			//unset($_REQUEST['response_code']);
-			//donateDispatcher(&$cp, 'getcode');
  			print $cp->selfURL("Please try again.", 
- 							   array($_REQUEST['response_code'],
- 									 'action' => 'getcode'));
+ 							   array('action' => 'getcode'));
 			break;
 		}
-		// ripped from thankyou, mostly. should abstract it out!
-		$address_array[] = implode(' ', array($top->obj->salutation,
-										   $top->obj->first_name,
-										   $top->obj->last_name
-									));
-
-		// note: it's company in leads, company_name in companies. fock.
-		foreach(array('title', 'company', 'address1', 'address2') 
-				as $var)
-		{
-			if($top->obj->$var){
-				$address_array[] = $top->obj->$var;
-			}
-		}
-		$address_array[] = sprintf("%s %s, %s", 
-										 $top->obj->city,
-										 $top->obj->state,
-										 $top->obj->zip);		
-		$address = implode('<br>', $address_array);
 		print "<h3>Is this you?</h3><p>$address</p>";
 		//print $top->horizTable();
 		print $cp->selfURL("Yes, that's me!", 
@@ -122,6 +125,7 @@ function donateDispatcher(&$cp, $action = false)
 		break;
 		
 	default:
+		print "HEY DEFAULT";
 		donateDispatcher(&$cp, 'getcode');
 		break;
 	}
