945fa210cdc8cf18be4f77c998c74d5c2ee6b66b
ken restivo <ken@restivo.org>
2005-01-24 03:23:27 +0000

    wow. rudimentary object dispatcher seems to be a'runnin'.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1044 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/tests/sane-donate.php
+++ web/tests/sane-donate.php
@@ -4,9 +4,7 @@
 
 chdir("../");                   // XXX only for "test" dir hack!
 require_once('CoopPage.php');
-require_once('CoopView.php');
-require_once('paypal.php');
-
+require_once('RSVPWizard.php');
 
 
 PEAR::setErrorHandling(PEAR_ERROR_PRINT);
@@ -14,159 +12,20 @@ PEAR::setErrorHandling(PEAR_ERROR_PRINT);
 $debug = 1;
 
 
-function showUser(&$cp, $leadid)
-{
-		$top = new CoopView(&$cp, 'leads', &$nothing);
-		$mi = $top->pk;
-		$top->obj->$mi = $leadid;
-		$found = $top->obj->find(true);
-		if(!$found){
-			return false;
-		}
-		// ripped from thankyou, mostly. should abstract it out!
-		$address_array[] = implode(' ', array($top->obj->salutation,
-										   $top->obj->first_name,
-										   $top->obj->last_name
-									));
-
-		// note: it's company in leads, company_name in companies. fock.
-		foreach(array('title', 'company', 'address1', 'address2') 
-				as $var)
-		{
-			if($top->obj->$var){
-				$address_array[] = $top->obj->$var;
-			}
-		}
-		$address_array[] = sprintf("%s %s, %s", 
-										 $top->obj->city,
-										 $top->obj->state,
-										 $top->obj->zip);		
-		$address = implode('<br>', $address_array);
-
-		return $address;
-}
-
-function getCodeForm(&$cp)
-{
-	$form =& new HTML_QuickForm( 'Springfest RSVP', 'getcodeform');
-	$form->addElement('text', 'response_code', 
-					  'Please enter your Response Code here:', 
-					  'size="4"');
-	$form->addElement('submit', 'verifyuser', 'Next>>');
-	
-	// important
-	if(SID){
-		$form->addElement('hidden', 'coop', session_id()); 
-	}
-	
-	$form->applyFilter('__ALL__', 'trim');
-	$form->addRule('response_code', 
-				   'Response code is required.', 
-				   'required', 'client');
-	$form->addRule('response_code', 
-				   'Response codes are all numbers, no letters or spaces.', 
-				   'numeric', 'client');
-	
-	// add custom rule: check db!
-	if($form->validate()){
-		donateDispatcher(&$cp, 'verifyuser');
-	} else {
-		print $form->toHTML();
-		}
-	
-}
-
-function donateDispatcher(&$cp, $action = false)
-{
-// there are other actions but thay happen in IPN and postpaypal
-	$actionsequence = array('getcode', 
-							'verifyuser',
-							'newrsvp',
-							'confirmrsvp');
-
-	switch($action){
-	case 'getcode':
-		getCodeForm(&$cp);
-		break;
-		
-	case 'verifyuser':
-		$address = showUser(&$cp, $_REQUEST['response_code']);
-		if(!$address){
-			printf("<p>I'm sorry, there is no such code as %d. You may have mistyped or the code on your invitation may not be legible.</p>",
-				   $_REQUEST['response_code']);
- 			print $cp->selfURL("Please try again.", 
- 							   array('action' => 'getcode'));
-			break;
-		}
-		print "<h3>Is this you?</h3><p>$address</p>";
-		//print $top->horizTable();
-		print $cp->selfURL("Yes, that's me!", 
-						   array('response_code' =>
-								 $_REQUEST['response_code'],
-								 'action' => 'newrsvp'));
-		print $cp->selfURL("No, that's not me.", 
-						   array($_REQUEST['response_code'],
-								 'action' => 'getcode'));
-		break;
-		
-	case 'newrsvp':
-		print showUser(&$cp, $_REQUEST['response_code']);
-		$ppf =& new PayPalForm();
-		$form = $ppf->buildRSVP(&$cp, $_REQUEST['response_code']);
-		// make sure dispatcher brings me back here for validation. duh.
-		$form->addElement('hidden', 'action', 'newrsvp');
-		//confessObj($form, 'form');
-		if($form->validate()){
-			//$form->freeze();
-			// change action here to confirmrsvp? or process?
-			//print $form->toHTML();
-			donateDispatcher(&$cp, 'confirmrsvp');
-		} else {
-			print $form->toHTML();
-		}
-		break;
-		
-	case 'confirmrsvp':
-		$tq = $_REQUEST['ticket_quantity'];
-		$amt = $_REQUEST['other_amount'];
-		$ticket_price = 25;
-		
-		// force the ticket price in there. hack hack hack!
-		$total = $amt < $tq * $ticket_price ? $amt + ($ticket_price * $tq) : $amt;
-
-		printf("<p>Click 'Next' to donate $%.02f", $total);
-		if($tq){
-			printf(" which includes reservations for %d persons for the event", 
-				   $_REQUEST['ticket_quantity']);
-		}
-		print ". Or click 'Previous' to edit your order.</p>";
-		// the setup to call the proper paypal form
-		// something must set SOURCE
-		// the edit will be a FORM with all hidden except EDIT button
-		//it'll bounce back to newrsvp!
-
-		break;
-		
-	default:
-		// XXX WATCH OUT! i have a fallthrough bug in verifyuser
-		//print "HEY DEFAULT";
-		donateDispatcher(&$cp, 'getcode');
-		break;
-	}
-} // end donatedispatcher
-
-
 $cp = new coopPage( $debug);
 
 // make it leagal html, and test it
 $cp->header();
 
-$_SESSION=$_REQUEST; 			// keep sessioninit happy
+
 
 print $cp->selfURL('Refresh');
 
 
-donateDispatcher(&$cp, $_REQUEST['action']);
+$wiz =& new RSVPWizard(&$cp);
+$wiz->run();
+
+$_SESSION=$_REQUEST; 			// keep sessioninit happy
 
 done ();
 
