4cd866826b36283c8e6ea227508238a0775e3400
ken restivo <ken@restivo.org>
2005-03-17 08:52:27 +0000

    runs, but reeeeeeeeeeally long
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1269 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/old/find-dupes.php
+++ web/old/find-dupes.php
@@ -28,12 +28,17 @@ $co = new CoopObject(&$cp, 'companies', &$dop);
 $co->obj->find();
 while($co->obj->fetch()){
 	//$co->obj->debugLevel(2);
+
+	// soundex search, using mysql to help me
 	$sub = new CoopObject(&$cp, 'companies', &$dop);
-	$sub->obj->whereAdd(sprintf('soundex(company_name) = soundex("%s") and %s != %d',
+	$sub->obj->whereAdd(sprintf('soundex(company_name) = soundex("%s") 
+								and %s != %d',
 								$co->obj->escape($co->obj->company_name), 
 								$co->pk, $co->obj->{$co->pk}));
-	if($sub->obj->find() && !in_array($co->obj->{$co->pk}, $dupefound)){
-		printf("<br>hey %s dupes", $co->obj->company_name);
+	if($sub->obj->find() && 
+	   (is_array($dupefound) && !in_array($co->obj->{$co->pk}, $dupefound)))
+	{
+		printf("<br>hey %s dupes ", $co->obj->company_name);
 		while($sub->obj->fetch()){
 			$dupefound[] = $sub->obj->{$sub->pk};
 			printf("are %s", $sub->obj->company_name);
@@ -41,6 +46,21 @@ while($co->obj->fetch()){
 		}
 	}
 	
+	/// similar-text search
+	$sub = new CoopObject(&$cp, 'companies', &$dop);
+	$sub->obj->whereAdd(sprintf(' %s != %d', $co->pk, $co->obj->{$co->pk}));
+	$sub->obj->find();
+	while($sub->obj->fetch())
+	{
+		$perc = 0;
+		similar_text($co->obj->company_name, $sub->obj->company_name, &$perc);
+		if($perc > 80){
+			//$dupefound[] = $sub->obj->{$sub->pk};
+			printf("<br> hey %s dupes %s?", 
+				   $co->obj->company_name, $sub->obj->company_name);
+		}
+
+	}
 	
 }
 
