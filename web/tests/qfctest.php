9df3e961a8fbc91353432aff138d740a536e4916
ken restivo <ken@restivo.org>
2005-05-21 20:09:06 +0000

    yay, working junmp
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1334 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/tests/qfctest.php
+++ web/tests/qfctest.php
@@ -9,7 +9,7 @@ require_once('CoopForm.php');
 require_once('CoopMenu.php');
 require_once 'HTML/QuickForm/Controller.php';
 require_once 'HTML/QuickForm/Action.php';
-
+require_once 'HTML/QuickForm/Action/Jump.php';
 
 
 PEAR::setErrorHandling(PEAR_ERROR_PRINT);
@@ -47,6 +47,38 @@ function viewHack(&$cp, &$atd)
 }
 
 
+class JumpSucks extends HTML_QuickForm_Action_Jump
+{
+	function perform(&$page, $actionName)
+		{
+			// ok, this is a cut-and-paste from action_jump,
+			// but does display not location
+
+			//print "jumpy jump [$actionName]";
+			//confessObj($page, 'the page receiving the jump');
+
+			// check whether the page is valid before trying to go to it
+			if ($page->controller->isModal()) {
+				// we check whether *all* pages up to current are valid
+				// if there is an invalid page we go to it, instead of the
+				// requested one
+				$pageName = $page->getAttribute('id');
+				if (!$page->controller->isValid($pageName)) {
+					$pageName = $page->controller->findInvalid();
+				}
+				$current =& $page->controller->getPage($pageName);
+
+			} else {
+				$current =& $page;
+			}
+
+			// ok, now call display on  $current!
+			$current->handle('display');
+		}
+
+}
+
+
 // cheap dispatcher
 //confessArray($_REQUEST,'req');
 switch($_REQUEST['action']){
@@ -54,6 +86,38 @@ switch($_REQUEST['action']){
  case 'new':
  case 'edit':
 	 
+	 class SecondPage extends HTML_QuickForm_Page
+	 {
+		 function buildForm()
+			 {
+				 $this->_formBuilt = true;
+				 
+
+				 $this->addElement('header',     null, 
+								   'Controller example 2: a simple form');
+				 $this->addElement('text',       'moreText', 
+								   'Please enter MORE:', 
+								   array('size'=>20, 'maxlength'=>50));
+				 
+				 $this->addElement('submit',     $this->getButtonName('next'), 
+								   'Send');
+				 
+				 $this->applyFilter('moreText', 'trim');
+				 $this->addRule('moreText', 'Pretty please!', 'required');
+
+				 // XXX only for simple with no coopform! build() does it.
+				 //confessObj($this->controller->cp, 'cp');
+ 				 if($sid = thruAuthCore($this->controller->cp->auth)){
+ 					 $this->addElement('hidden', 'coop', $sid); 
+ 				 }
+
+				 // still a sub-element of my cheap dispatcher!
+				 $this->addElement('hidden', 'action', 'edit'); 
+
+				 $this->setDefaultAction('next');
+			 }
+	 }
+
 	 class SimplePage extends HTML_QuickForm_Page
 	 {
 		 function buildForm()
@@ -68,7 +132,7 @@ switch($_REQUEST['action']){
 								   array('size'=>20, 'maxlength'=>50));
 				 // Bind the button to the 'submit' action
 				 $this->addElement('submit',     $this->getButtonName('next'), 
-								   'Send');
+								   'Next>>');
 				 
 				 $this->applyFilter('tstText', 'trim');
 				 $this->addRule('tstText', 'Pretty please!', 'required');
@@ -92,26 +156,24 @@ switch($_REQUEST['action']){
 		 function perform(&$page, $actionName)
 			 {
 				 echo "Submit successful!<br>\n<pre>\n";
-				 var_dump($page->exportValues());
+				 confessObj($page->controller, 'yay');
 				 echo "\n</pre>\n";
 			 }
 	 }
 
-	 $page =& new SimplePage('page1');
-
-// We actually add these handlers here for the sake of example
-// They can be automatically loaded and added by the controller
-//	 $page->addAction('display', new HTML_QuickForm_Action_Display());
-//	 $page->addAction('submit', new HTML_QuickForm_Action_Submit());
-
 
 	 $controller =& new HTML_QuickForm_Controller('simpleForm');
 	 $controller->cp =& $cp; // DO THIS FIRST!!
-	 $controller->addPage($page);
+
+	 $controller->addPage(new SimplePage('page1'));
+	 $controller->addPage(new SecondPage('page2'));
 
 
 	 // This is the action we should always define ourselves
 	 $controller->addAction('process', new ActionProcess());
+
+	 // self-explanatory
+	 $controller->addAction('jump', new JumpSucks());
 
 	 $controller->run();
 
