81ba525c2f074615fa6e30767dfffa2afed3cac4
ken restivo <ken@restivo.org>
2005-05-21 22:35:17 +0000

    first post
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1335 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ web/lib/qfc_custom.php
@@ -0,0 +1,43 @@
+
+   <?php
+
+   // $Id$
+
+require_once 'HTML/QuickForm/Action/Jump.php';
+
+class JumpDisplay  extends HTML_QuickForm_Action_Jump
+{
+	function perform(&$page, $actionName)
+		{
+			// ok, this is a cut-and-paste from action_jump,
+			// but does display not location
+
+			//print "jumpy jump [$actionName]";
+			//confessObj($page, 'the page receiving the jump');
+
+			// check whether the page is valid before trying to go to it
+			if ($page->controller->isModal()) {
+				// we check whether *all* pages up to current are valid
+				// if there is an invalid page we go to it, instead of the
+				// requested one
+				$pageName = $page->getAttribute('id');
+				if (!$page->controller->isValid($pageName)) {
+					$pageName = $page->controller->findInvalid();
+				}
+				$current =& $page->controller->getPage($pageName);
+
+			} else {
+				$current =& $page;
+			}
+
+			// ok, now call display on  $current!
+			$current->handle('display');
+		}
+
+}
+
+
+
+
+
+   ?>
