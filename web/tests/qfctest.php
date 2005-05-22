4544a1e66002da57086ab663e84ca4500d6a06b3
ken restivo <ken@restivo.org>
2005-05-22 07:10:59 +0000

    shuffled crap around
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1343 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/tests/qfctest.php
+++ web/tests/qfctest.php
@@ -10,16 +10,132 @@ require_once('CoopMenu.php');
 require_once('lib/qfc_custom.php');
 require_once('HTML/QuickForm/Controller.php');
 require_once('HTML/QuickForm/Action.php');
+require_once 'HTML/QuickForm/Action/Direct.php';
 
 
 PEAR::setErrorHandling(PEAR_ERROR_PRINT);
 
 //$debug = 3;
 
-//MAIN
-//$_SESSION['toptable'] 
 
-//DB_DataObject::debugLevel(2);
+	 
+class View extends HTML_QuickForm_Page
+{
+	function buildForm()
+		{
+			$this->_formBuilt = true;
+
+			$this->addElement('submit',     $this->getButtonName('next'), 
+							  'Add New');
+
+			$res = "<p>lots of nothing</p>";
+			
+			$this->addElement('static', 'viewstuff', false, $res); 
+
+
+			// XXX only for simple with no coopform! build does it.
+			//confessObj($this->controller->cp, 'cp');
+			if($sid = thruAuthCore($this->controller->cp->auth)){
+				$this->addElement('hidden', 'coop', $sid); 
+			}
+
+
+			$this->setDefaultAction('next');
+			//PEAR::raiseError('how did i get here?', 555);
+		}
+}
+
+	 
+class SimplePage extends HTML_QuickForm_Page
+{
+	function buildForm()
+		{
+			$this->_formBuilt = true;
+
+
+			$this->addElement('submit',   $this->getButtonName('view'), 
+				'View All');
+
+			$this->addElement('header',     null, 
+							  'Controller example 1: a simple form');
+			$this->addElement('text',       'tstText', 
+							  'Please enter something:', 
+							  array('size'=>20, 'maxlength'=>50));
+
+
+			$nav[] =& $this->createElement(
+				'submit',   $this->getButtonName('next'), 'Next >>');
+			$this->addGroup($nav, null, '', '&nbsp;', false);
+
+				 
+			$this->applyFilter('tstText', 'trim');
+			$this->addRule('tstText', 'Pretty please!', 'required');
+
+			// XXX only for simple with no coopform! build does it.
+			//confessObj($this->controller->cp, 'cp');
+			if($sid = thruAuthCore($this->controller->cp->auth)){
+				$this->addElement('hidden', 'coop', $sid); 
+			}
+
+			$this->setDefaultAction('next');
+			//PEAR::raiseError('how did i get here?', 555);
+		}
+}
+
+class SecondPage extends HTML_QuickForm_Page
+{
+	function buildForm()
+		{
+			$this->_formBuilt = true;
+				 
+			$this->addElement('header',     null, 
+							  'Controller example 2: a simple form');
+			$this->addElement('text',       'moreText', 
+							  'Please enter MORE:', 
+							  array('size'=>20, 'maxlength'=>50));
+				 
+
+			$nav[] =& $this->createElement(
+				'submit',   $this->getButtonName('back'), '<< Back');
+			$nav[] =& $this->createElement(
+				'submit',   $this->getButtonName('next'), 'Finish');
+			$this->addGroup($nav, null, '', '&nbsp;', false);
+				 
+			$this->applyFilter('moreText', 'trim');
+			$this->addRule('moreText', 'Pretty please!', 'required');
+
+			// XXX only for simple with no coopform! build() does it.
+			//confessObj($this->controller->cp, 'cp');
+			if($sid = thruAuthCore($this->controller->cp->auth)){
+				$this->addElement('hidden', 'coop', $sid); 
+			}
+
+			$this->setDefaultAction('next');
+		}
+}
+
+
+
+class ActionProcess extends HTML_QuickForm_Action
+{
+	function perform(&$page, $actionName)
+		{
+			print $page->controller->cp->flushBuffer();
+			echo "Submit successful!<br>\n<pre>\n";
+			confessObj($page->controller, 'yay');
+			echo "\n</pre>\n";
+				 
+			//clean up after yourself, and bring me back to top!
+			$page->controller->container(true);
+			$view =& $page->controller->getPage('view');
+
+			print $view->handle('display');
+		}
+
+}
+
+
+///////// MAIN
 
 $cp = new coopPage( $debug);
 $cp->buffer($cp->pageTop());
@@ -32,148 +148,28 @@ $cp->buffer($menu->topNavigation());
 $cp->buffer("<p>QFC test</p>");
 
 
-$cp->buffer($cp->selfURL('View'));
-$cp->buffer($cp->selfURL('Create New', array('action' => 'new')));
-				   
-$atd = new CoopView(&$cp, 'job_descriptions', $none);
-$atd->recordActions = array('edit' => "Edit",
-							'details' => 'Details'); 
-
-function viewHack(&$cp, &$atd)
-{
-	print 'nothing';
-	// return $atd->simpleTable();
-			
-}
-
-// XXX nasty!
-if($_REQUEST['action']){
-	$_SESSION['actionhack'] = $_REQUEST['action'];
-} else {
-	$_REQUEST['action'] =	$_SESSION['actionhack'];
-}
-
-// cheap dispatcher
-//confessArray($_REQUEST,'req');
-switch($_REQUEST['action']){
-//// EDIT AND NEW //////
- case 'new':
- case 'edit':
-	 
-	 class SimplePage extends HTML_QuickForm_Page
-	 {
-		 function buildForm()
-			 {
-				 $this->_formBuilt = true;
-
-				 $this->addElement('header',     null, 
-								   'Controller example 1: a simple form');
-				 $this->addElement('text',       'tstText', 
-								   'Please enter something:', 
-								   array('size'=>20, 'maxlength'=>50));
-				 // Bind the button to the 'submit' action
-				 $this->addElement('submit',     $this->getButtonName('next'), 
-								   'Next>>');
-				 
-				 $this->applyFilter('tstText', 'trim');
-				 $this->addRule('tstText', 'Pretty please!', 'required');
-
-				 // XXX only for simple with no coopform! build does it.
-				 //confessObj($this->controller->cp, 'cp');
- 				 if($sid = thruAuthCore($this->controller->cp->auth)){
- 					 $this->addElement('hidden', 'coop', $sid); 
- 				 }
-
-				 // still a sub-element of my cheap dispatcher!
-				 $this->addElement('hidden', 'action', 'edit'); 
-
-				 $this->setDefaultAction('next');
-				 //PEAR::raiseError('how did i get here?', 555);
-			 }
-	 }
-
-	 class SecondPage extends HTML_QuickForm_Page
-	 {
-		 function buildForm()
-			 {
-				 $this->_formBuilt = true;
-				 
-				 $this->addElement('header',     null, 
-								   'Controller example 2: a simple form');
-				 $this->addElement('text',       'moreText', 
-								   'Please enter MORE:', 
-								   array('size'=>20, 'maxlength'=>50));
-				 
-
-				 $nav[] =& $this->createElement(
-					 'submit',   $this->getButtonName('back'), '<< Back');
-				 $nav[] =& $this->createElement(
-					 'submit',   $this->getButtonName('next'), 'Finish');
-				 $this->addGroup($nav, null, '', '&nbsp;', false);
-				 
-				 $this->applyFilter('moreText', 'trim');
-				 $this->addRule('moreText', 'Pretty please!', 'required');
-
-				 // XXX only for simple with no coopform! build() does it.
-				 //confessObj($this->controller->cp, 'cp');
- 				 if($sid = thruAuthCore($this->controller->cp->auth)){
- 					 $this->addElement('hidden', 'coop', $sid); 
- 				 }
-
-				 // still a sub-element of my cheap dispatcher!
-				 $this->addElement('hidden', 'action', 'edit'); 
-
-				 $this->setDefaultAction('next');
-			 }
-	 }
+$controller =& new HTML_QuickForm_Controller('simpleForm');
+$controller->cp =& $cp; // DO THIS FIRST!!
 
 
-
-	 class ActionProcess extends HTML_QuickForm_Action
-	 {
-		 function perform(&$page, $actionName)
-			 {
-				 print $page->controller->cp->flushBuffer();
-				 echo "Submit successful!<br>\n<pre>\n";
-				 confessObj($page->controller, 'yay');
-				 echo "\n</pre>\n";
-				 
-				 //clean up after yourself!
-				 $page->controller->container(true);
-				 unset($_SESSION['actionhack']);
-			 }
-
-	 }
+$controller->addPage(new View ('view'));
+$controller->addPage(new SimplePage('page1'));
+$controller->addPage(new SecondPage('page2'));
 
 
-	 $controller =& new HTML_QuickForm_Controller('simpleForm');
-	 $controller->cp =& $cp; // DO THIS FIRST!!
+// This is the action we should always define ourselves
+$controller->addAction('process', new ActionProcess());
+
+// need this for fooey stuff
+$controller->addAction('view', new HTML_QuickForm_Action_Direct());
+
+// self-explanatory
+$controller->addAction('display', new CustomDisplay());
 
 
-	 $controller->addPage(new SimplePage('page1'));
-	 $controller->addPage(new SecondPage('page2'));
+$controller->run();
 
 
-	 // This is the action we should always define ourselves
-	 $controller->addAction('process', new ActionProcess());
-
-	 // self-explanatory
-	 $controller->addAction('display', new CustomDisplay());
-	 //$controller->addAction('jump', new JumpDisplay());
-
-	 $controller->run();
-
-	 break;
-
-//// DEFAULT (VIEW) //////
- default:
-	 print $cp->flushBuffer();
-	 print viewHack(&$cp, &$atd);
-	 break;
-}
-
-
-//$cp->confessArray($_SESSION, 'session at end of page', 3);
 done ();
 
 ////KEEP EVERTHANG BELOW
@@ -181,6 +177,6 @@ done ();
 ?>
 
 
-<!-- END JOBDESCRIPTIONS -->
+<!-- END QFC TEST -->
 
 
