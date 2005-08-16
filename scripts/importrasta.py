d1ee2e0d1e7b0b7c109017b45a54e75ac70151e5
ken restivo <ken@restivo.org>
2005-08-16 08:09:35 +0000

    damn damn
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1414 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/importrasta.py
+++ scripts/importrasta.py
@@ -8,8 +8,11 @@ import csv
 f=open("/mnt/kens/ki/proj/coop/imports/AMRoste05-06.csv" , "r")
 r=csv.reader(f,dialect='excel')
 
+##TODO: loop through looking for empty lines, followd by keys
+
+
 #ooh, i like python
-dict(zip(keys,[i.strip() for i in r.next()]))
+dict(zip(keys,[i.replace('*','').strip() for i in r.next()]))
 
 
 
