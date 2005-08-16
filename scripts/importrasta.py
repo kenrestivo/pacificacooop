9c240ba8dae546466f8d5f00f7ed482e2ee9e200
ken restivo <ken@restivo.org>
2005-08-16 09:15:18 +0000

    made it OOP-like. yay
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1417 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/importrasta.py
+++ scripts/importrasta.py
@@ -7,42 +7,50 @@ import csv
 import MySQLdb
 mdb=MySQLdb                             # loathe CapNames
 
+class RastaImport:
+    keys=[]
+    f=None
+    r=None
 
-#file open
-f=open("/mnt/kens/ki/proj/coop/imports/AMRoste05-06.csv" , "r")
-r=csv.reader(f,dialect='excel')
+    def setup(self):
+        #file open
+        self.f=open("/mnt/kens/ki/proj/coop/imports/AMRoste05-06.csv" , "r")
+        self.r=csv.reader(self.f,dialect='excel')
 
-#db open
-conn=mdb.connect(user='input', passwd='test', db='coop', 
-                host='bc', cursorclass=mdb.cursors.DictCursor)
-c=conn.cursor()
+        #db open
+        #conn=mdb.connect(user='input', passwd='test', db='coop', 
+         #           host='bc', cursorclass=mdb.cursors.DictCursor) 
+        #c=conn.cursor()
 
-def cleanInput(x):
-     return x.replace('*','').strip()
+    def cleanInput(self,x):
+         return x.replace('*','').strip()
 
+    def getKeys(self):
+        """loop through looking for the keys and the beginning of the rasta"""
+        self.f.seek(0)                               # just ot be sure
+        while map(self.cleanInput, self.r.next()).count('') < 10 :
+            print "Skipping header line..."
 
-##loop through looking for the keys and the beginning of the rasta
-f.seek(0)                               # just ot be sure
-while map(cleanInput, r.next()).count('') < 10 :
-        print "Skipping header line..."
+        ##skipping blanks
+        while True:
+            l=map(self.cleanInput, self.r.next())
+            if l.count('') < 10:
+                break
+            else:
+                print "Skipping BLANK line..."
 
-##skipping blanks
-while True:
-    l=map(cleanInput, r.next())
-    if l.count('') < 10:
-        break
-    else:
-        print "Skipping BLANK line..."
-
-
-
-#YAY! got the keys
-keys=l
-
-#ooh, i like python
-dict(zip(keys,[cleanInput(i) for i in r.next()]))
+        #YAY! got the keys
+        self.keys=l
 
 
+###### MAIN
+if __name__ == '__main__':
+    R=RastaImport()
+    R.setup()
+    R.getKeys()
+    #ooh, i like python
+    dict(zip(R.keys,map(R.cleanInput, R.r.next())))
 
-##wow cool!
+
+##wow cool! though don't need now with dicts!
 #[p.split() for p in l[1:3]]
