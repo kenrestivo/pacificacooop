d4134020bd23d234091d9facb001a0a4db7312d1
ken restivo <ken@restivo.org>
2005-08-16 09:30:39 +0000

    damn tabs in emacs damn
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1418 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/importrasta.py
+++ scripts/importrasta.py
@@ -7,6 +7,10 @@ import csv
 import MySQLdb
 mdb=MySQLdb                             # loathe CapNames
 
+
+
+## do i *really* need an object here? or is encapsualtion in import ok?
+## ah, ok. one rastaimport for each of am/pm
 class RastaImport:
     keys=[]
     f=None
@@ -16,31 +20,32 @@ class RastaImport:
         #file open
         self.f=open("/mnt/kens/ki/proj/coop/imports/AMRoste05-06.csv" , "r")
         self.r=csv.reader(self.f,dialect='excel')
+        print "Loaded file."
 
         #db open
         #conn=mdb.connect(user='input', passwd='test', db='coop', 
-         #           host='bc', cursorclass=mdb.cursors.DictCursor) 
+        #           host='bc', cursorclass=mdb.cursors.DictCursor) 
         #c=conn.cursor()
 
     def cleanInput(self,x):
-         return x.replace('*','').strip()
+        return x.replace('*','').strip()
+
 
     def getKeys(self):
-        """loop through looking for the keys and the beginning of the rasta"""
-        self.f.seek(0)                               # just ot be sure
-        while map(self.cleanInput, self.r.next()).count('') < 10 :
-            print "Skipping header line..."
+         """loop through looking for the keys and the beginning of the rasta"""
+         self.f.seek(0)                               # just ot be sure
+         while map(self.cleanInput, self.r.next()).count('') < 10 :
+                 print "Skipping header line..."
 
-        ##skipping blanks
-        while True:
-            l=map(self.cleanInput, self.r.next())
-            if l.count('') < 10:
-                break
-            else:
-                print "Skipping BLANK line..."
+         ##skipping blanks
+         while True:
+                 print "Skipping BLANK line..."
+                 l=map(self.cleanInput, self.r.next())
+                 if l.count('') < 10:
+                         break
 
-        #YAY! got the keys
-        self.keys=l
+         #YAY! got the keys
+         self.keys=l
 
 
 ###### MAIN
