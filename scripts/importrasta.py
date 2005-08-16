520d6a00c94812e88c7b6b7da3ad6acf3c557212
ken restivo <ken@restivo.org>
2005-08-16 08:50:44 +0000

    i love python
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1415 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/importrasta.py
+++ scripts/importrasta.py
@@ -4,12 +4,40 @@
 
 import os
 import csv
+import MySQLdb
+mdb=MySQLdb                             # loathe CapNames
 
+
+#file open
 f=open("/mnt/kens/ki/proj/coop/imports/AMRoste05-06.csv" , "r")
 r=csv.reader(f,dialect='excel')
 
-##TODO: loop through looking for empty lines, followd by keys
+#db open
+conn=mdb.connect(user='input', passwd='test', db='coop', 
+                host='bc', cursorclass=mdb.cursors.DictCursor)
+c=conn.cursor()
 
+def cleanInput(x):
+     return x.replace('*','').strip()
+
+
+##loop through looking for empty lines, followd by keys
+f.seek(0)                               # just ot be sure
+while map(cleanInput, r.next()).count('') < 10 :
+        print "Skipping header line..."
+
+##skipping blanks
+while True:
+    l=map(cleanInput, r.next())
+    if l.count('') < 10:
+        break
+    else:
+        print "Skipping BLANK line..."
+
+
+
+#YAY! got the keys
+keys=l
 
 #ooh, i like python
 dict(zip(keys,[i.replace('*','').strip() for i in r.next()]))
