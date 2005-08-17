8861baeb167905e081fee74b9afe3c5c5693dd86
ken restivo <ken@restivo.org>
2005-08-17 04:36:22 +0000

    holy damn crap. it works.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1423 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/importrasta.py
+++ scripts/importrasta.py
@@ -7,7 +7,7 @@ import csv
 #import MySQLdb
 #mdb=MySQLdb                             # loathe CapNames
 
-
+l=[]                                    # the completed am/pm minimarket
 
 ## do i *really* need an object here? or is encapsualtion in import ok?
 ## ah, ok. one rastaimport for each of am/pm
@@ -19,58 +19,56 @@ class RastaImport:
     session=''
 
     def __init__ (self, filename, session):
+        """Loads the file, basically. 
+            loop through looking for the keys and the beginning of the rasta"""
         self.fname=filename
         self.session=session
-        self.setup()
-        self.getKeys()
 
-    def setup(self):
-        """Loads the file, basically"""
-        #file open
         self.f=open(self.fname, "r")
         self.r=csv.reader(self.f,dialect='excel')
         print "Loaded file."
 
-        #db open
-        #conn=mdb.connect(user='input', passwd='test', db='coop', 
-        #           host='bc', cursorclass=mdb.cursors.DictCursor) 
-        #c=conn.cursor()
+
+    def get(self):
+        self.f.seek(0)                               # just ot be sure
+        while map(self._cleanInput, self.r.next()).count('') < 10 :
+            print "Skipping header line..."
+
+        ##skipping blanks
+        while True:
+            print "Skipping BLANK line..."
+            l=map(self._cleanInput, self.r.next())
+            if l.count('') < 10:
+                break
+
+        #YAY! got the keys
+        self.keys=l
+
+        l=[dict(zip(self.keys,map(self._cleanInput, x))) for x in self.r]
+        return l
+
+
+
 
     def _cleanInput(self,x):
-        """Utility method to clean up input"""
+        """Utility method to clean up input record"""
         return x.replace('*','').strip()
 
 
-    def getKeys(self):
-         """loop through looking for the keys and the beginning of the rasta"""
-         self.f.seek(0)                               # just ot be sure
-         while map(self._cleanInput, self.r.next()).count('') < 10 :
-                 print "Skipping header line..."
-
-         ##skipping blanks
-         while True:
-                 print "Skipping BLANK line..."
-                 l=map(self._cleanInput, self.r.next())
-                 if l.count('') < 10:
-                         break
-
-         #YAY! got the keys
-         self.keys=l
-
-    def loadLine(self):
-        """Iterator method, basically. Produces cleaned-up line"""
-        #ooh, i like python
-        l=dict(zip(self.keys,map(self._cleanInput, self.r.next())))
-    	l.update({'session':self.session})
-        return l
 
 ###### MAIN
 if __name__ == '__main__':
     AM=RastaImport("/mnt/kens/ki/proj/coop/imports/AMRoste05-06.csv", 'AM')
     PM=RastaImport("/mnt/kens/ki/proj/coop/imports/PMRoster05-06.csv", 'PM')
-	#AM.loadLine()
-
 
 
 ##wow cool! though don't need now with dicts!
 #[p.split() for p in l[1:3]]
+
+
+        #db open
+        #conn=mdb.connect(user='input', passwd='test', db='coop', 
+        #           host='bc', cursorclass=mdb.cursors.DictCursor) 
+        #c=conn.cursor()
+        
+            #l.update({'session':self.session}) 
