cb30393d38e1c5eef6c84ef14e6a315ba7854c81
ken restivo <ken@restivo.org>
2005-08-21 07:10:20 +0000

    got it
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1437 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- sql/queries.sql
+++ sql/queries.sql
@@ -1153,11 +1153,12 @@ order by packages.package_number
 
 
 --- the new user perms checking code: one trip to the goddamned db!
-select table_permissions.table_name, table_permissions.field_name,
+select 
+table_permissions.table_name, table_permissions.field_name,
 if(user_privileges.user_level > table_permissions.user_level, 
 table_permissions.user_level, user_privileges.user_level) as cooked_user,
 if(user_privileges.group_level>  table_permissions.group_level, 
-table_permissions.user_level, user_privileges.user_level) as cooked_group
+table_permissions.group_level, user_privileges.group_level) as cooked_group
 from user_privileges 
 left join table_permissions on user_privileges.realm = table_permissions.realm 
 where user_id = 52 and table_name = 'leads'
