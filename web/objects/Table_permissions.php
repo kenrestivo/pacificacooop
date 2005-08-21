9c646be2a23eeb35ed2e4a2d781221759c7c82c4
ken restivo <ken@restivo.org>
2005-08-21 02:45:56 +0000

    first post
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1434 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ web/objects/Table_permissions.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Table Definition for table_permissions
+ */
+require_once 'DB/DataObject.php';
+
+class Table_permissions extends DB_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    var $__table = 'table_permissions';               // table name
+    var $table_permissions_id;            // int(32)  not_null primary_key unique_key auto_increment
+    var $table_name;                      // string(255)  
+    var $field_name;                      // string(255)  
+    var $group_id;                        // int(32)  
+    var $realm;                           // string(55)  
+    var $user_level;                      // int(5)  
+    var $group_level;                     // int(5)  
+
+    /* ZE2 compatibility trick*/
+    function __clone() { return $this;}
+
+    /* Static get */
+    function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('Table_permissions',$k,$v); }
+
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+}
