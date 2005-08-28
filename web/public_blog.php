fa296331a6a7643f4b7a359e23a5876b73ac1d5b
ken restivo <ken@restivo.org>
2005-08-28 20:27:54 +0000

    posted
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1488 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/public_blog.php
+++ web/public_blog.php
@@ -1,10 +1,13 @@
 <?php
 
 $newsitems = array(array('title'=>'Fall Session starts September 12',
-                         'body' => 'There may still be a very few openings for Fall. Call 355-3272 for more information. Now is a good time to fill out a <a href="documents/Wait_list_application_2005.pdf">wait-list application</a>.'));
+                         'body' => 'There may still be a very few openings for Fall. Call 355-3272 for more information. Now is a good time to fill out a <a href="documents/Wait_list_application_2005.pdf">wait-list application</a>.',
+                         'updated' => '08/05/2005 12:00PM'));
+
 if($_REQUEST['summary']){
     foreach($newsitems as  $item){
-        printf("<p><b>%s</b><p>%s</p>", $item['title'], $item['body']);  
+        printf("<p><b>%s</b><p>%s (Posted %s)</p>", 
+               $item['title'], $item['body'], $item['updated']);  
     }
 } else {
     print "Blog interface still under construction";
