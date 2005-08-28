7432d3d7cb1429d2a7fc854fd1babf6c947764fc
ken restivo <ken@restivo.org>
2005-08-28 03:35:16 +0000

    first
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1477 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ web/logs/latest.php
@@ -0,0 +1,26 @@
+<?php
+
+
+$cwd = getcwd();
+
+print "$cwd<br>";
+
+
+
+// Outputs all the result of shellcommand "ls", and returns
+// the last output line into $last_line. Stores the return value
+// of the shell command in $retval.
+$last_line = system('ls', $retval);
+
+// Printing additional info
+echo '
+</pre>
+<hr>Last line of the output: '.$last_line.'
+<hr>Return value: '.$retval;
+
+
+system('ls '.escapeshellarg($cwd));
+
+
+
+?>
