857125a6274f138dbc380d82336508336384e3f0
ken restivo <ken@restivo.org>
2005-08-30 12:12:06 +0000

    i love this language
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1514 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/calendarparse.py
+++ scripts/calendarparse.py
@@ -6,7 +6,6 @@ f=open('/mnt/kens/ki/proj/coop/imports/PCNS2005-2006Calendar.txt', 'r')
 l=f.readlines()
 f.close()
 
-lsp=[filter(lambda x: x!= '', s.split('\t')) for s in l]
-filter(lambda x: len(x) > 1, lsp)
-act=[x[1] for x in lf]
+lsp=[filter(lambda x: x!= '', s.strip('\n').split('\t')) for s in l]
+act=[x[1] for x in filter(lambda x: len(x) > 1, lsp)]
 
