11276f2cf2af0b28274d7de736af675c1bd3bf70
ken restivo <ken@restivo.org>
2005-09-01 21:17:43 +0000

    yay
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1546 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/schoolyearifyperms.py
+++ scripts/schoolyearifyperms.py
@@ -3,13 +3,15 @@
 #insert into table_permissions (field_name, table_name, user_level, group_level)
 
 #uses global c above
-def has_school_year(j):
+def has_field(f,j):
     c.execute("explain %s" % (j['table_name']))
     k=[i['Field'] for i in c.fetchall()]
-    return k.count('school_year')
+    return k.count(f)
 
+
+#### missing schoolyears
 c.execute('select distinct table_name, realm_id from table_permissions')
-l=filter(has_school_year, c.fetchall())
+l=filter(lambda x: has_field('school_year', x), c.fetchall())
 
 print """insert into table_permissions
 (field_name, table_name, realm_id, user_level, group_level)
@@ -17,3 +19,16 @@ values"""
 print ",\n".join(["('school_year', '%s', %d, 200, 200)" %
                   (i['table_name'], i['realm_id']) for i in l])
     
+
+
+### ok, now time for the family stuff too
+c.execute("""select sum(if(field_name = 'family_id', 1, 0)) as fampresent, 
+realm_id, table_name 
+from table_permissions  
+group by table_name 
+having fampresent <1 
+order by table_name""")
+l=filter(lambda x: has_field('family_id', x), c.fetchall())
+print ",\n".join(["('family_id', '%s', %d, 200, 200)" %
+                  (i['table_name'], i['realm_id']) for i in l])
+
