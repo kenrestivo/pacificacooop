2798389095b2fb89a0594dd4fc7a79a0424f8a0c
ken restivo <ken@restivo.org>
2005-09-01 21:29:22 +0000

    yay, correct query
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1547 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/schoolyearifyperms.py
+++ scripts/schoolyearifyperms.py
@@ -3,32 +3,27 @@
 #insert into table_permissions (field_name, table_name, user_level, group_level)
 
 #uses global c above
+
+##TODO: don't report it if it is a primary key. that could get ugly.
+
 def has_field(f,j):
     c.execute("explain %s" % (j['table_name']))
     k=[i['Field'] for i in c.fetchall()]
     return k.count(f)
 
-
-#### missing schoolyears
-c.execute('select distinct table_name, realm_id from table_permissions')
-l=filter(lambda x: has_field('school_year', x), c.fetchall())
-
-print """insert into table_permissions
-(field_name, table_name, realm_id, user_level, group_level)
-values"""
-print ",\n".join(["('school_year', '%s', %d, 200, 200)" %
-                  (i['table_name'], i['realm_id']) for i in l])
-    
-
-
-### ok, now time for the family stuff too
-c.execute("""select sum(if(field_name = 'family_id', 1, 0)) as fampresent, 
+def addDefaultPerms(field, user, group):
+    c.execute("""select sum(if(field_name = '%s', 1, 0)) as present, 
 realm_id, table_name 
 from table_permissions  
-group by table_name 
-having fampresent <1 
+group by table_name, realm_id
+having present <1 
 order by table_name""")
-l=filter(lambda x: has_field('family_id', x), c.fetchall())
-print ",\n".join(["('family_id', '%s', %d, 200, 200)" %
-                  (i['table_name'], i['realm_id']) for i in l])
+    l=filter(lambda x: has_field(field, x), c.fetchall())
+    print ",\n".join(["('%s', '%s', %d, %d, %d)" %
+                      (field, i['table_name'], i['realm_id'], user, group)
+                      for i in l])
 
+
+### heh, didn't i have a huge perl script to do stuff like this once?
+addDefaultPerms('school_year', 200,200)
+addDefaultPerms('family_id', 200,200)
