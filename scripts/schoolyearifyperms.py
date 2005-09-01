46f235a5302a353a771983b341bdbe5572bef63a
ken restivo <ken@restivo.org>
2005-09-01 18:57:27 +0000

    damn. still not done yet.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1539 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/schoolyearifyperms.py
+++ scripts/schoolyearifyperms.py
@@ -9,9 +9,11 @@ def has_school_year(j):
     return k.count('school_year')
 
 c.execute('show tables')
-l=[i['Tables_in_coop'] for i in c.fetchall()]
+l=filter(has_school_year,
+         [i['Tables_in_coop'] for i in c.fetchall()])
 
-print "insert into table_permissions (field_name, table_name, user_level, group_level) values"
-print ",\n".join(["('school_year', %s, 200, 200)" % (i) for i in
-                  filter(has_school_year,l)])
+print """insert into table_permissions
+(field_name, table_name, user_level, group_level)
+values"""
+print ",\n".join(["('school_year', '%s', 200, 200)" % (i) for i in l])
     
