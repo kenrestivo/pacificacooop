816fcaedb77a4801ad5b4a536e9aca2b67af836a
ken restivo <ken@restivo.org>
2005-09-01 18:47:08 +0000

    yay, it works
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1538 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/schoolyearifyperms.py
+++ scripts/schoolyearifyperms.py
@@ -2,11 +2,16 @@
 
 #insert into table_permissions (field_name, table_name, user_level, group_level)
 
+#uses global c above
+def has_school_year(j):
+    c.execute("explain %s" % (j))
+    k=[i['Field'] for i in c.fetchall()]
+    return k.count('school_year')
+
 c.execute('show tables')
-t=c.fetchall()
-l=[i['Tables_in_coop'] for i in t]
+l=[i['Tables_in_coop'] for i in c.fetchall()]
 
 print "insert into table_permissions (field_name, table_name, user_level, group_level) values"
-
-print ",\n".join(["('school_year', %s, 200, 200)" % (i) for i in l])
+print ",\n".join(["('school_year', %s, 200, 200)" % (i) for i in
+                  filter(has_school_year,l)])
     
