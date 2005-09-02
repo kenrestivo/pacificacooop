0be7d9edde22d8c54b170eb44c605b664ba4c813
ken restivo <ken@restivo.org>
2005-09-02 18:46:07 +0000

    first post
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1557 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ web/qa/unit-tests.php
@@ -0,0 +1,32 @@
+<?php
+
+//$Id$
+// to find last years' alumni and add them to the leads db.
+// I HATE THIS!!! people should be people, dammit.
+
+chdir("../");                   // XXX only for "test" dir hack!
+require_once('CoopPage.php');
+require_once('CoopObject.php');
+require_once("members.inc");
+require_once("everything.inc");
+
+
+$cp = new coopPage( $debug);
+print $cp->pageTop();
+
+
+
+
+
+
+
+done ();
+
+
+////KEEP EVERTHANG BELOW
+
+?>
+
+
+<!-- END UNITTTEST -->
+
