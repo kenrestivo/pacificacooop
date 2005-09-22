8efced2d903e02e7977dc0a58ccfa71154897c4e
ken restivo <ken@restivo.org>
2005-09-22 19:57:54 +0000

    pretty
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1741 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- sql/upgrade-db.sql
+++ sql/upgrade-db.sql
@@ -2,8 +2,9 @@
 
 alter table job_descriptions add column free_tuition_start_month enum('January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
  alter table job_descriptions add column free_tuition_end_month enum('January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+
 update  job_descriptions set free_tuition_start_month = 'September', free_tuition_end_month = 'June'  where free_tuition_months = 10;
 update  job_descriptions set free_tuition_start_month = 'December', free_tuition_end_month = 'June'  where free_tuition_months = 6;
+
 alter table job_descriptions drop column free_tuition_months;
 
--- perms: set free_tuition_[start|end]_month to superuser-only
