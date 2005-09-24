5c11ef18b8051608dde89b40d35412368acba20b
ken restivo <ken@restivo.org>
2005-09-24 03:23:04 +0000

    moved solicit summary ouver to the new pseudo-gaspacho.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1742 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/reports/solicit_summary.php
+++ web/reports/solicit_summary.php
@@ -16,22 +16,215 @@
 	#  along with this program; if not, write to the Free Software
 	#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-// <!-- CARRIEREPORT -->
+//$Id$
 
-require_once("first.inc");
-
-	require_once("solicit_summary.inc");
-
-	//global hacks to save ken's wrists
-	$callbacks = $solicit_summary_callbacks;
-	$fields = $solicit_summary_fields;
+require_once('CoopPage.php');
+require_once('CoopView.php');
+require_once('CoopMenu.php');
+require_once('CoopForm.php');
+require_once('HTML/Table.php');
 
 
-	require_once("states.inc");
 
-done();
-	# end of inner php code
+//MAIN
+//$_SESSION['toptable'] 
+
+//DB_DataObject::debugLevel(2);
+
+$cp = new coopPage( $debug);
+print $cp->pageTop();
+
+
+$atd = new CoopView(&$cp, 'families', $none);
+
+$menu =& new CoopMenu();
+$menu->page =& $cp;				// XXX hack!
+print $menu->topNavigation();
+
+print "<p>Contribution Summary by Family</p>";
+
+
+print "\n<hr></div><!-- end header div -->\n"; //ok, we're logged in. show the rest of the page
+print '<div id="centerCol">';
+
+
+
+function viewHack(&$atd)
+{
+
+	$schoolyear = $atd->page->currentSchoolYear;
+ 
+
+	$res = "<h2>Solicitation totals for $schoolyear </h2>";
+
+
+	$res .= showRawQuery("Solicitation Cash Income by Type",
+" 
+select coa.description as Description,
+        sum(inc.total) as Before_Event, 
+        sum(pur.total) as At_Event
+from chart_of_accounts as coa
+left join 
+    (select account_number, sum(payment_amount) as total
+     from companies_income_join as cinj
+     left join income 
+              on cinj.income_id = 
+                income.income_id
+        where school_year = '$schoolyear'
+        group by cinj.company_id) 
+    as inc
+        on inc.account_number = coa.account_number
+left join 
+    (select  account_number, payment_amount as total
+     from springfest_attendees as atd
+    left join auction_purchases  as ap
+            on ap.springfest_attendee_id = 
+                atd.springfest_attendee_id
+     left join income 
+              on ap.income_id = 
+                income.income_id
+        where income.school_year = '$schoolyear'
+        group by atd.company_id) 
+    as pur
+        on pur.account_number = coa.account_number
+group by coa.account_number
+having Before_Event >0 or At_Event > 0
+order by Before_Event desc, At_Event desc
+"
+					   ,1);
+
+
+	$res .= showRawQuery("Solicitation Summary by Company",
+" 
+select concat_ws(' - ', company_name, concat_ws(' ', first_name, last_name)) 
+    as Company,
+        sum(inc.payment_amount) as cash_donations,
+        sum(pur.payment_amount) as auction_purchases,
+        sum(auct.item_value) as auction_donations,
+        sum(iks.item_value) as in_kind_donations
+from companies
+left join 
+    (select  sum(item_value) as item_value, company_id
+     from companies_auction_join  as caj
+     left join auction_donation_items  as adi
+              on caj.auction_donation_item_id = 
+                adi.auction_donation_item_id
+        where school_year = '$schoolyear'
+        group by caj.company_id) 
+    as auct
+        on auct.company_id = companies.company_id
+left join 
+    (select  sum(item_value) as item_value, company_id
+     from companies_in_kind_join as cikj
+     left join in_kind_donations as ikd
+              on cikj.in_kind_donation_id = 
+                ikd.in_kind_donation_id
+        where school_year = '$schoolyear'
+        group by cikj.company_id) 
+    as iks
+        on iks.company_id = companies.company_id
+left join 
+    (select  sum(payment_amount) as payment_amount, company_id
+     from companies_income_join as cinj
+     left join income 
+              on cinj.income_id = 
+                income.income_id
+        where school_year = '$schoolyear'
+        group by cinj.company_id) 
+    as inc
+        on inc.company_id = companies.company_id
+left join 
+    (select  payment_amount, company_id
+     from springfest_attendees as atd
+    left join auction_purchases  as ap
+            on ap.springfest_attendee_id = 
+                atd.springfest_attendee_id
+     left join income 
+              on ap.income_id = 
+                income.income_id
+        where income.school_year = '$schoolyear'
+        group by atd.company_id) 
+    as pur
+        on pur.company_id = companies.company_id
+group by companies.company_id
+having cash_donations > 0 or auction_purchases > 0 or auction_donations > 0 or in_kind_donations > 0
+order by cash_donations desc, auction_purchases desc, 
+    auction_donations desc, in_kind_donations desc 
+
+", 1);
+
+	$res .= showRawQuery("Top Soliciting Families",
+" 
+select families.name as Soliciting_family,
+        sum(inc.payment_amount) as cash_donations,
+        sum(auct.item_value) as auction_donations,
+        sum(iks.item_value) as in_kind_donations
+from families
+left join 
+    (select  caj.family_id, sum(item_value) as item_value
+     from companies_auction_join  as caj
+     left join auction_donation_items  as adi
+              on caj.auction_donation_item_id = 
+                adi.auction_donation_item_id
+        where school_year = '$schoolyear'
+        group by caj.family_id) 
+    as auct
+        on auct.family_id = families.family_id
+left join 
+    (select  cikj.family_id, sum(item_value) as item_value
+     from companies_in_kind_join as cikj
+     left join in_kind_donations as ikd
+              on cikj.in_kind_donation_id = 
+                ikd.in_kind_donation_id
+        where school_year = '$schoolyear'
+        group by cikj.family_id) 
+    as iks
+        on iks.family_id = families.family_id
+left join 
+    (select  cinj.family_id, sum(payment_amount) as payment_amount
+     from companies_income_join as cinj
+     left join income 
+              on cinj.income_id = 
+                income.income_id
+        where school_year = '$schoolyear'
+        group by cinj.family_id) 
+    as inc
+        on inc.family_id = families.family_id
+group by families.family_id
+having cash_donations > 0 or auction_donations > 0 or in_kind_donations > 0
+order by cash_donations desc, 
+    auction_donations desc, in_kind_donations desc 
+
+", 1);
+
+	return $res;
+	 
+}
+
+// cheap dispatcher
+//confessArray($_REQUEST,'req');
+switch($_REQUEST['action']){
+ 
+	 
+//// EDIT AND NEW //////
+ case 'new':
+ case 'edit':
+	 break;
+
+//// DEFAULT (VIEW) //////
+ default:
+	 print viewHack(&$atd);
+
+	 break;
+}
+
+
+done ();
+
+////KEEP EVERTHANG BELOW
 
 ?>
 
+
 <!-- END SOLICITSUMMARY -->
+
