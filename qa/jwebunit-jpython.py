16ef767fcc343b9765a5d0127d902562e16f8818
ken restivo <ken@restivo.org>
2005-10-08 23:29:14 +0000

    first
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1865 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ qa/jwebunit-jpython.py
@@ -0,0 +1,15 @@
+
+jwebpath='/usr/local/share/jwebunit/'
+libs= [
+    'jwebunit.jar',
+    'lib/httpunit.jar',
+    'lib/nekohtml.jar',
+    'lib/xercesImpl.jar',
+    'lib/junit.jar',
+    'lib/xml-apis.jar',
+    'lib/js.jar']
+
+
+paths=[jwebpath+x for x in libx]
+
+
