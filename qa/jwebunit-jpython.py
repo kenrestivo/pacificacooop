6d3c4f8bfb06b19eac976c7226582f1cea2f0b1e
ken restivo <ken@restivo.org>
2005-10-09 00:18:39 +0000

    well java is all ed up, but this works at least
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1866 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- qa/jwebunit-jpython.py
+++ qa/jwebunit-jpython.py
@@ -1,15 +1,33 @@
 
+from pprint import pprint
+
+def pd(thing):
+    pprint(dir(thing))
+
+
+### the lib setups
+
+import sys
+
 jwebpath='/usr/local/share/jwebunit/'
-libs= [
-    'jwebunit.jar',
-    'lib/httpunit.jar',
-    'lib/nekohtml.jar',
-    'lib/xercesImpl.jar',
-    'lib/junit.jar',
-    'lib/xml-apis.jar',
-    'lib/js.jar']
+# jython doesn't like me.
+libs=['jwebunit.jar', 'lib/httpunit.jar', 'lib/nekohtml.jar', 'lib/xercesImpl.jar', 'lib/junit.jar', 'lib/xml-apis.jar', 'lib/js.jar']
+sys.path.extend([jwebpath+x for x in libs])
 
+from net.sourceforge.jwebunit import WebTestCase
 
-paths=[jwebpath+x for x in libx]
+## let's do it now!
+wtc=WebTestCase()
+tc=wtc.getTestContext()
+tc.setBaseUrl('http://www/coop-dev')
 
+def choose_family(family):
+    wtc.beginAt('/')
+    ##TODO add asserts that this is the RIGHT page
+    wtc.assertTextPresent('</html>')
+    wtc.assertFormPresent()
+    wtc.assertSubmitButtonPresent('login')
+    wtc.selectOption('auth[uid]', family)
+    wtc.assertSubmitButtonPresent('login')
+    wtc.submit('login')
 
