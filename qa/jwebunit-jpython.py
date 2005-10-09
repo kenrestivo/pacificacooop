c4ff851a6fa215d07bac6be016631527cca6b3d2
ken restivo <ken@restivo.org>
2005-10-09 01:14:20 +0000

    a few more
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1867 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- qa/jwebunit-jpython.py
+++ qa/jwebunit-jpython.py
@@ -11,7 +11,7 @@ import sys
 
 jwebpath='/usr/local/share/jwebunit/'
 # jython doesn't like me.
-libs=['jwebunit.jar', 'lib/httpunit.jar', 'lib/nekohtml.jar', 'lib/xercesImpl.jar', 'lib/junit.jar', 'lib/xml-apis.jar', 'lib/js.jar']
+libs=['lib/js.jar', 'jwebunit.jar', 'lib/httpunit.jar', 'lib/nekohtml.jar', 'lib/xercesImpl.jar', 'lib/junit.jar', 'lib/xml-apis.jar']
 sys.path.extend([jwebpath+x for x in libs])
 
 from net.sourceforge.jwebunit import WebTestCase
@@ -30,4 +30,40 @@ def choose_family(family):
     wtc.selectOption('auth[uid]', family)
     wtc.assertSubmitButtonPresent('login')
     wtc.submit('login')
+
+
+def enter_password():
+    wtc.assertTextPresent('</html>')
+    wtc.assertFormPresent()
+    wtc.assertSubmitButtonPresent('login')
+    wtc.assertFormElementPresent('auth[pwd]')
+    wtc.setFormElement('auth[pwd]', 'tester')
+    wtc.submit('login')
+
+
+def main_page_ok():
+    wtc.assertTextPresent "</html>")
+    wtc.assertLinkPresentWithText "Log Out")
+    ## shirley has no enter wtc.assertLinkPresentWithText("Enter New")
+    wtc.assertLinkPresentWithText('View')
+
+
+def dump_page():
+    wtc.getDialog().getResponseText()
+
+
+def get_response():
+    return wtc.getDialog().getResponse()
+
+
+## go as far as you can, so far.
+def get_to_main_page(family):
+    choose_family(family)
+    enter_password()
+    main_page_ok()
+
+
+
+
+## grab misc-shit from httpunit-jscheme
 
