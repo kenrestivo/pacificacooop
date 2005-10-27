d4176f63cd2976e0a6e4dc1c053b0d2fcced487d
ken restivo <ken@restivo.org>
2005-10-27 04:51:05 +0000

    had to do it
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1974 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- sql/upgrade-db.sql
+++ sql/upgrade-db.sql
@@ -1,15 +1,9 @@
 
 --- do it
 
-update  leads set address1 = null where address1 like '%(%';
-alter table leads drop column school_year;
-alter table leads drop column family_id;
-
--- the import 2004-2005 invitations query (queries.sql)
-
-update table_permissions set user_level = null, group_level = null, menu_level = null, year_level = null where table_name = 'invitations';
-
--- new realm: people
--- perms for people!
--- invitation perms : delete/none/view
--- perms for label_printed:  view/view
+insert into invitations
+(lead_id, school_year, family_id, relation) 
+values
+(1598, '2005-2006', 121, 'Friend'),
+(1599, '2005-2006', 121, 'Friend'),
+(1601, '2005-2006', 121, 'Friend')
