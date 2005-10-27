fbda4eb16c2f459993e7e3537de877b78f7704b5
ken restivo <ken@restivo.org>
2005-10-27 20:11:57 +0000

    nifty. show cool table nemes.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@1976 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/objects/Instructions.php
+++ web/objects/Instructions.php
@@ -23,4 +23,61 @@ class Instructions extends CoopDBDO
 
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
+
+	var $fb_formHeaderText =  'User Instructions';
+	var $fb_shortHeader =  'Instructions';
+	var $fb_linkDisplayFields = array('table_name', 'action');
+	var $fb_fieldLabels = array (
+        'table_name' => 'Table',
+        'action'  => 'Action the instructions apply to',
+        'instruction' => 'Instructions for this action'
+		);
+
+    var $fb_displayCallbacks = array('table_name' => 'useLabel');
+
+    // TODO: genericie this! it's used in table_permissions, and elsewhere!
+    function preGenerateForm(&$form)
+        {
+
+            // TODO: heirselect!
+            $foo = $this->factory('table_permissions');
+			$foo->query('show tables');
+			$options[] = '-- CHOOSE ONE --';
+            confessObj($this, 'this');
+            $thing='Tables_in_'.$this->_database;
+			while($foo->fetch()){
+                $tab = $this->factory($foo->$thing);
+				$options[$foo->$thing] = 
+                    $tab->fb_formHeaderText ? $tab->fb_formHeaderText : strtr(ucwords($foo->$thing), '_', ' ');
+			}
+			$el =& HTML_QuickForm::createElement(
+                'select', 
+                $form->CoopForm->prependTable('table_name'), 
+                $this->fb_fieldLabels['table_name'], 
+                &$options);
+            $this->fb_preDefElements['table_name'] = $el;
+			return $el;
+            
+        }
+
+    // THIS TOO! duplicate of elsewhere
+  function useLabel(&$co, $val, $key)
+        {
+            //WARNING this can really suck if you change table names!!
+            // you must run the update if needed
+            $sub =& $this->factory($this->table_name);
+                
+            if($key == 'table_name'){
+                return $sub->fb_formHeaderText;
+            }
+
+            if($key == 'field_name'){
+                return $sub->fb_fieldLabels[$val] ? 
+                    $sub->fb_fieldLabels[$val] : $val;
+            }
+            
+        }
+
+
 }
+
