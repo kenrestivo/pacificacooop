fe58478c37a1e580e8b39922f082c7805dcc7caf
ken restivo <ken@restivo.org>
2005-11-28 20:44:14 +0000

    foo
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2037 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- sql/upgrade-db.sql
+++ sql/upgrade-db.sql
@@ -4,14 +4,18 @@ drop table access_levels;
 -- use the new definition for accesslevels (definition.sql)
 -- seed the new accesslevels (seed.sql)
 
-
 --- do it
 
--- add table permisssions: user instructions and auction stuff to System realm
-
+-- table perms: fees income change from solicitation to fees
+-- table perms: add family fees
 -- user perms: fees income change from solicitation to fees
 
 -- restivo family gets admin perms  (springfest gets them removed?)
 
--- finally, look at audit trail for system, to check i got all of these
+-- add table permisssions: user instructions and auction stuff to System realm
+
+-- UNNECESSARY! my new trick works! update table_permissions set user_level = null, group_level = null, menu_level = null, year_level = null where table_name = 'families_income_join';
+
+
+
 
