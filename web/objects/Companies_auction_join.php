8498381eee889d73a31a00f44d2525c4b3e40154
ken restivo <ken@restivo.org>
2005-12-16 22:30:10 +0000

    the companies auction join -- back from the dead!
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2076 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/objects/Companies_auction_join.php
+++ web/objects/Companies_auction_join.php
@@ -23,4 +23,32 @@ class Companies_auction_join extends CoopDBDO
 
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
+
+	var $fb_linkDisplayFields = array('auction_donation_item_id', 
+									  'company_id');
+
+	var $fb_fieldLabels = array ('auction_donation_item_id' => 'Auction Item',
+                                 'company_id' => 'Solicitation Company');
+
+	var $fb_formHeaderText =  'Springfest Solicitation Auction Donation Items';
+
+	var $fb_requiredFields = array('auction_donation_item_id', 
+                                   'company_id');
+
+
+    var $fb_shortHeader = 'Solicitation Donations';
+
+    function fb_linkConstraints(&$co)
+		{
+            $auc =& new CoopObject(&$co->page, 'auction_donation_items', 
+                                   &$co);
+            $auc->constrainSchoolYear();
+            $co->protectedJoin($auc);
+            // TODO: somehow make orderbylinkdisplay() recursive
+            $this->orderBy('item_description');
+            $co->grouper();
+		}
+
+
+
 }
