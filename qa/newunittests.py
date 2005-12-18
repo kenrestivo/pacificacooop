a52a23e2f0395b0ff5a39715383b8b266d9cf787
ken restivo <ken@restivo.org>
2005-12-18 03:01:53 +0000

    holy crap, it works
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2087 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- qa/newunittests.py
+++ qa/newunittests.py
@@ -4,7 +4,16 @@
 
 htmlunitdir = '/usr/scratch/htmlunit-1.7/lib/'
 
-import os
+import os,sys
 for i in os.listdir(htmlunitdir):
     sys.path.append('/'.join([htmlunitdir, i]))
 
+
+import com.gargoylesoftware.htmlunit 
+
+
+## batteries NOT INCLUDED!
+sys.path.append('/usr/scratch/commons-httpclient-3.0-rc4/commons-httpclient-3.0-rc4.jar')
+
+
+wc = com.gargoylesoftware.htmlunit.WebClient()
