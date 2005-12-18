577fc0a9503c9f9f9ce7e4821de8a86a0a3f4dfd
ken restivo <ken@restivo.org>
2005-12-18 03:45:40 +0000

    hey, it logs me in. yippie!
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2091 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- qa/newunittests.py
+++ qa/newunittests.py
@@ -18,8 +18,20 @@ from java.net import URL
 
 
 wc = htmlunit.WebClient(htmlunit.BrowserVersion.MOZILLA_1_0, )
-page= wc.getPage(URL('http://htmlunit.sourceforge.net'))
-page.getTitleText()
+page= wc.getPage(URL('http://www/coop-dev'))
 
+page.getTitleText()
 #assertEquals("htmlunit - Welcome to HtmlUnit", page.getTitleText() )
+
+
+##login form
+f=page.getForms()[0]
+f.getSelectByName('auth[uid]')
+for i in s.getOptions():
+    if i.asText() == 'Cooke Family':
+            i.setSelected(1)
+f.getInputByName('auth[pwd]').setValueAttribute('tester')
+mainpage=f.getInputByName('login').click()
+
+
 
