1cd50ae2549fdabf756a171c457be464eb489cc4
ken restivo <ken@restivo.org>
2005-12-18 08:40:04 +0000

    attempt to fit into pytest framework
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2103 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- qa/newunittests.py
+++ qa/newunittests.py
@@ -15,7 +15,7 @@ import com.gargoylesoftware.htmlunit
 htmlunit = com.gargoylesoftware.htmlunit
 
 from java.net import URL
-
+import unittest
 
 
 ## some utility funcs
@@ -28,37 +28,61 @@ def usableSelect(sel, val):
     #throw Exception(val + ' is not present in selectbox!')
 
 #TODO: don't click on links to same page, go find out what this page is
-def getAllLinks(page):
-    return [i for i in page.getAllHtmlChildElements() if i.getTagName() == 'a']
+
+
+class Test_Coop(unittest.TestCase):
+    """test the website. page is the current page. mainpage is homepage."""
+    wc = None
+    page= None 
+    mainpage= None
+    url= 'http://www/coop-dev'))
+    
+    def setUp(self):
+        self.wc = htmlunit.WebClient(htmlunit.BrowserVersion.MOZILLA_1_0, )
+
+    def testGetLoginPage(self):
+        self.page= self.wc.getPage(URL(self.url))
+        pageLoaded()
+
+    def testLogIn(self):
+        """ i CHECK FIRST!! i might already be logged in"""
+        try:
+            f=self.page.getForms()[0]
+            f.getInputByName('auth[pwd]').setValueAttribute('tester')
+            usableSelect(f.getSelectByName('auth[uid]'), 'Cooke Family')
+            self.mainpage=f.getInputByName('login').click()
+        except KeyError:
+            self.mainpage=self.page
+
+
+    def testSubPages(self):
+        for i in self.getAllLinks():
+            i.click()
+            pageLoaded()
+
+
+
+    def pageLoaded(self):
+            self.assertEqual(1, self.page.getWebResponse().getContentAsString().count('</html>'))
+
+            
+    def getAllLinks(self):
+        """htmlunit has no such function as getalllinks. so, here it is"""
+        return [i for i in self.page.getAllHtmlChildElements() if i.getTagName() == 'a']
+
+
+    def specialAudit(self):
+        """little utility to check for javascript ugliness in audit page"""
+        aud=[a for a in self.getAllLinks() if a.getHrefAttribute().count('audit')][0]
+        audpage=aud.click()
 
 
 
 
+##mainpage.getWebResponse().getUrl()
 ###### MAIN ######
 
-wc = htmlunit.WebClient(htmlunit.BrowserVersion.MOZILLA_1_0, )
-page= wc.getPage(URL('http://www/coop-dev'))
 
-page.getTitleText()
-#assertEquals("Data Entry", page.getTitleText() )
-##assertEquals(1, page.getWebResponse().getContentAsString().count('</html>'))
-
-##login form
-## CHECK FIRST!! i might already be logged in
-try:
-    f=page.getForms()[0]
-    f.getInputByName('auth[pwd]').setValueAttribute('tester')
-    usableSelect(f.getSelectByName('auth[uid]'), 'Cooke Family')
-    mainpage=f.getInputByName('login').click()
-except KeyError:
-    mainpage=page
-
-
-mainpage.getWebResponse().getUrl()
-
-## my special audit thing
-aud=[a for a in getAllLinks(mainpage) if a.getHrefAttribute().count('audit')][0]
-audpage=aud.click()
-
-
-
+## do this. it's good.
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(unittest.makeSuite(Test_Coop))
