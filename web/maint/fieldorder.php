7724fa37ec2c3636a4696c429a629e66654d951f
ken restivo <ken@restivo.org>
2005-12-19 10:01:05 +0000

    first post
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2125 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ web/maint/fieldorder.php
@@ -0,0 +1,56 @@
+<?php
+
+//$Id$
+// show the field order
+
+chdir("../");                   // XXX only for "test" dir hack!
+require_once('CoopPage.php');
+require_once('CoopObject.php');
+//require_once('HTML/Table.php');
+
+
+$cp = new coopPage( $debug);
+//print $cp->pageTop();
+
+print date('m/d/Y');
+
+
+$page = new CoopPage(4);
+$co =& new CoopObject(&$page, 'table_permissions', &$nothing);
+
+
+$co->obj->query(
+    'select distinct table_name from table_permissions order by table_name');
+
+while($co->obj->fetch()){
+    $tables[] = $co->obj->table_name;
+}
+
+
+print '<p>A nifty utility to show you all the tables for which we have permissions, and the ORDER of the fields as they will be displayed, which is of course in the order of the fb_fieldLabels</p>';
+
+            
+foreach($tables as $table){
+    $err = ''; 
+    $status = ''; 
+    $targ =& new CoopObject(&$page, $table, &$co);
+
+    $labels = array();
+    printf('<h3>%s (%s)</h3>', $targ->obj->fb_formHeaderText, $table);
+    foreach($targ->obj->fb_fieldLabels as $key => $label){
+        $labels[] = sprintf('%s (%s)', $label, $key);
+    }
+    printf('<ul><li>%s</li></ul>', implode('</li><li>', $labels));
+}
+
+
+done ();
+
+////KEEP EVERTHANG BELOW
+
+?>
+
+
+<!-- END SHOWSTRUCT -->
+ 
+
