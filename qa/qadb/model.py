c369dadba2f6d113dc115584e4106ada1546e275
ken restivo <ken@restivo.org>
2005-12-25 21:44:30 +0000

    first
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2189 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ qa/qadb/model.py
@@ -0,0 +1,20 @@
+#$Id$
+
+from sqlobject import *
+import dbhost
+
+sqlhub.processConnection = connectionForURI(dbhost.connectionurl)
+
+class SessionInfo(SQLObject):
+       ip_addr=StringCol(length=20)
+       entered=DateTimeCol()
+       updated=DateTimeCol()
+       User=ForeignKey('Users')
+       vars=BLOBCol()
+       class sqlmeta:
+              idName='session_id'
+
+
+
+
+     
