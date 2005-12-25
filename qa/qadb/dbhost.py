a7895efcff16baa04c35c6e8d96c4b1b73fa230c
ken restivo <ken@restivo.org>
2005-12-25 22:25:58 +0000

    foo
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2191 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ qa/qadb/dbhost.py
@@ -0,0 +1,16 @@
+#$Id$
+
+
+##TODO: check dev!
+host='bc'
+db='qa'
+user='input'
+pass='test'
+port =3306
+
+connectionurl=     'mysql://%s:%s@%s:%d/%s' % (user, pass, host, port, db)
+
+# need this everywhere
+sitepackages = '/mnt/kens/ki/proj/coop/web/site-packages'
+
+
