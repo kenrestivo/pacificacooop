00faeda940b1d77901668b6f3cfcaaeeadb56f61
ken restivo <ken@restivo.org>
2005-12-28 23:02:24 +0000

    better
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2208 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/logs/testresults.php
+++ web/logs/testresults.php
@@ -3,11 +3,17 @@
 //$Id$
 // nifty little util to grab the latest page and display it where's i can see it
 
+print "test results so far:<br />";
+
 if ($handle = opendir('.')) {
     /* This is the correct way to loop over the directory. */
     while (false !== ($file = readdir($handle))) {
         if(strstr($file, 'w3c_report')){
-			printf('<a href="%s">%s</a><br />', $file, $file);
+            list($num, $crap) = explode('-', $file);
+			printf('<a href="%s">%s</a>&nbsp;<a href="%s">%s</a><br />', 
+                   $file, $file,
+                   $num . '-death.html',
+                   $num . '-death.html');
         }
     }
     closedir($handle);
