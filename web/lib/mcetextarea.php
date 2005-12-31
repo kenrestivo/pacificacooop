66d5d8a2c6a1b8da3a2d930b5ee692ef8baa85d7
ken restivo <ken@restivo.org>
2005-12-31 03:01:35 +0000

    store the init in an external js file, because here comes the fun
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2250 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/lib/mcetextarea.php
+++ web/lib/mcetextarea.php
@@ -24,18 +24,8 @@ class HTML_QuickForm_mcetextarea extends HTML_QuickForm_textarea
             // change to tiny_mce_gzip.php, but it doesn't work
             $res .= $this->_parentForm->CoopForm->page->jsRequireOnce('lib/tiny_mce/tiny_mce_gzip.php', 
                                                                       'COOP_TINYMCE_INCLUDE');
-            $res .= wrapJS(
-                'tinyMCE.init({
-  mode : "textareas",
-  theme: "advanced",
-  theme_advanced_disable: "image,anchor,newdocument,visualaid,link,unlink,code", 
-  theme_advanced_buttons3_add: "cut,copy,pasteword,pastetext,selectall",
-  convert_newlines_to_brs: true,
-  plugins: "paste",
-  paste_use_dialog: false,
-  paste_auto_cleanup_on_paste: true,     
-  paste_strip_class_attributes : "all",
- });',
+
+            $res .= $this->_parentForm->CoopForm->page->jsRequireOnce('lib/tinymceinit.js', 
                 'HTML_QUICKFORM_MCETEXTAREA_EXISTS');
            
             $res .= "<noscript><h1>NOTICE! Some features on this page require Javascript. You will need to enable Javascript in your browser to use them.</h1></noscript>";
