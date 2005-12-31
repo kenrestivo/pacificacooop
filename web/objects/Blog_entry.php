08fa43be7b9c9f00ef1e5c6cd69d7f6cf461fbac
ken restivo <ken@restivo.org>
2005-12-31 06:53:29 +0000

    valid html
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2258 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/objects/Blog_entry.php
+++ web/objects/Blog_entry.php
@@ -78,7 +78,7 @@ class Blog_entry extends CoopDBDO
 			limit 4", 
                                  $clause));
             while($this->fetch()){
-                $res .= sprintf("<p><b>%s</b>&nbsp;%s</p>%s<p class=\"small\">(Posted %s by %s)</p><br />\n\n", 
+                $res .= sprintf("<div><b>%s</b>&nbsp;%s</div>%s<p class=\"small\">(Posted %s by %s)</p><br />\n\n", 
                                 $this->short_title, $this->body, 
                                 $publiconly ? '' : 
                                 $co->recordButtons(&$this, false),
