aa31a9c5ce106f84a968b09b6bd61478b37e8c06
ken restivo <ken@restivo.org>
2006-01-05 04:06:59 +0000

    first useless post
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2302 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ scripts/make-cache-longtext-columns.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python -u
+
+#$Id$
+
+
+import os
+import csv
+from datetime import date
+import MySQLdb, MySQLdb.cursors
+import datetime
+
+import dbhost
+
+
+def alterTables(c):
+    c= conn.cursor()
+    c.execute('show tables')
+    tables=c.fetchall()
+    for t in tables:
+        table=t['name']
+        c.execute('explain %s' % (table)
+        cols=c.fetchall()
+        c.execute('alter table %s add column %s_cache varchar(255)' % (table, )) 
+
+
+
+###### MAIN
+if __name__ == '__main__':
+    conn=MySQLdb.connect(user=dbhost.user, passwd=dbhost.pw, db=dbhost.db,
+                         host=dbhost.host, port=dbhost.port,
+                         cursorclass=MySQLdb.cursors.DictCursor) 
+    alterTables(conn)
+
+##END
