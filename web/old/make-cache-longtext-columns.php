886a1965d15c706e3b8354ebb67188386528f503
ken restivo <ken@restivo.org>
2006-01-05 07:11:42 +0000

    using dbobject
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2303 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/old/make-cache-longtext-columns.php
+++ web/old/make-cache-longtext-columns.php
@@ -5,43 +5,35 @@
 
 chdir("../");                   // XXX only for "test" dir hack!
 
-require_once("session-init.php");
-require_once('DB.php');
-
-setupDB();
+require_once("CoopObject.php");
 
 
-
-// aaptes from make-links. some duprication of code
-
-global $dburl;
-$db =& DB::connect($dburl);
-if (DB::isError($db)) {
-    die($db->getMessage());
-}
-
+function makeColumns(&$cp)
+{
+ 
 // get tables
-$res =& $db->query("show tables");
-while($row =& $res->fetchRow()){
-	$tables[] = $row[0];
-}
-
-// get whitey
-foreach($tables as $table){
-	$res =& $db->query("explain $table");
-	while($row =& $res->fetchRow(DB_FETCHMODE_ASSOC)){
-		//confessArray($row, "row $table");
-		if($row['Type'] == 'longtext'){
-            printf('alter table %s add column %s_cache varchar(255);<br />',
-                   $table, $row['Field']);
-            // OK populate it now
-            
-		}
-	}
-
+    $tab =& new CoopObject(&$cp, 'table_permissions', &$nothing);
+    $tab->obj->query("show tables");
+    while($tab->obj->fetch()){
+        $table  = $tab->obj->Tables_in_coop;
+        $lucy =& new CoopObject(&$cp, 'table_permissions', &$nothing);
+        $lucy->obj->query("explain $table");
+        while($lucy->obj->fetch()){
+            //confessArray($row, "row $table");
+            if($lucy->obj->Type == 'longtext'){
+                printf('alter table %s add column %s_cache varchar(255);<br />',
+                       $table, $row['Field']);
+                // OK populate it now
+            }
+        }
+    }
 }
 
 ////KEEP EVERTHANG BELOW
+$cp =& new CoopPage($debug);
+makeColumns($cp);
+
+
 
 ?>
 
