dd3248c670c64834f79f6e066a2b1b0fcba1be75
ken restivo <ken@restivo.org>
2006-01-09 04:58:33 +0000

    his version of my code. yay.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2344 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/lib/phpserialize/PHPSerialize.py
+++ web/lib/phpserialize/PHPSerialize.py
@@ -3,7 +3,7 @@ import types, string
 """
 Serialize class for the PHP serialization format.
 
-@version v0.3 BETA
+@version v0.4 BETA
 @author Scott Hurring; scott at hurring dot com
 @copyright Copyright (c) 2005 Scott Hurring
 @license http://opensource.org/licenses/gpl-license.php GNU Public License
@@ -16,9 +16,11 @@ Usage:
 # Create an instance of the serialize engine
 s = PHPSerialize()
 # serialize some python data into a string
-serialized_string = s.serialize(data)
+serialized_string = s.serialize(string)
+# encode a session list (php's session_encode) 
+serialized_string = s.session_encode(list)
 
-Please see README.txt for more information.
+See README.txt for more information.
 """
 
 class PHPSerialize(object):
@@ -26,33 +28,91 @@ class PHPSerialize(object):
 	Class to serialize data using the PHP Serialize format.
 
 	Usage:
-	s = PHPSerialize()
-	serialized_string = s.serialize(data)
+	serialized_string = PHPSerialize().serialize(data)
+	serialized_string = PHPSerialize().session_encode(list)
 	"""
-
+	
 	def __init__(self):
 		pass
 
-	def serialize(self, data):
-		return self._serialize(data)
-
-	def _serialize(self, data):
+	def session_encode(self, session):
+		"""Thanks to Ken Restivo for suggesting the addition
+		of session_encode
 		"""
-		Serialize data recursively.
-		Uses type() to figure out what type to serialize a thing as...
+		out = ""
+		for (k,v) in session.items():
+			out = out + "%s|%s" % (k, self.serialize(v))
+		return out
+		
+	def serialize(self, data):
+		return self.serialize_value(data)
+
+	def is_int(self, data):
+		"""
+		Determine if a string var looks like an integer
+		TODO: Make this do what PHP does, instead of a hack
+		"""
+		try: 
+			int(data)
+			return True
+		except:
+			return False
+		
+	def serialize_key(self, data):
+		"""
+		Serialize a key, which follows different rules than when 
+		serializing values.  Many thanks to Todd DeLuca for pointing 
+		out that keys are serialized differently than values!
+		
+		From http://us2.php.net/manual/en/language.types.array.php
+		A key may be either an integer or a string. 
+		If a key is the standard representation of an integer, it will be
+		interpreted as such (i.e. "8" will be interpreted as int 8,
+		while "08" will be interpreted as "08"). 
+		Floats in key are truncated to integer. 
+		"""
+		# Integer, Long, Float, Boolean => integer
+		if type(data) is types.IntType or type(data) is types.LongType \
+		or type(data) is types.FloatType or type(data) is types.BooleanType:
+			return "i:%s;" % int(data)
+			
+		# String => string or String => int (if string looks like int)
+		elif type(data) is types.StringType:
+			if self.is_int(data):
+				return "i:%s;" % int(data)
+			else:
+				return "s:%i:\"%s\";" % (len(data),  data);
+		
+		# None / NULL => empty string
+		elif type(data) is types.NoneType:
+			return "s:0:\"\";"
+		
+		# I dont know how to serialize this
+		else:
+			raise Exception("Unknown / Unhandled key  type (%s)!" % type(data))
+
+
+	def serialize_value(self, data):
+		"""
+		Serialize a value.
 		"""
 
 		# Integer => integer
 		if type(data) is types.IntType:
 			return "i:%s;" % data
 
-		# Floating Point => double
+		# Float, Long => double
 		elif type(data) is types.FloatType or type(data) is types.LongType:
 			return "d:%s;" % data
 
-		# String => string
+		# String => string or String => int (if string looks like int)
+		# Thanks to Todd DeLuca for noticing that PHP strings that
+		# look like integers are serialized as ints by PHP 
 		elif type(data) is types.StringType:
-			return "s:%i:\"%s\";" % (len(data), data);
+			if self.is_int(data):
+				return "i:%s;" % int(data)
+			else:
+				return "s:%i:\"%s\";" % (len(data), data);
 
 		# None / NULL
 		elif type(data) is types.NoneType:
@@ -66,8 +126,8 @@ class PHPSerialize(object):
 			out = []
 			# All arrays must have keys
 			for k in data:
-				out.append(self._serialize(i))
-				out.append(self._serialize(k))
+				out.append(self.serialize_key(i))
+				out.append(self.serialize_value(k))
 				i += 1
 			return "a:%i:{%s}" % (len(data), "".join(out))
 
@@ -76,15 +136,13 @@ class PHPSerialize(object):
 		elif type(data) is types.DictType:
 			out = []
 			for k in data:
-				out.append(self._serialize(k))
-				out.append(self._serialize(data[k]))
+				out.append(self.serialize_key(k))
+				out.append(self.serialize_value(data[k]))
 			return "a:%i:{%s}" % (len(data), "".join(out))
 
 		# Boolean => bool
 		elif type(data) is types.BooleanType:
-			if data: b = 1
-			else: b = 0
-			return "b:%i;" % (b)
+			return "b:%i;" % (data == 1)
 
 		# I dont know how to serialize this
 		else:
--- web/lib/phpserialize/PHPUnserialize.py
+++ web/lib/phpserialize/PHPUnserialize.py
@@ -1,9 +1,9 @@
-import types, string
+import types, string, re
 
 """
 Unserialize class for the PHP serialization format.
 
-@version v0.3 BETA
+@version v0.4 BETA
 @author Scott Hurring; scott at hurring dot com
 @copyright Copyright (c) 2005 Scott Hurring
 @license http://opensource.org/licenses/gpl-license.php GNU Public License
@@ -21,167 +21,169 @@ data = u.unserialize(serialized_string)
 Please see README.txt for more information.
 """
 
-import re
-
 class PHPUnserialize(object):
-    """
-    Class to unserialize something from the PHP Serialize format.
+	"""
+	Class to unserialize something from the PHP Serialize format.
 
-    Usage:
-    u = PHPUnserialize()
-    data = u.unserialize(serialized_string)
-    """
+	Usage:
+	u = PHPUnserialize()
+	data = u.unserialize(serialized_string)
+	"""
 
-    def __init__(self):
-        pass
+	def __init__(self):
+		pass
 
-    def unserialize(self, data):
-        return self._unserialize(data, 0)[2]
+	def session_decode(self, data):
+		"""Thanks to Ken Restivo for suggesting the addition
+		of session_encode
+		"""
+		session = {}
+		while len(data) > 0:
+			m = re.match('^(\w+)\|', data)
+			if m:
+				key = m.group(1)
+				offset = len(key)+1
+				(dtype, dataoffset, value) = self._unserialize(data, offset)
+				offset = offset + dataoffset
+				data = data[offset:]
+				session[key] = value
+			else:
+				# No more stuff to decode 
+				return session
+		
+		return session
+		
+	def unserialize(self, data):
+		return self._unserialize(data, 0)[2]
 
-    def _unserialize(self, data, offset=0):
-        """
-        Find the next token and unserialize it.
-        Recurse on array.
+	def _unserialize(self, data, offset=0):
+		"""
+		Find the next token and unserialize it.
+		Recurse on array.
 
-        offset = raw offset from start of data
-        """
+		offset = raw offset from start of data
+		
+		return (type, offset, value)
+		"""
 
-        dtype = None
-        buf = []
-        dre = re.compile('(\w):')
-        match = dre.match(data, offset)
-        if match:
-            dataoffset = match.end()
-            dtype = match.group(1)
+		buf = []
+		dtype = string.lower(data[offset:offset+1])
 
+		#print "# dtype =", dtype
 
-        #print "# dtype =", dtype
+		# 't:' = 2 chars
+		dataoffset = offset + 2
+		typeconvert = lambda x : x
+		chars = datalength = 0
 
-        typeconvert = lambda x : x
-        chars = datalength = 0
+ 		# int => Integer
+		if dtype == 'i':
+			typeconvert = lambda x : int(x)
+			(chars, readdata) = self.read_until(data, dataoffset, ';')
+			# +1 for end semicolon
+			dataoffset += chars + 1
 
-        # handle php session_encode() arrays with bare-string keys
-        if dtype == None:
-            sessionre = re.compile('(\w+)\|')
-            while True:
-                match = sessionre.match(data, dataoffset)
-                if match == None:
-                    break
-                dataoffset = match.end()
-                key=match.group(1)
-                (vtype, vchars, value) = self._unserialize(data, dataoffset)
-                dataoffset += vchars
-                readdata[key] = value
+ 		# bool => Boolean
+		elif dtype == 'b':
+			typeconvert = lambda x : (int(x) == 1)
+			(chars, readdata) = self.read_until(data, dataoffset, ';')
+			# +1 for end semicolon
+			dataoffset += chars + 1
 
-        # int => Integer
-        elif dtype == 'i':
-            typeconvert = lambda x : int(x)
-            (chars, readdata) = self.read_until(data, dataoffset, ';')
-            # +1 for end semicolon
-            dataoffset += chars + 1
+		# double => Floating Point
+		elif dtype == 'd':
+			typeconvert = lambda x : float(x)
+			(chars, readdata) = self.read_until(data, dataoffset, ';')
+			# +1 for end semicolon
+			dataoffset += chars + 1
 
-        # bool => Boolean
-        elif dtype == 'b':
-            typeconvert = lambda x : (int(x) == 1)
-            (chars, readdata) = self.read_until(data, dataoffset, ';')
-            # +1 for end semicolon
-            dataoffset += chars + 1
+		# n => None
+		elif dtype == 'n':
+			readdata = None
 
-        # double => Floating Point
-        elif dtype == 'd':
-            typeconvert = lambda x : float(x)
-            (chars, readdata) = self.read_until(data, dataoffset, ';')
-            # +1 for end semicolon
-            dataoffset += chars + 1
+		# s => String
+		elif dtype == 's':
+			(chars, stringlength) = self.read_until(data, dataoffset, ':')
+			# +2 for colons around length field
+			dataoffset += chars + 2
 
-        # n => None
-        elif dtype == 'n':
-            readdata = None
+			# +1 for start quote
+			(chars, readdata) = self.read_chars(data, dataoffset+1, int(stringlength))
+			# +2 for endquote semicolon
+			dataoffset += chars + 2
 
-        # s => String
-        elif dtype == 's':
-            (chars, stringlength) = self.read_until(data, dataoffset, ':')
-            # +2 for colons around length field
-            dataoffset += chars + 2
+			if chars != int(stringlength) != int(readdata):
+				raise Exception("String length mismatch")
 
-            # +1 for start quote
-            (chars, readdata) = self.read_chars(data, dataoffset+1, int(stringlength))
-            # +2 for endquote semicolon
-            dataoffset += chars + 2
+		# array => Dict
+		# If you originally serialized a Tuple or List, it will
+		# be unserialized as a Dict.  PHP doesn't have tuples or lists,
+		# only arrays - so everything has to get converted into an array
+		# when serializing and the original type of the array is lost
+		elif dtype == 'a':
+			readdata = {}
 
-            if chars != int(stringlength) != int(readdata):
-                raise Exception("String length mismatch")
+			# How many keys does this list have?
+			(chars, keys) = self.read_until(data, dataoffset, ':')
+			# +2 for colons around length field
+			dataoffset += chars + 2
 
-        # array => Dict
-        # If you originally serialized a Tuple or List, it will
-        # be unserialized as a Dict.  PHP doesn't have tuples or lists,
-        # only arrays - so everything has to get converted into an array
-        # when serializing and the original type of the array is lost
-        elif dtype == 'a':
-            readdata = {}
+			# Loop through and fetch this number of key/value pairs
+			for i in range(0, int(keys)):
+				# Read the key
+				(ktype, kchars, key) = self._unserialize(data, dataoffset)
+				dataoffset += kchars
+				#print "Key(%i) = (%s, %i, %s) %i" % (i, ktype, kchars, key, dataoffset)
 
-            # How many keys does this list have?
-            (chars, keys) = self.read_until(data, dataoffset, ':')
-            # +2 for colons around length field
-            dataoffset += chars + 2
+				# Read value of the key
+				(vtype, vchars, value) = self._unserialize(data, dataoffset)
+				dataoffset += vchars
+				#print "Value(%i) = (%s, %i, %s) %i" % (i, vtype, vchars, value, dataoffset)
 
-            # Loop through and fetch this number of key/value pairs
-            for i in range(0, int(keys)):
-                # Read the key
-                (ktype, kchars, key) = self._unserialize(data, dataoffset)
-                dataoffset += kchars
-                #print "Key(%i) = (%s, %i, %s) %i" % (i, ktype, kchars, key, dataoffset)
+				# Set the list element
+				readdata[key] = value
 
-                # Read value of the key
-                (vtype, vchars, value) = self._unserialize(data, dataoffset)
-                dataoffset += vchars
-                #print "Value(%i) = (%s, %i, %s) %i" % (i, vtype, vchars, value, dataoffset)
+				# +1 for end semicolon
+			dataoffset += 1
+			#chars = int(dataoffset) - start
 
-                # Set the list element
-                readdata[key] = value
-
-                # +1 for end semicolon
-            dataoffset += 1
-
-        #chars = int(dataoffset) - start
-        
-        # I don't know how to unserialize this
-        else:
-            raise Exception("Unknown / Unhandled data type (%s)!" % dtype)
+		# I don't know how to unserialize this
+		else:
+			raise Exception("Unknown / Unhandled data type (%s)!" % dtype)
 
 
-        return (dtype, dataoffset-offset, typeconvert(readdata))
+		return (dtype, dataoffset-offset, typeconvert(readdata))
 
-    def read_until(self, data, offset, stopchar):
-        """
-        Read from data[offset] until you encounter some char 'stopchar'.
-        """
-        buf = []
-        char = data[offset:offset+1]
-        i = 2
-        while char != stopchar:
-            # Consumed all the characters and havent found ';'
-            if i+offset > len(data):
-                raise Exception("Invalid")
-            buf.append(char)
-            char = data[offset+(i-1):offset+i]
-            i += 1
+	def read_until(self, data, offset, stopchar):
+		"""
+		Read from data[offset] until you encounter some char 'stopchar'.
+		"""
+		buf = []
+		char = data[offset:offset+1]
+		i = 2
+		while char != stopchar:
+			# Consumed all the characters and havent found ';'
+			if i+offset > len(data):
+				raise Exception("Invalid")
+			buf.append(char)
+			char = data[offset+(i-1):offset+i]
+			i += 1
 
-        # (chars_read, data)
-        return (len(buf), "".join(buf))
+		# (chars_read, data)
+		return (len(buf), "".join(buf))
 
-    def read_chars(self, data, offset, length):
-        """
-        Read 'length' number of chars from data[offset].
-        """
-        buf = []
-        # Account for the starting quote char
-        #offset += 1
-        for i in range(0, length):
-            char = data[offset+(i-1):offset+i]
-            buf.append(char)
+	def read_chars(self, data, offset, length):
+		"""
+		Read 'length' number of chars from data[offset].
+		"""
+		buf = []
+		# Account for the starting quote char
+		#offset += 1
+		for i in range(0, length):
+			char = data[offset+(i-1):offset+i]
+			buf.append(char)
 
-        # (chars_read, data)
-        return (len(buf), "".join(buf))
+		# (chars_read, data)
+		return (len(buf), "".join(buf))
 
 
