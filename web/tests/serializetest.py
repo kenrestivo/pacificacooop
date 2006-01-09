d3673e5c83468f8c08cc2cd4d17393ed4c0d4681
ken restivo <ken@restivo.org>
2006-01-09 05:37:43 +0000

    easier to use in inerpreter
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2347 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/tests/serializetest.py
+++ web/tests/serializetest.py
@@ -13,8 +13,12 @@ from phpserialize.PHPUnserialize import PHPUnserialize
 ## go get it
 sys.path.insert(0, '/mnt/kens/ki/proj/coop/web/objects')
 sys.path.insert(0, '/mnt/kens/ki/proj/coop/web')
-from  model import *
+import model
 
-as=SessionInfo.get(434)
 
-inpython=PHPUnserialize().unserialize(as.vars)
+
+
+as2=model.SessionInfo.selectBy(session_id = '827ea3347b547e97b3e51710af714812')
+
+
+inpython=PHPUnserialize().session_decode(as.vars)
