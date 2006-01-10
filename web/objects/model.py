ce3112919a11aaccfb5e6a754440ef50e2ff331c
ken restivo <ken@restivo.org>
2006-01-10 20:50:37 +0000

    datetime must have 00000 not none for default
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2361 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/objects/model.py
+++ web/objects/model.py
@@ -8,8 +8,8 @@ sqlhub.processConnection = connectionForURI(dbhost.connectionurl)
 class SessionInfo(SQLObject):
        session_id=StringCol(length=32)
        ip_addr=StringCol(length=20)
-       entered=DateTimeCol(default=None)
-       updated=DateTimeCol(default=None)
+       entered=DateTimeCol(default='0000-00-00 00:00:00')
+       updated=DateTimeCol(default='0000-00-00 00:00:00')
        User=ForeignKey('Users')
        vars=BLOBCol()
        class sqlmeta:
