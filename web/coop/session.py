2c74554e7a659a87e8edc8372fcb639eda21b6d3
ken restivo <ken@restivo.org>
2006-01-10 17:39:35 +0000

    first post
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2358 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/coop/session.py
+++ web/coop/session.py
@@ -1,9 +1,20 @@
+"""
+a simple library for session management that interoperates with the
+PHP $_SESSION stuff, as implemented in the coop database
+i.e. the SessionInfo or session_info table in mysql
+it's not very extensible at this time, beyond the coop database
+"""
+
 
 import model
 import Cookie
 from phpserialize.PHPSerialize import PHPSerialize
 from phpserialize.PHPUnserialize import PHPUnserialize
 from posix import environ
+
+
+class NoSessionSaved(Exception):
+    pass
 
 
 class Session:
@@ -14,20 +25,40 @@ class Session:
     new_cookies = None
     page=None
     session_data = {}
+    db_obj = None
+    key_name = 'coop'
 
-if environ.has_key('HTTP_COOKIE'):
-    self.recv_cookies=Cookie.BaseCookie(environ['HTTP_COOKIE'])
-    self.recv_cookie_dict=dict([(i[0],i[1].value) for i in self.recv_cookies.items()])
-    page.debug.append('found cookies: %s' % (self.recv_cookie_dict))
-    self.session_data = phpun.session_decode(model.SessionInfo.get(self.recv_cookie_dict['coop']).vars)
-    #TODO: cache the db object too-- will need to save the session later
+    def write_session(self):
+        pass
 
-if not (environ.has_key('HTTP_COOKIE') and self.recv_cookies.has_key('coop')):
-    new_cookies=Cookie.BaseCookie()
-    newid = 'XXXtestnewsession'
-    self.new_cookies['coop'] = newid
-    #TODO: save the  new session!
-    page.headers.append(repr(self.new_cookies))
-    page.debug.append('new cookies: "%s<br />"' % (str(self.new_cookies)))
 
+    def __init__(self, page):
+        try:
+            self.get_session()
+        except NoSessionSaved:
+            self.create_session()
+            
+
+    def get_session(self):
+        if not environ.has_key('HTTP_COOKIE'):
+            raise NoSessionSaved
+        self.recv_cookies=Cookie.BaseCookie(environ['HTTP_COOKIE'])
+        if not self.recv_cookies.has_key(self.key_name)):
+            raise NoSessionSaved
+        self.recv_cookie_dict=dict(
+            [(i[0],i[1].value) for i in self.recv_cookies.items()])
+        self.page.debug.append('found cookies: %s' % (
+            self.recv_cookie_dict))
+        self.db_obj = model.SessionInfo.get(
+            self.recv_cookie_dict[self.key_name])
+        self.session_data = phpun.session_decode(self.db_obj.vars)
+                
+           
+    def create_session(self):
+        new_cookies=Cookie.BaseCookie()
+        newid = 'XXXtestnewsession'
+        self.new_cookies[self.key_name] = newid
+        self.page.headers.append(repr(self.new_cookies))
+        self.page.debug.append('new cookies: "%s<br />"' % (str(self.new_cookies)))
+    
 
