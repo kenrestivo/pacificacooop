d1865e09a9433ad6b1b6d72ef575031aef2acced
ken restivo <ken@restivo.org>
2006-01-12 18:31:00 +0000

    well here it is
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2392 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/objects/Companies_income_join.php
+++ web/objects/Companies_income_join.php
@@ -23,4 +23,29 @@ class Companies_income_join extends CoopDBDO
 
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
+	var $fb_linkDisplayFields = array('company_id', 'income_id');
+
+	var $fb_fieldLabels = array ('income_id' => 'Payment Information',
+                                 'company_id' => 'Solicitation Company',
+                                 'family_id' => 'Soliciting Family');
+
+	var $fb_formHeaderText =  'Springfest Solicitation Cash Donations';
+
+	var $fb_requiredFields = array('auction_donation_item_id', 
+                                   'company_id');
+
+
+    var $fb_shortHeader = 'Solicitation Cash';
+
+    function fb_linkConstraints(&$co)
+		{
+            $auc =& new CoopObject(&$co->page, 'income', 
+                                   &$co);
+            $auc->constrainSchoolYear();
+            $co->protectedJoin($auc);
+            // TODO: somehow make orderbylinkdisplay() recursive
+            $co->grouper();
+		}
+
+
 }
