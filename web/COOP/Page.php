2be4d6097c6e8d0c12b0659acffcf227d086d62b
ken restivo <ken@restivo.org>
2006-01-16 20:53:41 +0000

    use css to do the topnav
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2431 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/COOP/Page.php
+++ web/COOP/Page.php
@@ -174,7 +174,9 @@ class coopPage
 			$u = getUser($this->auth['uid']);	// ugh.
 
 			return sprintf(
-                '<div><div style="font-weight: bold; float:left">%s</div><div style="text-align:right"><strong>Welcome %s!</strong><br>%s</div></div>', 
+                '<div><div id="heading">%s</div>
+                <div id="loginblock"><strong>Welcome %s!</strong><br>
+                %s</div></div>', 
                 $this->heading,
                 $u['username'],
                 $this->selfURL(array('value' =>"Log Out", 
