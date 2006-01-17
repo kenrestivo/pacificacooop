f554a602c000178896120d83015835192728f8c3
ken restivo <ken@restivo.org>
2006-01-17 11:41:15 +0000

    old style but here it is
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2455 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/reports/attendance.php
+++ web/reports/attendance.php
@@ -1,6 +1,6 @@
 <?php
 
-	#  Copyright (C) 2004-2005  ken restivo <ken@restivo.org>
+	#  Copyright (C) 2004  ken restivo <ken@restivo.org>
 	# 
 	#  This program is free software; you can redistribute it and/or modify
 	#  it under the terms of the GNU General Public License as published by
@@ -19,62 +19,113 @@
 //$Id$
 
 require_once('CoopPage.php');
-require_once('CoopNewDispatcher.php');
-require_once "HTML/Template/PHPTAL.php";
-require_once "lib/phptal_filters.php";
-require_once('lib/dbdo_iterator.php');  // XXX hack, around problems on nfsn
+require_once('CoopView.php');
+require_once('CoopForm.php');
+require_once('HTML/Table.php');
 
 
 
-class Report{
-    var $page;
-    var $families;
-    var $templateFile = 'attendance.xhtml';
+//MAIN
+//$_SESSION['toptable'] 
 
-    function Report (&$cp){
-        $this->page =& $cp;
-    }
+//DB_DataObject::debugLevel(2);
 
-    function build()
-        {
-            $this->families =& new CoopView(&$this->page, 'families', 
-                                      &$nothing);
-            $this->families->find(true);
-            $this->page->title = 'Parent Ed Attendance Summary Report';
-        }
+$cp = new coopPage( $debug);
+print $cp->pageTop();
 
 
-} // END REPORT CLASS
+$atd = new CoopView(&$cp, 'companies', $none);
+
+print $cp->topNavigation();
+print $cp->stackPath();
+
+print "\n<hr /></div><!-- end header div -->\n"; //ok, we're logged in. show the rest of the page
+print '<div id="centerCol">';
 
 
 
-//////// MAIN
-$cp =& new coopPage( $debug);
+
+function viewHack(&$atd)
+{
+
+    $res = '';
+
+    
+    $view =& new CoopView(&$atd->page, 'families', &$faketop);
+	$view->obj->fb_formHeaderText = 'Parent Ed Attendance';
+    $view->obj->fb_fieldLabels= array(
+        'name' => 'Co-Op Family',
+        'missed_meetings' => 'Missed Meetings',
+        'meetings_required' => 'Meetings Required',
+        'meetings_attended' => 'Meetings Attended');
+    $view->obj->preDefOrder= array_keys($view->obj->fb_fieldLabels);
+    $view->obj->fb_recordActions = array('details' => ACCESS_VIEW);      
+
+    $view->obj->query(
+        sprintf(
+'select distinct enrolled.family_id, enrolled.name, 
+count(distinct calendar_events.calendar_event_id) - round(hours/3) as missed_meetings,
+count(distinct calendar_events.calendar_event_id) as meetings_required,
+hours/3 as meetings_attended
+from calendar_events,
+(select distinct families.family_id, families.name, start_date, dropout_date
+                    from families
+                        left join kids on families.family_id = kids.family_id 
+                        left join enrollment on kids.kid_id = enrollment.kid_id 
+                    where enrollment.school_year = "%s"
+                    and (enrollment.dropout_date < "1900-01-01"
+                       or enrollment.dropout_date > "2006-01-06"
+                        or enrollment.dropout_date is null)
+                    group by families.family_id
+                    order by families.name) as enrolled
+left join (select sum(hours) as hours, family_id
+     from  parent_ed_attendance
+     left join parents    on parent_ed_attendance.parent_id = parents.parent_id
+     left join calendar_events 
+        on parent_ed_attendance.calendar_event_id = calendar_events.calendar_event_id
+     where school_year = "%s"
+     group by family_id) as attended
+on attended.family_id = enrolled.family_id
+where school_year = "%s" and event_id = 2
+and event_date >= start_date and  
+    (event_date <= dropout_date or dropout_date is null)
+and event_date <= now()
+group by enrolled.family_id
+order by enrolled.name
+',
+        $view->getChosenSchoolYear(),
+        $view->getChosenSchoolYear(),
+        $view->getChosenSchoolYear()
+));
+    $res .= $view->simpleTable(false);
 
 
-// got to RUN certain things before anything makes sense
-$cp->logIn();
-
-$report =& new Report(&$cp);
-$report->build();
-
-// let the template know all about it
-$template = new PHPTAL($report->templateFile);
-$template->setRef('page', $cp);
-$template->setRef('report', $report);
-
-
-//confessObj($template->getContext(), 'context');
-
-
-$template->addOutputFilter(new XML_to_HTML());
-
-if(headers_sent($file, $line)){
-    PEAR::raiseError("headers sent at $file $line ", 666);
+	return $res;
+	 
 }
-print  $template->execute();
-$cp->finalDebug();
+
+// cheap dispatcher
+//confessArray($_REQUEST,'req');
+switch($_REQUEST['action']){
+ 
+	 
+//// EDIT AND NEW //////
+ case 'new':
+ case 'edit':
+	 break;
+
+//// DEFAULT (VIEW) //////
+ default:
+	 print viewHack(&$atd);
+
+	 break;
+}
 
 
+done ();
+
+////KEEP EVERTHANG BELOW
 
 ?>
+
+
