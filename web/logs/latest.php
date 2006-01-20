723d0a65decf846218ec4c01e825fc425f6c549c
ken restivo <ken@restivo.org>
2006-01-20 09:35:59 +0000

    yay, takes my python logs too
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2497 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/logs/latest.php
+++ web/logs/latest.php
@@ -6,7 +6,7 @@
 if ($handle = opendir('.')) {
     /* This is the correct way to loop over the directory. */
     while (false !== ($file = readdir($handle))) {
-        if(strstr($file, 'debug.html')){
+        if(strstr($file, '-debug.')){
             $secondprev = $prev;
             $prev = $saver;
             $saver = $file;
@@ -18,7 +18,7 @@ if ($handle = opendir('.')) {
     print '<a href="testresults.php">back to test results</a><br />';
 
 
-    printf('LATEST.php: %s is most recent. <a href="%s">%s</a> is previous. <a href="%s">%s</a> is third previous<br>',
+    printf('LATEST.php: %s is most recent. <a href="%s">%s</a> is previous. <a href="%s">%s</a> is third previous<br><br>',
            $saver, $prev, $prev, $secondprev, $secondprev);
     $fd = fopen($saver, 'r');
     do {
