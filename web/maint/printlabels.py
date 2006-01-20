a59251c55326fab3f3ab9b6e6c438622edaa4fb9
ken restivo <ken@restivo.org>
2006-01-20 02:56:43 +0000

    holy crap it works
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2490 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/maint/printlabels.py
+++ web/maint/printlabels.py
@@ -47,13 +47,45 @@ for i in environ.items():
     page.raw_output.append('%s: %s<br />' % i )
     
 
+from reportlab.platypus import SimpleDocTemplate, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.rl_config import defaultPageSize
+from reportlab.platypus.tables import Table,TableStyle
+from reportlab.lib import colors
+from reportlab.lib.units import inch
+
+
+page=coop_page.Page()
+page.headers['Content-Type'] = 'application/pdf'
+page.headers['Content-Disposition'] = 'attachment; filename= "labeltest.pdf"'
+
+    
+    
+doc = SimpleDocTemplate(sys.stdout)
+story=[]
+
+## note data must be multiple of 3, need to PAD IT OUT!!
+n=3
+rawdata = range(0, 105)
+data = [rawdata[i:i + n] for i in range(0, len(rawdata), n)]
+
+DEBUG_LAYOUT = TableStyle( [('OUTLINE', (0,0), (-1,-1), 0.25, colors.red),
+                            ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+                            ('ALIGN', (0,0), (-1,-1), 'LEFT'),
+                            ('VALIGN', (0,0), (-1,-1), 'MIDDLE')] )
+
+t=Table(data, colWidths=2*inch, rowHeights=1.5*inch)
+t.setStyle(DEBUG_LAYOUT)
+story.append(t)
+    
 
 
 
 
 ##### finally output stuff
-#page.render_raw(True)
-page.render_template()
+page.output_headers()
+doc.build(story)
+
 
 
 #END
