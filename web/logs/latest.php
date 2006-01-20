48179076a61fb4d8a1a79e20c314328ae095c1a9
ken restivo <ken@restivo.org>
2006-01-20 11:28:33 +0000

    show python debug crap too
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2501 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/logs/latest.php
+++ web/logs/latest.php
@@ -6,7 +6,8 @@
 if ($handle = opendir('.')) {
     /* This is the correct way to loop over the directory. */
     while (false !== ($file = readdir($handle))) {
-        if(strstr($file, '-debug.')){
+        if(strstr($file, '-debug.') || preg_match('/tmp.+\.txt/', $file))
+        {
             $secondprev = $prev;
             $prev = $saver;
             $saver = $file;
@@ -21,6 +22,9 @@ if ($handle = opendir('.')) {
     printf('LATEST.php: %s is most recent. <a href="%s">%s</a> is previous. <a href="%s">%s</a> is third previous<br><br>',
            $saver, $prev, $prev, $secondprev, $secondprev);
     $fd = fopen($saver, 'r');
+    if(strstr($saver, '.txt')){
+        print "<pre>";
+    }
     do {
         $data = fread($fd, 8192);
         if (strlen($data) == 0) {
