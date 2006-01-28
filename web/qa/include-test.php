e1fd5ebc60284caec85ae7ecfca2e07122e25afb
ken restivo <ken@restivo.org>
2006-01-28 19:01:46 +0000

    excel writer too
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2561 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/qa/include-test.php
+++ web/qa/include-test.php
@@ -3,7 +3,7 @@
 //$Id$
 
 // unit test for my vitally required includes. return an error if
-// the shit has hit the fan
+// something has gone horribly wrong
 chdir('../');
 require_once('first.inc');
 require_once "HTML/QuickForm.php";
@@ -15,12 +15,16 @@ require_once 'HTML/Menu/DirectTreeRenderer.php';
 require_once("DB/DataObject.php");
 require_once("HTML/Table.php");
 require_once('DB/DataObject.php');
-//require_once('DB/DataObject/FormBuilder.php');
 require_once('Pager/Pager.php');
 require_once('Mail.php');
 require_once('Text/Diff.php');
+require_once "Spreadsheet/Excel/Writer.php";
 
-//require_once('FAIL-NOW.php');
+//TODO: check for lib/tiny_mce/tiny_mce.js , which is NOT in my cvs!
+//and is required for the app to run
+
+
+//require_once('FAIL-NOW.php'); // make sure it fails too ;-)
 
 print "OK";
 
