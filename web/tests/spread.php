8f4b3439c8a5f1e35adc6653b535854aef77569e
ken restivo <ken@restivo.org>
2006-01-28 02:04:50 +0000

    first post
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2548 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ web/tests/spread.php
@@ -0,0 +1,22 @@
+<?php
+// Include PEAR::Spreadsheet_Excel_Writer
+require_once "Spreadsheet/Excel/Writer.php";
+
+// Create an instance
+$xls =& new Spreadsheet_Excel_Writer();
+
+// Send HTTP headers to tell the browser what's coming
+$xls->send("test.xls");
+
+// Add a worksheet to the file, returning an object to add data to
+$sheet =& $xls->addWorksheet('Binary Count');
+
+// Write some numbers
+for ( $i=0;$i<11;$i++ ) {
+ // Use PHP's decbin() function to convert integer to binary
+ $sheet->write($i,0,decbin($i));
+}
+
+// Finish the spreadsheet, dumping it to the browser
+$xls->close();
+?>
