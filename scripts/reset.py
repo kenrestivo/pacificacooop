fe26a47e2ac970d52d1d1a05bab3a5ba68b13487
ken restivo <ken@restivo.org>
2006-02-01 06:28:03 +0000

    clearer
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2585 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/reset.py
+++ scripts/reset.py
@@ -5,17 +5,16 @@
 # resets the COOP (main) db back to  live backup
 
 from os import system,popen
-
+from getpass import getpass
 
 #XXX get args from shell calling this!
 args = ''
 
+filenames = [[l[0:-1] for l in popen('ls -t backups/*%s*' % (i)).readlines()][0] for i in ['defs', 'data']]
 
-data=[l[0:-1] for l in popen('ls -t backups/*data*').readlines()][0]
-defs=[l[0:-1] for l in popen('ls -t backups/*defs*').readlines()][0]
-
-
-for i in [defs, data]:
+for i in filenames:
+    print 'restoring %s...' % (i)
     system('bzcat %s | mysql %s' %(i, args)) 
 
+print 'setting fake "tester" password...'
 system('mysql %s -e "update users set password = md5(\'tester\')" coop' %(args))
