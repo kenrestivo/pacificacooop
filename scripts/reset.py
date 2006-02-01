8fedc5a7813ebd757de6b3533d6b4936d278934e
ken restivo <ken@restivo.org>
2006-02-01 06:31:55 +0000

    very cool, now only need to type the password once!
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2586 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/reset.py
+++ scripts/reset.py
@@ -12,9 +12,12 @@ args = ''
 
 filenames = [[l[0:-1] for l in popen('ls -t backups/*%s*' % (i)).readlines()][0] for i in ['defs', 'data']]
 
+print 'Type MySQL password'
+pw=getpass()
+
 for i in filenames:
-    print 'restoring %s...' % (i)
-    system('bzcat %s | mysql %s' %(i, args)) 
+    print 'restoring from %s...' % (i)
+    system('bzcat %s | mysql -p%s %s' %(i, pw, args)) 
 
 print 'setting fake "tester" password...'
-system('mysql %s -e "update users set password = md5(\'tester\')" coop' %(args))
+system('mysql -p%s %s -e "update users set password = md5(\'tester\')" coop' %(pw,args))
