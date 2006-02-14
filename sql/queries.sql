eba18cff6f0501400ae4d0eee408e8dd8c45fafa
ken restivo <ken@restivo.org>
2006-02-14 02:37:06 +0000

    add both to springfest invitations report.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2647 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- sql/queries.sql
+++ sql/queries.sql
@@ -2133,35 +2133,27 @@ order by total desc
 ;
 
 --- both
-select  relation,
-    sum(if(family_id>0,0,payment_amount)) as Alumni_List ,
-    sum(if(family_id>0,payment_amount,0)) as Family_Supplied ,
+select  invitations.relation,
+    sum(if(invitations.family_id>0,0,payment_amount)) as Alumni_List ,
+    sum(if(invitations.family_id>0,payment_amount,0)) as Family_Supplied ,
     sum(payment_amount)  as Total 
-from
-(select invitations.relation as relation, 
-invitations.family_id as family_id, 
-payment_amount 
-  from tickets
-   left join invitations 
-       on invitations.lead_id = tickets.lead_id
-       and invitations.school_year  = "2005-2006"
-   left join income on income.income_id = tickets.income_id
-where tickets.lead_id is not null 
-     and tickets.ticket_quantity > 0
-     and tickets.school_year = "2005-2006"
-group by invitations.relation) as ticket_sum
- join
-(select invitations.relation as relation, 
-invitations.family_id as family_id, 
-payment_amount 
-  from leads_income_join
-   left join invitations 
+from invitations
+   left join leads_income_join
        on invitations.lead_id = leads_income_join.lead_id
        and invitations.school_year  = "2005-2006"
-   left join income on income.income_id = leads_income_join.income_id
-where leads_income_join.lead_id is not null 
-     and income.school_year = "2005-2006"
-group by invitations.relation) as rsvp_sum
+   left join tickets
+       on invitations.lead_id = tickets.lead_id
+       and invitations.school_year  = "2005-2006"
+   left join income on income.income_id = tickets.income_id or leads_income_join.income_id = income.income_id
+where income.school_year = "2005-2006" 
+    and 
+    (leads_income_join.lead_id is not null 
+        or 
+    (tickets.lead_id is not null 
+        and tickets.ticket_quantity > 0
+        and tickets.school_year = "2005-2006"))
+group by invitations.relation
+order by total desc
 ;
 
 
