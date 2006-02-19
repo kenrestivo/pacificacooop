9f762a4a2c1e6b55f5316b99ccf695868ef5e408
ken restivo <ken@restivo.org>
2006-02-19 23:12:54 +0000

    first
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2686 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ web/services/custom_font.php
@@ -0,0 +1,46 @@
+<?php
+ 
+// if no text is provided, we will use some default text
+if (!isset($_GET["section_heading"]))
+    $text = "Please Enter Some Text!!";
+else
+    $text = $_GET["section_heading"];
+ 
+// set this to the location of the TrueType font file that you want to use
+$font = "/mnt/kens/ki/proj/coop/imports/Bernhard_Modern_BT.ttf";
+$font_size = 18;
+// angle of the font in degrees
+$font_angle = 0;
+// the weight of the font stroke
+$stroke = 2;
+// the position of the font
+$startx = 10;
+$starty = 30;
+ 
+$im = imagecreate(450,40);
+ 
+// define the colours that we will be using
+$white = imagecolorallocate($im, 255, 255, 255);
+$blue1 = imagecolorallocate($im, 175, 188, 199);
+$blue2 = imagecolorallocate($im, 64, 99, 122);
+ 
+imagefilltoborder($im, 0, 0, $blue1, $blue1);
+
+// now draw out the outline (stroke) on the text
+for ($ox = -$stroke; $ox <= $stroke; $ox++) {
+    for ($oy = -$stroke; $oy <= $stroke; $oy++) {
+        imagettftext($im, $font_size, $font_angle, $startx+$ox, $starty+$oy,
+                $blue2, $font, $text);
+    }
+}
+imagettftext($im, $font_size, $font_angle, $startx, $starty, $white, $font, $text);
+ 
+// set the correct HTTP header for a PNG image
+header("Content-type: image/png");
+ 
+imagepng($im);
+ 
+// remember to free up the memory used on the server to create the image!
+imagedestroy($im);
+
+?>
