b86503c1f8afa02b9fd376d2e28758c594b54080
ken restivo <ken@restivo.org>
2006-02-23 17:30:14 +0000

    make it OOPish
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2712 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ web/COOP/Page/TAL.php
@@ -0,0 +1,75 @@
+<?php
+
+	#  Copyright (C) 2004-2006  ken restivo <ken@restivo.org>
+	# 
+	#  This program is free software; you can redistribute it and/or modify
+	#  it under the terms of the GNU General Public License as published by
+	#  the Free Software Foundation; either version 2 of the License, or
+	#  (at your option) any later version.
+	# 
+	#  This program is distributed in the hope that it will be useful,
+	#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+	#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	#  GNU General Public License for more details. 
+	# 
+	#  You should have received a copy of the GNU General Public License
+	#  along with this program; if not, write to the Free Software
+	#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+//$Id$
+
+require_once('CoopPage.php');
+require_once('CoopView.php');
+require_once "HTML/Template/PHPTAL.php";
+require_once "lib/phptal_filters.php";
+require_once('CoopIterator.php');  // XXX hack, around problems on nfsn
+
+
+class CoopReport
+{
+    var $page; // coop page cache
+    var $template; // reference to phptal template
+    
+    function CoopReport($debug = false)
+        {
+            $this->page =& new coopPage( $debug);
+            
+            // got to RUN certain things before anything makes sense
+            $this->page->logIn();
+
+        }
+
+    
+    function build()
+        {
+            // virtual function. put your stuff in subclass of this.
+            $template = new PHPTAL('sometemplate.xhtml');
+            
+        }
+
+
+
+    function run()
+        {
+            $this->build();
+
+            // NOTE: if this ref is unavailable,
+            // the whole page fails except done()
+            $this->template->setRef('page', $this->page);
+        
+            $this->template->addOutputFilter(new XML_to_HTML());
+            
+            if(headers_sent($file, $line)){
+                PEAR::raiseError("headers sent at $file $line ", 666);
+            }
+            print  $this->template->execute();
+            $this->page->finalDebug();
+
+        }
+
+
+
+} // END COOPREPORT
+
+
+?>
--- web/reports/bid_sheets.php
+++ web/reports/bid_sheets.php
@@ -18,97 +18,71 @@
 
 //$Id$
 
-require_once('CoopPage.php');
-require_once('CoopNewDispatcher.php');
-require_once "HTML/Template/PHPTAL.php";
-require_once "lib/phptal_filters.php";
-require_once('CoopIterator.php');  // XXX hack, around problems on nfsn
+require_once('CoopReport.php');
 
-
-
-// specific to this page. when i dispatch with REST, i'll need several
-function &build(&$page)
+class BidSheetReport extends CoopReport
 {
 
-    // let the template know all about it
-    $template = new PHPTAL('bid-sheet.xhtml');
+// specific to this page. when i dispatch with REST, i'll need several
+    function build()
+        {
 
-    $page->title = 'Bid Sheets and Gift Certificates';
+            // let the template know all about it
+            $this->template = new PHPTAL('bid-sheet.xhtml');
 
-    ////////////// BIDSHEETS
-    $bidsheets =& new CoopView(&$page, 'packages', &$nothing);
-    $pt =& new CoopView(&$page, 'package_types', &$bidsheets);
-    $bidsheets->protectedJoin($pt);
-    $bidsheets->obj->whereAdd('package_type_short = "Silent"');
-    // tal needs this to decide whether to print the increment
-    array_push($bidsheets->obj->fb_preDefOrder, 'package_type_short');
-    $bidsheets->obj->fb_fieldLabels['package_type_short'] = 'Package Type';
+            $this->page->title = 'Bid Sheets and Gift Certificates';
+
+            ////////////// BIDSHEETS
+            $bidsheets =& new CoopView(&$this->page, 'packages', &$nothing);
+            $pt =& new CoopView(&$this->page, 'package_types', &$bidsheets);
+            $bidsheets->protectedJoin($pt);
+            $bidsheets->obj->whereAdd('package_type_short = "Silent"');
+            // tal needs this to decide whether to print the increment
+            array_push($bidsheets->obj->fb_preDefOrder, 'package_type_short');
+            $bidsheets->obj->fb_fieldLabels['package_type_short'] = 'Package Type';
     
 
-    $bidsheets->fullText= 1; // gotta have it
+            $bidsheets->fullText= 1; // gotta have it
 
-    if(devSite() && $_REQUEST['limit']){
-         // XXX TEMPORARY HACK FOR TESTING
-        $bidsheets->obj->limit($_REQUEST['limit']);
-    }
+            if(devSite() && $_REQUEST['limit']){
+                // XXX TEMPORARY HACK FOR TESTING
+                $bidsheets->obj->limit($_REQUEST['limit']);
+            }
 
-    $bidsheets->find(true);
-    $template->setRef('bidsheets', $bidsheets);
+            $bidsheets->find(true);
+            $this->template->setRef('bidsheets', $bidsheets);
 
-    $page->printDebug("sy $sy nav $nav ". $bidsheets->getChosenSchoolYear(), 1);
+            $this->page->printDebug("sy $sy nav $nav ". $bidsheets->getChosenSchoolYear(), 1);
 
 
-    ////////////// GIFT CERTIFICATES 
-    $giftcerts =& new CoopView(&$page, 'packages', &$nothing);
-    $pt =& new CoopView(&$page, 'package_types', &$giftcerts);
-    $giftcerts->protectedJoin($pt);
-    $giftcerts->obj->whereAdd('item_type = "Gift Certificate"');
-    $bidsheets->obj->whereAdd('(package_type_short = "Live" or package_type_short = "Silent")');
+            ////////////// GIFT CERTIFICATES 
+            $giftcerts =& new CoopView(&$this->page, 'packages', &$nothing);
+            $pt =& new CoopView(&$this->page, 'package_types', &$giftcerts);
+            $giftcerts->protectedJoin($pt);
+            $giftcerts->obj->whereAdd('item_type = "Gift Certificate"');
+            $bidsheets->obj->whereAdd('(package_type_short = "Live" or package_type_short = "Silent")');
 
-    $giftcerts->fullText= 1; // gotta have it
+            $giftcerts->fullText= 1; // gotta have it
 
-    if(devSite() && $_REQUEST['limit']){
-         // XXX TEMPORARY HACK FOR TESTING
-        $giftcerts->obj->limit($_REQUEST['limit']);
-    }
+            if(devSite() && $_REQUEST['limit']){
+                // XXX TEMPORARY HACK FOR TESTING
+                $giftcerts->obj->limit($_REQUEST['limit']);
+            }
 
-    $giftcerts->find(true);
-    $template->setRef('giftcerts', $giftcerts);
+            $giftcerts->find(true);
+            $this->template->setRef('giftcerts', $giftcerts);
 
-    // simple year.
-    list($crap, $year) = explode('-', $giftcerts->getChosenSchoolYear());
-    $year = 'Springfest ' . $year;
-    $template->setRef('eventdate', $year);
+            // simple year.
+            list($crap, $year) = explode('-', $giftcerts->getChosenSchoolYear());
+            $year = 'Springfest ' . $year;
+            $this->template->setRef('eventdate', $year);
     
-
-    return $template;
+        }
 }
 
 
-//////// MAIN
-$cp =& new coopPage( $debug);
-
-
-// got to RUN certain things before anything makes sense
-$cp->logIn();
-
-
-$template =& build(&$cp);
-
-// NOTE: if this ref is unavailable, the whole page fails except done()
-$template->setRef('page', $cp);
-
-
-//confessObj($template->getContext(), 'context');
-
-
-$template->addOutputFilter(new XML_to_HTML());
-
-if(headers_sent($file, $line)){
-    PEAR::raiseError("headers sent at $file $line ", 666);
-}
-print  $template->execute();
-$cp->finalDebug();
+    $r =& new BidSheetReport(4);
+    $r->run();
 
 
 ?>
