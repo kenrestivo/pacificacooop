23f017f4cb6c3243b7bb3ac51ea07ce1590a7f8a
ken restivo <ken@restivo.org>
2006-02-24 07:36:14 +0000

    ok here we go: custom fonts!
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2720 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/services/custom_font.php
+++ web/services/custom_font.php
@@ -1,33 +1,50 @@
 <?php
  
 // if no text is provided, we will use some default text
-if (!isset($_GET["section_heading"]))
+if (!isset($_GET["text"])){
     $text = "Please Enter Some Text!!";
-else
-    $text = $_GET["section_heading"];
- 
+}    else {
+    $text = $_GET["text"];
+}
+
+
+if (!isset($_GET["size"])){
+    $font_size = 18;
+}    else {
+    $font_size = $_GET["size"];
+}
+
+
 // set this to the location of the TrueType font file that you want to use
 $font = "/mnt/kens/ki/proj/coop/imports/Bernhard_Modern_BT.ttf";
-$font_size = 18;
+
+
 // angle of the font in degrees
 $font_angle = 0;
 // the weight of the font stroke
 $stroke = 2;
 // the position of the font
-$startx = 10;
-$starty = 30;
  
-$im = imagecreate(450,40);
+
+
+$bbox = imagettfbbox($font_size, $font_angle, $font, $text);
+$x_size = abs($bbox[4] - $bbox[0]);
+$y_size = abs($bbox[5] - $bbox[1]);
+
+// gah, descenders. multiply by 1.25 to guess
+$im = imagecreate($x_size * 1.05, $y_size * 1.25);
+
  
 // define the colours that we will be using
-$white = imagecolorallocate($im, 255, 255, 255);
-$blue1 = imagecolorallocate($im, 175, 188, 199);
-$blue2 = imagecolorallocate($im, 64, 99, 122);
-$black = imagecolorallocate($im, 0, 0, 10);
+$bgcolor = imagecolorallocate($im, 255, 255, 255);
+$textcolor = imagecolorallocate($im, 0, 0, 10);
  
 //imagefilltoborder($im, 0, 0, $blue1, $blue1);
+$startx = $font_size/4;
+$starty = $font_size*1.5;
 
-imagettftext($im, $font_size, $font_angle, $startx, $starty, $black, $font, $text);
+imagettftext($im, $font_size, $font_angle, $startx, $starty, 
+             $textcolor, $font, $text);
  
 // set the correct HTTP header for a PNG image
 header("Content-type: image/png");
