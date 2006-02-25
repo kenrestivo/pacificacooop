16c5d6bac04c95ae7180ea3f7a90f694b69033da
ken restivo <ken@restivo.org>
2006-02-25 21:14:08 +0000

    a litle fancier
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2753 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/tests/pdftest.php
+++ web/tests/pdftest.php
@@ -5,9 +5,13 @@ chdir('../');
 require_once('CoopPage.php');
 require_once('lib/fpdf.php');
 
-$pdf=new FPDF();
+$pdf=new FPDF('P', pt, 'Letter');
+$pdf->SetMargins(36,36,36,36);
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'Hello World!');
+$pdf->Cell(100,22,'Hello at '. date('r'));
+$pdf->Ln();
+$pdf->Cell(100,22,'this rules');
 $pdf->Output();
+
 ?> 
