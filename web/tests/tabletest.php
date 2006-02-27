8bf7656163110f6a973784d0f8db3b247adbe27c
ken restivo <ken@restivo.org>
2006-02-27 05:32:46 +0000

    hey, working basic pdml crap
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2765 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ web/tests/tabletest.php
@@ -0,0 +1,32 @@
+<?php
+
+chdir('../');
+
+require_once('CoopPDF.php');
+require_once('lib/pdml.php');
+
+
+class TestPDML extends CoopPDF
+{
+
+    function build()
+        {
+            $fname='/mnt/www/restivo/bc/pdml/hello.pdml';
+            $f = fopen($fname, 'r');
+            $data = fread($f, filesize($fname));
+            fclose ($f);
+
+
+            $this->fpdf = new PDML('P','pt','A4'); 
+            $this->fpdf->compress=0;
+            $this->fpdf->ParsePDML($data);
+
+        }
+}
+
+
+$r =& new TestPDML($debug);
+$r->run();
+
+
+?> 
