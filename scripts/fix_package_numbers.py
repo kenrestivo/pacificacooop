eb0bae9dcc73515afb7d29705030bb20668e8172
ken restivo <ken@restivo.org>
2006-03-01 22:34:47 +0000

    first
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2788 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ scripts/fix_package_numbers.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+
+from sys import path
+path.append('/mnt/kens/ki/is/python/lib')
+path.append('/mnt/kens/ki/proj/coop/scripts')
+path.append('/mnt/kens/ki/proj/coop/web')
+path.append('/mnt/kens/ki/proj/coop/web/objects')
+
