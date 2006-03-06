5ad52335bc5ff896793f1603c25a4446913cd18c
ken restivo <ken@restivo.org>
2006-03-06 01:37:57 +0000

    well it works
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2822 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ web/maint/renumber-packages.php
@@ -0,0 +1,90 @@
+<?php
+
+	#  Copyright (C) 2004-2006  ken restivo <ken@restivo.org>
+	# 
+	#  This program is free software; you can redistribute it and/or modify
+	#  it under the terms of the GNU General Public License as published by
+	#  the Free Software Foundation; either version 2 of the License, or
+	#  (at your option) any later version.
+	# 
+	#  This program is distributed in the hope that it will be useful,
+	#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+	#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	#  GNU General Public License for more details. 
+	# 
+	#  You should have received a copy of the GNU General Public License
+	#  along with this program; if not, write to the Free Software
+	#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+//$Id$
+
+chdir('../');
+require_once('CoopPage.php');
+require_once('CoopView.php');
+
+
+
+//MAIN
+//$_SESSION['toptable'] 
+
+//DB_DataObject::debugLevel(2);
+
+$cp = new coopPage( $debug);
+print $cp->pageTop();
+
+
+$atd = new CoopView(&$cp, 'packages', $none);
+
+print $cp->topNavigation();
+print $cp->stackPath();
+
+print "\n<hr /></div><!-- end header div -->\n"; //ok, we're logged in. show the rest of the page
+print '<div class="centerCol">';
+
+print '<h1>renumber packages</h1>';
+
+
+function viewHack(&$atd)
+{
+        $ptype =& new CoopObject(&$atd->page, 'package_types', &$atd);
+        $ptype->obj->find();
+        while($ptype->obj->fetch()) {   
+            $pack =& new CoopObject(&$atd->page, 'packages', &$atd);
+            $pack->obj->query('set @num=0'); // makes me nervous
+            $pack->obj->query(
+                sprintf(
+                    'update %s set package_number=(@num:=@num+1) 
+where package_type_id = %d and school_year = "%s"
+ORDER BY package_number',
+                    $pack->obj->__table,
+                    $ptype->obj->package_type_id,
+                    $pack->getChosenSchoolYear()));
+        }
+	 
+}
+
+// cheap dispatcher
+//confessArray($_REQUEST,'req');
+switch($_REQUEST['action']){
+ 
+	 
+//// EDIT AND NEW //////
+ case 'new':
+ case 'edit':
+	 break;
+
+//// DEFAULT (VIEW) //////
+ default:
+	 print viewHack(&$atd);
+
+	 break;
+}
+
+
+done ();
+
+////KEEP EVERTHANG BELOW
+
+?>
+
+
