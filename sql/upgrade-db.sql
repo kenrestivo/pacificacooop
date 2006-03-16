dee194fa720b86e35cec3e0a78eaef032cd0b49b
ken restivo <ken@restivo.org>
2006-03-16 09:53:00 +0000

    stupid tal version. this is so above my head.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2837 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- sql/upgrade-db.sql
+++ sql/upgrade-db.sql
@@ -30,9 +30,6 @@
 -- add votes/question/answer tables (definition)
  INSERT INTO realms (short_description , meta_realm_id ) VALUES
 ('Polls' , 0 );
-INSERT INTO table_permissions (table_name , field_name
-, realm_id , user_level , group_level , menu_level , year_level ) VALUES 
-('votes' , NULL, 25 , -1 , -1 , -1 , -1 );
 
 INSERT INTO user_privileges (user_id , group_id ,
 user_level , group_level , realm_id , year_level , menu_level ) VALUES 
@@ -41,11 +38,7 @@ INSERT INTO user_privileges (user_id , group_id ,
 user_level , group_level , realm_id , year_level , menu_level ) VALUES 
 ( 0 , 1 , 600 , 0 , 25 , 200 , -1 );
 
- INSERT INTO table_permissions (table_name , field_name
-, realm_id , user_level , group_level , menu_level , year_level ) VALUES
-('questions' , NULL, 25 , -1 , -1 , -1 , -1 );
-INSERT INTO table_permissions (table_name , field_name
-, realm_id , user_level , group_level , menu_level , year_level ) VALUES 
-('answers' , NULL, 25 , -1 , -1 , -1 , -1 );
+
+
 
 -- seed the questions and answers (seed.sql)
