c6986a7b25f463c85aa3567521290c9e2cf56978
ken restivo <ken@restivo.org>
2006-03-24 02:39:45 +0000

    still broken
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2861 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/finddeletion.py
+++ scripts/finddeletion.py
@@ -13,20 +13,11 @@ import MySQLdb, MySQLdb.cursors
 school_year = '2005-2006'
 testvalues = [290, 246, 1655, 448, 486]
 
-def restore(file):
-    print 'restoring from %s (%s)...' % (i, file)
-    system('bzcat %s | mysql -p%s %s' %(file, pw, args)) 
-    print 'setting fake "tester" password...'
-    system('mysql -p%s %s -e "update users set password = md5(\'tester\')" coop' %(pw,args))
 
 
 
 
 ######MAIN
-conn=MySQLdb.connect(user='input', passwd='test', db='coop',
-                     host='127.0.0.1', port=2299,
-                     cursorclass=MySQLdb.cursors.DictCursor) 
-c=conn.cursor()
 
 #get args from shell
 args = ' '.join(sys.argv[1:])
@@ -48,16 +39,22 @@ pw=getpass()
 found = dict()
 for n in range(0,len(res['data'])):
     for i in res.keys():
-        restore(res[i][n])
-    ### test the results
+        print 'restoring from %s (%s)...' % (i, res[i][n])
+        system('bzcat %s | mysql -p%s %s' %(res[i][n], pw, args))
+    print 'setting fake "tester" password...'
+    system('mysql -p%s %s -e "update users set password = md5(\'tester\')" coop' %(pw,args))
+   ### test the results
+    conn=MySQLdb.connect(user='input', passwd='test', db='coop',
+                     host='127.0.0.1', port=2299,
+                     cursorclass=MySQLdb.cursors.DictCursor) 
+    c=conn.cursor()
     found[res['data'][n]] = dict()
     for t in testvalues:
         c.execute("""select * from sponsorships where lead_id = %d and school_year = '%s'""" % (t,school_year))
-        found[res['data'][n]][t] =  c.fetchall()
+        found[res['data'][n]][t] =  len(c.fetchall())
+    conn.close()
+
     
         
 
 
-#finally, restore the latest one [0]
-for i in res.keys():
-    restore(res[i][0])
