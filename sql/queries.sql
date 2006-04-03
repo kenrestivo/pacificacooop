8a2d9be9efc3a7d6c423166dcadf6de84b2c8d26
ken restivo <ken@restivo.org>
2006-04-03 06:14:50 +0000

    the subquery version! it kicks ass, it even works now.
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2944 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- sql/queries.sql
+++ sql/queries.sql
@@ -2478,13 +2478,14 @@ where this_question.question_id is null
 --- narsty recoverexisting query
 select distinct thank_you.*,
 coalesce(auction_summary.school_year, in_kind_summary.school_year, 
-    income.school_year) as school_year,
+    income_summary.school_year) as school_year,
 concat_ws("\n", coalesce(leads.company, companies.company_name), 
     concat_ws(" ", coalesce(leads.first_name, companies.first_name),
         coalesce(leads.last_name, companies.last_name))) as recipient,
 concat_ws(" ", working_parents.first_name, working_parents.last_name) 
     as salesperson,
-concat_ws('\n', auction_summary.short_description) as items
+concat_ws('\n', auction_summary.short_description, 
+        income_summary.total_payment) as items
 from thank_you
 left join (select auction_donation_items.*, 
                 companies_auction_join.family_id, 
@@ -2503,28 +2504,35 @@ left join (select in_kind_donations.*,
                 on companies_in_kind_join.in_kind_donation_id = 
                     in_kind_donations.in_kind_donation_id) as in_kind_summary 
     on thank_you.thank_you_id = in_kind_summary.thank_you_id
-left join income on thank_you.thank_you_id = income.thank_you_id
-left join companies_income_join 
-    on companies_income_join.income_id = income.income_id
+left join (select sum(income.payment_amount) as total_payment,
+            income.thank_you_id, income.school_year,
+             coalesce(leads_income_join.lead_id, tickets.lead_id) as lead_id,
+            companies_income_join.company_id, companies_income_join.family_id
+            from income 
+              left join companies_income_join 
+                  on companies_income_join.income_id = income.income_id
+              left join leads_income_join 
+                    on leads_income_join.income_id = income.income_id
+            left join tickets on tickets.income_id = income.income_id
+            group by income.thank_you_id)
+            as income_summary
+    on thank_you.thank_you_id = income_summary.thank_you_id
 left join companies 
-    on coalesce(companies_income_join.company_id, 
+    on coalesce(income_summary.company_id, 
         auction_summary.company_id, in_kind_summary.company_id) 
             = companies.company_id
-left join leads_income_join on leads_income_join.income_id = income.income_id
-left join tickets on tickets.income_id = income.income_id
 left join leads 
-    on coalesce(leads_income_join.lead_id, tickets.lead_id) = leads.lead_id
+    on income_summary.lead_id = leads.lead_id
 left join (select parents.* from parents 
             left join workers on parents.parent_id = workers.parent_id
             where workers.parent_id is not null) as working_parents
         on working_parents.family_id = 
-        coalesce(companies_income_join.family_id, 
+        coalesce(income_summary.family_id, 
                 auction_summary.family_id)
 where (auction_summary.school_year = "2004-2005" 
     or in_kind_summary.school_year = "2004-2005" 
-    or income.school_year = "2004-2005") 
-and (companies.company_id  = 49 or companies.company_id = 127 or companies.company_id  = 27)
-group by thank_you.thank_you_id
+    or income_summary.school_year = "2004-2005") 
+and (companies.company_id  = 49 or companies.company_id = 127 or companies.company_id  = 27 or thank_you.thank_you_id = 64)
 order by coalesce(leads.company, companies.company_name), 
     coalesce(leads.last_name, companies.last_name),
      coalesce(leads.first_name, companies.first_name)
