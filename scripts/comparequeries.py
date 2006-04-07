3db76ae87b0591d4afd65eda0bb3153d6829c639
ken restivo <ken@restivo.org>
2006-04-07 06:43:27 +0000

    first post
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2983 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ scripts/comparequeries.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python -u
+
+#$Id$
+
+"""Imports the rasta in the old Excel/CSV format, and updates
+the databse with its spiffy new contents"""
+
+
+import os
+from datetime import date
+import MySQLdb, MySQLdb.cursors
+import datetime
+import re
+
+
+
+def fix(line):
+    return re.sub('^--.*','' , re.sub('\\\G', '',line))
+
+
+
+conn=MySQLdb.connect(user='input', passwd='test', db='coop',
+                     host='127.0.0.1', port=3306,
+                     cursorclass=MySQLdb.cursors.DictCursor) 
+c=conn.cursor()
+
+
+c.execute("""set @school_year := '2005-2006', @ticket_price := 30,
+@ad_text := 'ad valued at', @ticket_text := 'to Springfest valued at',
+@cash_text := 'cash'""")
+
+
+f=open('/mnt/kens/ki/proj/coop/web/sql/thankyouneeded.sql')
+
+query = ''.join([fix(line) for line in f.readlines()])
+
+
