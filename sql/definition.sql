bd5520b834ee373c3a73fa009962c355eacfc533
ken restivo <ken@restivo.org>
2006-04-19 20:18:14 +0000

    add title.,
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@2999 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- sql/definition.sql
+++ sql/definition.sql
@@ -1059,6 +1059,7 @@ school_year varchar(50) default NULL
 
 create table misc_documents(
 misc_documents_id int(32) primary key not null unique auto_increment,
+title varchar(255),
 body longtext,
 _cache_body varchar(255)
 );
--- web/objects/Misc_documents.php
+++ web/objects/Misc_documents.php
@@ -11,6 +11,7 @@ class Misc_documents extends CoopDBDO
 
     var $__table = 'misc_documents';                  // table name
     var $misc_documents_id;               // int(32)  not_null primary_key unique_key auto_increment
+    var $title;                           // string(255)  
     var $body;                            // blob(16777215)  blob
     var $_cache_body;                     // string(255)  
 
@@ -22,4 +23,23 @@ class Misc_documents extends CoopDBDO
 
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
+
+
+	var $fb_linkDisplayFields = array('_cache_body');
+
+	var $fb_fieldLabels = array (
+        'title' => 'Document Title',
+        'body' => 'Body of document'
+        );
+
+	var $fb_formHeaderText =  'Miscellaneous Documents';
+
+    var $fb_shortHeader = 'Documents';
+
+//     function preGenerateForm(&$form)
+//         {
+            //TODO: use my own custom widget?
+//         }
+
+
 }
