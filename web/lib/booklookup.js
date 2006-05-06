f7fc489f8b17275fbe087e88928aaf87fb37f912
ken restivo <ken@restivo.org>
2006-05-06 08:40:05 +0000

    error/status messages
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@3015 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/lib/booklookup.js
+++ web/lib/booklookup.js
@@ -7,6 +7,9 @@
 function bookLookup(isbn_name, baseurl, access_key){
     isbn = document.getElementsByName(isbn_name).item(0);
     isbn.value = isbn.value.replace(/[^0-9X]/g,'');
+    status = document.getElementById('status-books-isbn');
+    status.innerHTML = 'Searching...';
+
     d=doSimpleXMLHttpRequest(baseurl + '/amazon-hack.php', 
         {'Service':'AWSECommerceService',
          'AWSAccessKeyId': access_key,
@@ -14,7 +17,6 @@ function bookLookup(isbn_name, baseurl, access_key){
          'IdType': 'ASIN',
          'ItemId': isbn.value,
          'ResponseGroup': 'Small'})
-        
 
     d.addCallback(
         function(data){
@@ -29,5 +31,14 @@ function bookLookup(isbn_name, baseurl, access_key){
             isbn.form['books-authors'].value = a.join(', ');
             isbn.form['books-title'].value = ''; // clear out first
             isbn.form['books-title'].value = r.getElementsByTagName('Title').item(0).textContent;
+            if(isbn.form['books-title'].value != ''){
+                status.innerHTML = 'Found it!';
+            } else {
+                status.innerHTML = 'Try again: No such ISBN number';
+            }
         });
+    d.addErrback(
+        function(data){
+        status.innerHTML = 'ERROR: Could not find ISBN. Did you mistype it?';
+         });
 }
