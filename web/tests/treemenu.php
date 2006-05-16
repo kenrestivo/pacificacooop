92c51a85adecea8fcca17fd41ce4631f958c4fe1
ken restivo <ken@restivo.org>
2006-05-16 05:44:30 +0000

    holy crap, it recurses now
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@3060 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/tests/treemenu.php
+++ web/tests/treemenu.php
@@ -12,6 +12,50 @@ require_once("CoopView.php");
 require_once('HTML/TreeMenu.php');
 
 
+class Crap 
+{
+    var $tmenu; // ref of TreeMenu object
+    var $menustack = array();
+
+
+    function Crap()
+        {
+            $this->tmenu  =& new HTML_TreeMenu();
+        }
+
+    function subcurse(&$item, &$parent)
+        {
+            // XXX whoops! need to recurse, dude!
+            if(!empty($item['sub'])){
+                array_push($this->menustack, &$parent);
+                foreach($item['sub'] as $id => $subitem){
+                    $subnode =& new HTML_TreeNode(array('text' => $subitem['title'],
+                                                        'link' => $subitem['url'],
+                                                        'icon' => $icon));
+                    $this->menustack[count($this->menustack) - 1]->addItem($subnode);
+                    $this->subcurse(&$subitem, &$subnode);
+                }
+                array_pop($this->menustack);
+            }
+            
+        }
+    
+    
+    function traverse(&$menustruct)
+        {
+            foreach($menustruct as $id => $item){
+                $node =& new HTML_TreeNode(array('text' => $item['title'],
+                                                 'link' => $item['url'],
+                                                 'icon' => $icon));
+                $this->tmenu->addItem($node); // have to add the toplevels to the menu!
+                $this->subcurse(&$item, &$node);
+            }
+        }
+     
+} // end class
+
+
+
 printf('<script src="%s/HTML_TreeMenu/TreeMenu.js" language="JavaScript" type="text/javascript"></script>', 
        COOP_ABSOLUTE_URL_PATH_PEAR_DATA);
 
@@ -26,30 +70,12 @@ $cp->logIn(); // gotta do that for testing
 $menu =& new CoopMenu(&$cp);
 $menu->getMenu();
 
-$tmenu  =& new HTML_TreeMenu();
-$menustack = array();
-foreach($menu->vars['menu'] as $id => $item){
-   $node =& new HTML_TreeNode(array('text' => $item['title'],
-                                    'link' => $item['url'],
-                                    'icon' => $icon));
-   $tmenu->addItem($node); // have to add the toplevels to the menu!
-
-   // XXX whoops! need to recurse, dude!
-   if(!empty($item['sub'])){
-       array_push($menustack, &$node);
-       foreach($item['sub'] as $id => $subitem){
-           $subnode =& new HTML_TreeNode(array('text' => $subitem['title'],
-                                            'link' => $subitem['url'],
-                                            'icon' => $icon));
-           $menustack[count($menustack) - 1]->addItem($subnode);
-       }
-       array_pop($menustack);
-   }
-}
+$crap =& new Crap();
+$crap->traverse(&$menu->vars['menu']);
 
 // Create the presentation class for the side menu!
 $treeMenu =& new HTML_TreeMenu_DHTML(
-    $tmenu, 
+    $crap->tmenu, 
     array('images' => 
           COOP_ABSOLUTE_URL_PATH_PEAR_DATA . '/HTML_TreeMenu/images',
           'defaultClass' => 'treeMenuDefault'));
