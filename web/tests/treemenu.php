a6eba9f4185adc02a8472be7239735b3cafd06c8
ken restivo <ken@restivo.org>
2006-05-16 03:43:40 +0000

    remove old cruft
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@3058 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- web/tests/treemenu.php
+++ web/tests/treemenu.php
@@ -17,60 +17,6 @@ printf('<script src="%s/HTML_TreeMenu/TreeMenu.js" language="JavaScript" type="t
 
 $icon = 'folder.gif';
 
-$menu  =& new HTML_TreeMenu();
-
-$topnode =& new HTML_TreeNode(array('text' => "First level",
-                                 'link' => "/phpwork/environs.php",
-                                 'icon' => $icon));
-$menu->addItem($topnode); // have to add the toplevels to the menu!
-$prev =& $topnode;
-
-$next   = &$prev->addItem(new HTML_TreeNode(array('text' => "Second level",
-                                                  'link' => "",
-                                                  'icon' => $icon)));
-$prev =& $next; // going down one level
-
-$next   = &$prev->addItem(new HTML_TreeNode(array('text' => "Third level",
-                                                'link' => "/phpwork/environs.php",
-                                                'icon' => $icon)));
-
-$next = &$prev->addItem(new HTML_TreeNode(array('text' => "Fourth level",
-                                                'link' => "/phpwork/environs.php",
-                                                'icon' => $icon)));
-$prev =& $next; // going down one level
-
-$next =& $prev->addItem(new HTML_TreeNode(array('text' => "Fifth level",
-                                        'icon' => $icon,
-                                        'cssClass' => 'treeMenuBold')));
-
-$topnode->addItem(new HTML_TreeNode(array('text' => "Second level, item 2",
-                                        'link' => "/phpwork/environs.php",
-                                        'icon' => $icon)));
-
-$topnode->addItem(new HTML_TreeNode(array('text' => "Second level, item 3",
-                                        'link' => "/phpwork/environs.php",
-                                        'icon' => $icon)));
-
-$topnode =& new HTML_TreeNode(array('text' => "Another First level",
-                                 'link' => "/phpwork/environs.php",
-                                 'icon' => $icon));
-
-$menu->addItem($topnode); // have to add the toplevels to the menu!
-
-
-// Create the presentation class for the side menu!
-$treeMenu = &new HTML_TreeMenu_DHTML(
-    $menu, 
-    array('images' => 
-          COOP_ABSOLUTE_URL_PATH_PEAR_DATA . '/HTML_TreeMenu/images',
-          'defaultClass' => 'treeMenuDefault'));
-$treeMenu->printMenu();
-
-
-
-// for the page top, quick nav?
-$listBox  = &new HTML_TreeMenu_Listbox($menu);
-$listBox->printMenu();
 
 
 /// let's try mine now
@@ -108,6 +54,9 @@ $treeMenu =& new HTML_TreeMenu_DHTML(
           COOP_ABSOLUTE_URL_PATH_PEAR_DATA . '/HTML_TreeMenu/images',
           'defaultClass' => 'treeMenuDefault'));
 $treeMenu->printMenu();
+
+$listBox  = &new HTML_TreeMenu_Listbox($tmenu);
+$listBox->printMenu();
 
 
 ?> 
