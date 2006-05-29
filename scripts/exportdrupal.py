b5c129137329eaf13382bb01719f804cfe2f7452
ken restivo <ken@restivo.org>
2006-05-29 16:30:43 +0000

    cool
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@3086 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/exportdrupal.py
+++ scripts/exportdrupal.py
@@ -12,29 +12,29 @@ path.append('/mnt/kens/ki/proj/coop/web/objects')
 
 
 
-civicrmfields=['familyID', 'name', 'phone', 'address1', 'email']
-
-
-
+civicrmfields=['families.family_id', 'families.name',
+               'phone', 'address1', 'email', 'user_id']
 
 
 
 from model import *
 
 
-
-f=open('/tmp/foobar.txt', 'w')
-w=csv.writer(f)
-
-
-
 r=Families.select()
+c=r[0]._connection.getConnection().cursor()
+
+
+
+c.execute('select %s from families left join users using (family_id)' % (','.join(civicrmfields)))
+
 
 ##TODO: parse out the ",pacifica 94044" shit
 
+f=open('/mnt/kens/ki/proj/coop/imports/families-export.txt', 'w')
+w=csv.writer(f)
 w.writerow(civicrmfields)
-for rec in r:
-    w.writerow([getattr(rec, i) for i in civicrmfields])
+for rec in c:
+    w.writerow(rec)
 
 
 
