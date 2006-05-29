106da5d77dd1fd5f4adaec3a78c2b2121b581e8b
ken restivo <ken@restivo.org>
2006-05-29 16:59:03 +0000

    has to have space, otherwise it freaks out, i've no idea why
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@3088 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/exportdrupal.py
+++ scripts/exportdrupal.py
@@ -12,7 +12,7 @@ path.append('/mnt/kens/ki/proj/coop/web/objects')
 
 
 
-civicrmfields=['families.family_id', 'families.name',
+civicrmfields=['families.family_id', 'users.name',
                'phone', 'address1', 'email', 'user_id']
 
 drupalfields=['families.family_id', 'users.name', 'email', 'user_id']
@@ -31,20 +31,30 @@ c=r[0]._connection.getConnection().cursor()
 ##TODO: parse out the ",pacifica 94044" shit
 
 c.execute('select %s from families left join users using (family_id)' % (','.join(civicrmfields)))
-f=open('/mnt/kens/ki/proj/coop/imports/families-export.txt', 'w')
+
+f=open('/mnt/kens/ki/proj/coop/imports/families-export.csv', 'w')
+
 w=csv.writer(f)
+
 w.writerow(civicrmfields)
+
 for rec in c:
     w.writerow(rec)
+
 f.close()
 
 
 c.execute('select %s from families left join users using (family_id) where email is not null' % (','.join(drupalfields)))
-f=open('/mnt/kens/ki/proj/coop/imports/users-export.txt', 'w')
+
+f=open('/mnt/kens/ki/proj/coop/imports/users-export.csv', 'w')
+
 w=csv.writer(f)
+
 w.writerow(drupalfields)
+
 for rec in c:
     w.writerow(rec)
+
 f.close()
 
 
