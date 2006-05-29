c1872cd3b842a6e5b4174c9248307996cffeb250
ken restivo <ken@restivo.org>
2006-05-29 16:42:30 +0000

    well, it kind of works using cursors
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@3087 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

--- scripts/exportdrupal.py
+++ scripts/exportdrupal.py
@@ -15,6 +15,7 @@ path.append('/mnt/kens/ki/proj/coop/web/objects')
 civicrmfields=['families.family_id', 'families.name',
                'phone', 'address1', 'email', 'user_id']
 
+drupalfields=['families.family_id', 'users.name', 'email', 'user_id']
 
 
 from model import *
@@ -25,22 +26,29 @@ c=r[0]._connection.getConnection().cursor()
 
 
 
-c.execute('select %s from families left join users using (family_id)' % (','.join(civicrmfields)))
 
 
 ##TODO: parse out the ",pacifica 94044" shit
 
+c.execute('select %s from families left join users using (family_id)' % (','.join(civicrmfields)))
 f=open('/mnt/kens/ki/proj/coop/imports/families-export.txt', 'w')
 w=csv.writer(f)
 w.writerow(civicrmfields)
 for rec in c:
     w.writerow(rec)
+f.close()
 
 
-
-f.close
+c.execute('select %s from families left join users using (family_id) where email is not null' % (','.join(drupalfields)))
+f=open('/mnt/kens/ki/proj/coop/imports/users-export.txt', 'w')
+w=csv.writer(f)
+w.writerow(drupalfields)
+for rec in c:
+    w.writerow(rec)
+f.close()
 
 
 
 ##[[getattr(rec, i) for i in civicrmfields] for rec in r]
+
 
