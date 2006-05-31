51a79cd75e52ccfd8d911b4a76c7a643426fdfe0
ken restivo <ken@restivo.org>
2006-05-31 10:40:01 +0000

    with killer dbdo test
    git-svn-id: file:///mnt/sdd1/kens/svnroot/trunk/coop@3115 f16cc87a-5815-0410-8a9a-9fe0dcc0200c

---
+++ web/tests/include-nochdir.php
@@ -0,0 +1,42 @@
+<?php
+
+//$Id$
+
+// unit test for my vitally required includes. return an error if
+// something has gone horribly wrong
+
+// so that it will presumably work from anywhere!
+require_once('../first.inc');
+
+
+require_once "HTML/QuickForm.php";
+require_once "HTML/QuickForm/group.php";
+require_once "HTML/Table.php";
+require_once('HTML/TreeMenu.php');
+require_once('DB/DataObject.php');
+require_once('Mail.php');
+require_once('Text/Diff.php');
+require_once "Spreadsheet/Excel/Writer.php";
+require_once "HTML/Template/PHPTAL.php";
+require_once('Types/Iterator.php');
+require_once('Types/Ref.php');
+
+require_once('CoopPage.php');
+require_once('CoopView.php');
+require_once('CoopForm.php');
+require_once('COOP/Menu.php');
+
+
+$cp =& new CoopPage($debug);
+$yech =& new CoopObject(&$cp, 'families', &$none);
+
+
+//TODO: check for lib/tiny_mce/tiny_mce.js , which is NOT in my cvs!
+//and is required for the app to run
+
+
+//require_once('FAIL-NOW.php'); // make sure it fails too ;-)
+
+print "OK";
+
+?>
